1b4bae344c29e2e507d3ee5cd0bafa7f9af6a31a
Alistair English <alistair.english@protonmail.com>
2021-04-26 12:33:43 +1000

    simple subscriber node hooked up to live sim feed

--- src/simple_control/src/main.cpp
+++ src/simple_control/src/main.cpp
@@ -1,5 +1,51 @@
-#include <opencv2/core.hpp>
+#include <memory>
 
-int main() {
-    return 1;
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <cv_bridge/cv_bridge.h>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/image.hpp"
+using std::placeholders::_1;
+
+class ImageSubscriber : public rclcpp::Node
+{
+    public:
+    ImageSubscriber()
+    : Node("minimal_subscriber")
+    {
+        subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+            "/fsds/camera/cam1", 10, std::bind(&ImageSubscriber::topic_callback, this, _1)
+        );
+    }
+
+  private:
+    void topic_callback(const sensor_msgs::msg::Image::SharedPtr msg) const
+    {
+        cv_bridge::CvImagePtr cv_ptr;
+        try
+        {
+            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+        }
+        catch (cv_bridge::Exception& e)
+        {
+            RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
+            return;
+        }
+        cv::imshow("da image", cv_ptr->image);
+        cv::waitKey(1);
+    }
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+};
+
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<ImageSubscriber>());
+  rclcpp::shutdown();
+  return 0;
 }
