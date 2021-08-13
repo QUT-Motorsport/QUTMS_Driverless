5184dfa7c944a79d1a7bc223aecdc1207a256c28
Alastair Bradford <albradford2468@gmail.com>
2021-08-13 11:52:23 +1000

    Removed YOLOv3 darknet, added YOLOv5 as a node
    Also added pytorch as a python dependency in dockerfile

---
+++ src/.yolo_pipeline/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'yolo_pipeline'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='QUT Motorsport',
+    maintainer_email='qutmotorsport.team@gmail.com',
+    description='A package to detect cones from a topic using yolov5',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'firstNode = yoloController.firstNode:main',
+            'detectCones = yoloController.detectCones:main',
+            'testConesTopic = yoloController.yoloTester:main'
+        ],
+    },)
