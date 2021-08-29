e2eb4b13259d6addbaedb8c1ae0ad8251c4b0a98
Alistair English <alistair.english@protonmail.com>
2021-08-29 15:24:48 +1000

    successful connection to sim

--- src/unreal_sim/start.sh
+++ src/unreal_sim/start.sh
@@ -1,15 +1,17 @@
 (
     source /opt/ros/noetic/setup.bash
-    roscore 
+    roscore
 ) &
 
+sleep 2
+
 (
-    source /Formula-Student-Driverless-Simulator/ros/devel/setup.bash
+    source /root/Formula-Student-Driverless-Simulator/ros/devel/setup.bash
     roslaunch fsds_ros_bridge fsds_ros_bridge.launch host:=$SIM_HOST manual_mode:=$MANUAL_MODE
 ) &
 
 (
-    source /bridge_ws/install/setup.bash
+    source /root/bridge_ws/install/setup.bash
     ros2 run ros1_bridge dynamic_bridge --bridge-all-topics
 ) &
 
