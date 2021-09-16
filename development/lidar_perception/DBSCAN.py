771c6335ae17785cdd264f3944e342fbb84d9da2
liam-ferrante <liam.p.ferrante@gmail.com>
2021-09-16 16:09:53 +1000

    Created a function for plotting identified clusters

--- development/lidar_perception/DBSCAN.py
+++ development/lidar_perception/DBSCAN.py
@@ -58,3 +58,28 @@ def recursive_cluster(cluster, neighbourhood, x_ordered_labelled):
             cluster.append(new_neighbourhood)
             recursive_cluster(cluster, new_neighbourhood, x_ordered_labelled)
     return cluster
+
+def plot_clusters_old(clusters, object_points):
+    init_plot_2D("Clustering Grid", "x", "y")
+
+    x = [coords[0] for coords in object_points]
+    y = [coords[1] for coords in object_points]
+
+    plt.plot(x, y, '.', color='red')
+
+    x_means = []
+    y_means = []
+    for i in range(len(clusters)):
+        print("\n")
+        print("Cluster", i)
+        x_mean = 0
+        y_mean = 0
+        for j in range(len(clusters[i][0])):
+            x_mean += clusters[i][0][j][0][0]
+            y_mean += clusters[i][0][j][0][1]
+            print(clusters[i][0][j], "hi")
+        x_means.append(x_mean / len(clusters[i][0]))
+        y_means.append(y_mean / len(clusters[i][0]))
+        print(x_mean, y_mean)
+
+    plt.plot(x_means, y_means, '.', color='blue')
