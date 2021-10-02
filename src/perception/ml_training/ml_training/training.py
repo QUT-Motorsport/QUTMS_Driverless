3cb22c4563f924fa1e2c22d240fe9efe0f955892
Alastair Bradford <albradford2468@gmail.com>
2021-10-02 17:54:39 +1000

    Updated requirements, .devcontainer ignored

--- src/perception/ml_training/ml_training/training.py
+++ src/perception/ml_training/ml_training/training.py
@@ -8,9 +8,15 @@ from fs_msgs.msg import Track
 from qutms_msgs.msg import ConeScan, ConeData
 
 # import other python libraries
+import transforms3d as t3d
 import numpy as np
 import time
 
+
+# define camera coords relative to car frame
+CAM_LOCAL_X = -0.5 #m
+CAM_LOCAL_Y = 0 #m
+CAM_LOCAL_Z = 1 #m
 
 class LocationProcessing(Node):
     def __init__(self):
@@ -30,7 +36,9 @@ class LocationProcessing(Node):
             '/fsds/testing_only/track',
             self.track_callback,
             10)
-        self.odom_subscription_  # prevent unused variable warning
+        self.track_subscription_  # prevent unused variable warning
+        self.got_track = False
+        self.cones = list()
 
         ## creates publisher to 'control_command' with type ControlCommand
         # self.scan_publisher_ = self.create_publisher(
@@ -44,44 +52,62 @@ class LocationProcessing(Node):
 
     # callback function for odom processing
     def odom_callback(self, odom_msg):
-        None
+        # msg format:   Odometry/PoseWithCovariance/Pose/Point/
+        #               - [x,y,z]
+        #               Odometry/PoseWithCovariance/Pose/Quaternion/ 
+        #               - [x,y,z,w]
+        
+        pose_x = odom_msg.pose.pose.position.x
+        pose_y = odom_msg.pose.pose.position.x
+        pose_z = odom_msg.pose.pose.position.x
+        pose = [pose_x, pose_y, pose_z]
+        print("\npose:", pose)
 
+        rot_x = odom_msg.pose.pose.orientation.x
+        rot_y = odom_msg.pose.pose.orientation.y
+        rot_z = odom_msg.pose.pose.orientation.z
+        rot_w = odom_msg.pose.pose.orientation.w
+        rot = [rot_x, rot_y, rot_z, rot_w]
+        print("\nrot:", rot)
+
+        time.sleep(1)
     
     # callback function for track data
     def track_callback(self, track_msg):
-        None
+        # msg format:   Track/Cone/Point/
+        #               - [x,y,z]
+        #               Track/Cone/
+        #               - colour
+        #                   uint8 BLUE=0
+        #                   uint8 YELLOW=1
+        #                   uint8 ORANGE_BIG=2
+        #                   uint8 ORANGE_SMALL=3
+        #                   uint8 UNKNOWN=4
         
+        self.cones = list() # clear previous list
+        if self.got_track == False:
+            for i in range(len(track_msg)):
+                curr_cone = track_msg[i] # define current cone in track list
+                x = curr_cone.location.x
+                y = curr_cone.location.y 
+                z = curr_cone.location.z
+                c = curr_cone.color
+
+                self.cones.append([x,y,z,c])
+            
+            self.got_track = True
 
 
     ## publisher for processed cone data
     def publisher(self):
-        cone_scan = ConeScan()
-        # head = Header()
-
-        cone_data = list()
-        for i in range(len(self.cone_coords)):
-            cone = ConeData()
-            cone.x = self.cone_coords[i][0]
-            cone.y = self.cone_coords[i][1]
-            cone.z = self.cone_coords[i][2]
-            cone.c = self.cone_coords[i][3]
-            cone_data.append(cone)
-       
-        # cant work out how headers work lmao, this breaks stuff
-        # head.stamp = rospy.Time.now()
-        # head.frame_id = "lidar2"
-        # cone_scan.header = head
-
-        cone_scan.data = cone_data
-
-        self.scan_publisher_.publish(cone_scan)
+        None
 
 
 ## main call
 def main(args=None):
     rclpy.init(args=args)
 
-    node = CamProcessing()
+    node = LocationProcessing()
     rclpy.spin(node)
     
     node.destroy_node()
