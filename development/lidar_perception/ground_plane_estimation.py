6266b4a67a2d757a4547e51831241bc1d159b697
liam-ferrante <liam.p.ferrante@gmail.com>
2021-10-14 10:28:20 +1000

    Commited optimised functions of both label_points functions

--- development/lidar_perception/ground_plane_estimation.py
+++ development/lidar_perception/ground_plane_estimation.py
@@ -297,6 +297,78 @@ def label_points(segments, ground_lines):
             labelled_points[i][j].append(is_ground)
     return labelled_points
 
+# Conservative approach implemented using T_D_MAX parameter
+# Modifies input
+def label_points_2(segments, ground_lines):
+    # Assuming multiple lines can be in one segment
+    # Identifying closest line for each point
+    for i in range(NUM_SEGMENTS):
+        for j in range(len(segments[i])): 
+            point = segments[i][j]
+            is_ground = False
+            num_lines = len(ground_lines[i])
+            seg_idx = i
+            # If there is no ground line in current segment, find the closest one
+            if num_lines == 0:
+                left_counter = i-1
+                right_counter = i+1
+                left_idx = (left_counter) % NUM_SEGMENTS
+                right_idx = (right_counter) % NUM_SEGMENTS
+                while left_idx != right_idx:
+                    print("i:", i, "left:", left_idx, "right:", right_idx)
+                    if len(ground_lines[left_idx]) > 0:
+                        seg_idx = left_idx
+                        break
+                    elif len(ground_lines[right_idx]) > 0:
+                        seg_idx = right_idx
+                        break
+                    left_counter -= 1
+                    right_counter += 1
+                    left_idx = (left_counter) % NUM_SEGMENTS
+                    right_idx = (right_counter) % NUM_SEGMENTS
+                if left_idx == right_idx:
+                    raise AssertionError("No ground lines found")
+            line = line_to_end_points(ground_lines[seg_idx][0], seg_idx)
+            closest_dist = dist_points_3D(point, line[0], line[1])
+            for k in range(1, num_lines):
+                line = ground_lines[seg_idx][k]
+                dist_to_line = dist_points_3D(point, line[0], line[1])
+                if (dist_to_line < closest_dist):
+                    closest_dist = dist_to_line
+            if (closest_dist < T_D_MAX and closest_dist < T_D_GROUND):
+                is_ground = True
+            segments[i][j].append(is_ground)
+    return segments
+
+# Conservative approach implemented using T_D_MAX parameter
+# Modifies input
+def label_points_2_old(segments, ground_lines):
+    # Assuming multiple lines in one segment
+    # Identifying closest line for each point
+    for i in range(NUM_SEGMENTS):
+        # For every point in each segment. Perhaps make an array of constants of point counts?
+        for j in range(len(segments[i])):
+            point = segments[i][j]
+            is_ground = False
+            closest_line = None
+            num_lines = len(ground_lines[i])
+            if num_lines > 0:
+                line = ground_lines[i][0]
+                closest_line = line
+                closest_dist = dist_points_3D(point, line[2], line[0], line[1])
+                for k in range(1, num_lines):
+                    line = ground_lines[i][k]
+                    dist_to_line = dist_points_3D(point, line[2], line[0], line[1])
+                    if (dist_to_line < closest_dist):
+                        closest_line = line
+                        closest_dist = dist_to_line
+                hacky_point = [math.sqrt(point[0]**2 + point[1]**2), point[2]] # fix this
+                point_to_line_dist = line_extraction.dist_point_line(hacky_point, closest_line[0], closest_line[1])
+                if (closest_dist < T_D_MAX and point_to_line_dist < T_D_GROUND):
+                    is_ground = True
+            segments[i][j].append(is_ground)
+    return segments
+
 def non_ground_points(labelled_points):
     # Flatten parent array (remove segements)
     labelled_points = [points for sublist in labelled_points for points in sublist]
