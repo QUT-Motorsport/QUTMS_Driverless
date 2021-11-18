00c4741959b751d1e3161c450d905f75d97fa1a3
liam-ferrante <liam.p.ferrante@gmail.com>
2021-11-18 11:37:04 +1000

    Polished DBSCAN

--- src/perception/lidar_pipeline/lidar_pipeline/sub_module/DBSCAN.py
+++ src/perception/lidar_pipeline/lidar_pipeline/sub_module/DBSCAN.py
@@ -1,23 +1,22 @@
-# Density-Based Spatial Clustering of Applications with Noise (DBSCAN)
+# Modules
 import numpy as np
 from sklearn.cluster import DBSCAN
 
-# Noise disabled and clusters get overwritten by cluster_centers
-def init_DBSCAN_2(object_points):
-    EPSILON = 0.275  # Neighbourhood scan size
-    MIN_POINTS = 2 # Minimum number of points required to form a neighbourhood
+# Density-Based Spatial Clustering of Applications with Noise (DBSCAN)
+def get_objects(object_points):
+    EPSILON = 0.275  # Neighbourhood Scan Size
+    MIN_POINTS = 2 # Number of points required to form a neighbourhood
+
     clustering = DBSCAN(eps = EPSILON, min_samples = MIN_POINTS).fit(np.array(object_points))
     cluster_count = max(clustering.labels_) + 1
     clusters = [[] for i in range(cluster_count)]
+
     for i in range(len(clustering.labels_)):
         label = clustering.labels_[i]
         point = object_points[i]
         if label != -1:
             clusters[label].append(point)
-            
-    # I don't acutually think we care about the height of the center
-    # of the cluster. We just want to reconstruct a cyliner, not a sphere.
-    # I believe Monash cares though - but they're a bunch of monkeys
+
     for i in range(len(clusters)):
         x_cluster = [coords[0] for coords in clusters[i]]
         y_cluster = [coords[1] for coords in clusters[i]]
@@ -25,5 +24,17 @@ def init_DBSCAN_2(object_points):
         x_mean = sum(x_cluster) / len(x_cluster)
         y_mean = sum(y_cluster) / len(y_cluster)
         #z_mean = sum(z_cluster) / len(z_cluster)
-        clusters[i] = [x_mean, y_mean] # z_mean
+        clusters[i] = [x_mean, y_mean] # [..., z_mean]
+
     return clusters
+
+# Notes
+# 1. This version of init_DBSCAN has disabled noise, and clusters get overwritten
+#    by cluster_centers to increase the performance of the algorithm. (Refer to 
+#    GIT history for deprecated code that offered noise and preserved clusters).
+# 2. z_cluster and z_mean: I don't think we need to know the height value of the
+#    center of a cluster. We are reconstructing an unbounded cylinder at each
+#    cetner - not a sphere, so the height should not matter. That said, I believe
+#    Monash University uses it - but they're a bunch of monkeys anyways. 
+# 3. Neighbourhood scan size should be roughly the diameter of the cones that are
+#    used on the track.
