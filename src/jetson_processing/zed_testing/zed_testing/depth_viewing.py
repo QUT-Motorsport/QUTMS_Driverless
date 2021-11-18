ffc847fe15a042090db21478e2dee924bef9ff21
Alastair Bradford <77331735+b1n-ch1kn@users.noreply.github.com>
2021-11-18 12:11:00 +1000

    fixed depth nodes

--- src/jetson_processing/zed_testing/zed_testing/depth_viewing.py
+++ src/jetson_processing/zed_testing/zed_testing/depth_viewing.py
@@ -20,25 +20,33 @@ class Depth_Processing(Node):
         super().__init__('depth_processing')
         # create a quality of service profile compatible with zed node
         qos_profile = QoSProfile(depth=1)
-        qos_profile.reliability = QoSReliabilityPolicy.BEST_EFFORT
+        qos_profile.reliability = QoSReliabilityPolicy.RELIABLE
         qos_profile.durability = QoSDurabilityPolicy.VOLATILE
         qos_profile.history = QoSHistoryPolicy.KEEP_LAST
 
         # creates subscriber to 'depth' with type Image that sends data to depth_callback
         self.disp_subscription_ = self.create_subscription(
             DisparityImage,
-            '/zed2/zed_node/depth/depth_registered',
-            self.depth_callback,
+            '/zed2i/zed_node/disparity/disparity_image',
+            self.disp_callback,
             qos_profile)
         self.disp_subscription_  # prevent unused variable warning
 
+        # creates subscriber to 'depth' with type Image that sends data to depth_callback
+        self.depth_subscription_ = self.create_subscription(
+            Image,
+            '/zed2i/zed_node/depth/depth_registered',
+            self.depth_callback,
+            qos_profile)
+        self.depth_subscription_  # prevent unused variable warning
+
         self.br = CvBridge()
 
-    def depth_callback(self, disp_msg):
+    def disp_callback(self, disp_msg):
         print("received")
-        depth_img = disp_msg.image
-        current_frame =  self.br.imgmsg_to_cv2(depth_img)
-        cv2.imshow("depth camera", current_frame) # image with bounding boxes
+        disp_img = disp_msg.image
+        current_frame =  self.br.imgmsg_to_cv2(disp_img)
+        cv2.imshow("depth camera", current_frame)
         cv2.waitKey(1)
 
 
