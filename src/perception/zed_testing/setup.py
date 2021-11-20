8c096551703d1fe0f5ba33eab69c17b3cf4c42a2
Alastair Bradford <albradford2468@gmail.com>
2021-11-20 20:36:40 +1000

    got opencv bounding boxes + depth map thing working yay

--- src/perception/zed_testing/setup.py
+++ src/perception/zed_testing/setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 package_name = 'zed_testing'
+submodule_name = 'zed_testing/sub_module'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=[package_name, submodule_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
--- src/perception/zed_testing/zed_testing/depth_viewing.py
+++ src/perception/zed_testing/zed_testing/depth_viewing.py
@@ -15,6 +15,8 @@ import numpy as np
 
 from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
 
+from .sub_module.img_processing import * # import image processing func
+
 class Depth_Processing(Node):
     def __init__(self):
         super().__init__('depth_processing')
@@ -24,7 +26,15 @@ class Depth_Processing(Node):
         qos_profile.durability = QoSDurabilityPolicy.VOLATILE
         qos_profile.history = QoSHistoryPolicy.KEEP_LAST
 
-        # creates subscriber to 'depth' with type Image that sends data to depth_callback
+        # creates subscriber to 'image_rect_color' with type Image that sends data to stereo_callback
+        self.stereo_subscription_ = self.create_subscription(
+            Image,
+            '/zed2i/zed_node/stereo/image_rect_color',
+            self.stereo_callback,
+            qos_profile)
+        self.stereo_subscription_  # prevent unused variable warning
+
+        # creates subscriber to 'disparity_image' with type DisparityImage that sends data to disp_callback
         self.disp_subscription_ = self.create_subscription(
             DisparityImage,
             '/zed2i/zed_node/disparity/disparity_image',
@@ -32,21 +42,41 @@ class Depth_Processing(Node):
             qos_profile)
         self.disp_subscription_  # prevent unused variable warning
 
-        # # creates subscriber to 'depth' with type Image that sends data to depth_callback
-        # self.depth_subscription_ = self.create_subscription(
-        #     Image,
-        #     '/zed2i/zed_node/depth/depth_registered',
-        #     self.depth_callback,
-        #     qos_profile)
-        # self.depth_subscription_  # prevent unused variable warning
-
         self.br = CvBridge()
+        self.found_cones = list()
+        self.display = True
+        self.bounding_box_frame = list()
+
+
+    # callback function for camera image processing
+    def stereo_callback(self, cam_msg):
+        # get image
+        raw_frame = self.br.imgmsg_to_cv2(cam_msg)
+        self.found_cones, self.bounding_box_frame = cam_main(raw_frame)
+
 
     def disp_callback(self, disp_msg):
-        disp_img = disp_msg.image
-        current_frame =  self.br.imgmsg_to_cv2(disp_img)
-        cv2.imshow("depth camera", current_frame)
-        cv2.waitKey(1)
+        disp_img = disp_msg.image # disparity image of floats
+        focal = disp_msg.f # focal len in pixels
+        base = disp_msg.t # baseline
+
+        current_frame = self.br.imgmsg_to_cv2(disp_img, desired_encoding='32FC1')
+
+        for cone in self.found_cones:
+            disparity = current_frame[cone[1], cone[0]]
+            depth = -focal * base / disparity
+
+            string = str(round(depth, 3)) + "m"
+            cv2.putText(
+                self.bounding_box_frame, string, # frame to draw on, label to draw
+                (cone[2], cone[3]-3), # position
+                cv2.FONT_HERSHEY_SIMPLEX, 0.4, # font, font size
+                (0, 255, 0), 1) # red font colour, thickness 1
+
+        if self.display == True and self.bounding_box_frame != []:
+            # show bounding boxes
+            cv2.imshow("depth", self.bounding_box_frame) # image with bounding boxes
+            cv2.waitKey(1)
 
 
 ## main call
