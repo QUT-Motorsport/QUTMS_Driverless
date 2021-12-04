4400b6706b5cbae2b33f39d8fc6468aa39c6b1ed
Tom Fraser <tr.fraser@bigpond.com>
2021-12-04 11:10:40 +1000

    Added ros params file for ip and port configuration of the steering actuator

--- src/hardware/can_driver/src/node_steering.cpp
+++ src/hardware/can_driver/src/node_steering.cpp
@@ -1,46 +1,57 @@
+#include <algorithm>
 #include <memory>
 
-#include "rclcpp/rclcpp.hpp"
 #include "ackermann_msgs/msg/ackermann_drive.hpp"
 #include "can2etherenet_adapter.hpp"
+#include "rclcpp/rclcpp.hpp"
 #include "steering_control.hpp"
-#include <algorithm>
 
 using std::placeholders::_1;
 
-class Steering : public rclcpp::Node
-{
-private:
-std::shared_ptr<Can2Ethernet> c;
-std::shared_ptr<SteeringControl> steering;
+class Steering : public rclcpp::Node {
+   private:
+	std::shared_ptr<Can2Ethernet> c;
+	std::shared_ptr<SteeringControl> steering;
 
-void topic_callback(const ackermann_msgs::msg::AckermannDrive::SharedPtr msg) const{
-        float cappedAngle = std::fmax(std::fmin(msg->steering_angle, M_PI), -M_PI);
-        int32_t steeringDemandStepper = cappedAngle * 5000/M_PI;
+	void topic_callback(const ackermann_msgs::msg::AckermannDrive::SharedPtr msg) const {
+		float cappedAngle = std::fmax(std::fmin(msg->steering_angle, M_PI), -M_PI);
+		int32_t steeringDemandStepper = cappedAngle * 5000 / M_PI;
 
-        RCLCPP_INFO(this->get_logger(), "Stepper: %i", steeringDemandStepper);
-        RCLCPP_INFO(this->get_logger(), "Radians: %f", cappedAngle);
-        this->steering->targetPosition(steeringDemandStepper);
-        this->steering->reachedTarget();
-    }
-    rclcpp::Subscription<ackermann_msgs::msg::AckermannDrive>::SharedPtr subscription_;
+		RCLCPP_INFO(this->get_logger(), "Stepper: %i", steeringDemandStepper);
+		RCLCPP_INFO(this->get_logger(), "Radians: %f", cappedAngle);
+		this->steering->targetPosition(steeringDemandStepper);
+		this->steering->reachedTarget();
+	}
+	rclcpp::Subscription<ackermann_msgs::msg::AckermannDrive>::SharedPtr subscription_;
 
-public:
-    Steering() : Node("steering") {
-        this->subscription_ = this->create_subscription<ackermann_msgs::msg::AckermannDrive>("steering", 10, std::bind(&Steering::topic_callback, this, _1));
-        this->c = std::make_shared<Can2Ethernet>("192.168.2.8", 20005);
-        this->steering = std::make_shared<SteeringControl>(c);
-	    this->steering->setAcceleration(std::make_pair<uint32_t, uint32_t>(10000, 10000));
-    }
-    
-    ~Steering(){
-        this->steering->shutdown();
-    }
+   public:
+	Steering() : Node("steering") {
+        this->declare_parameter<std::string>("ip", "");
+		this->declare_parameter<int>("port", 0);
+		this->subscription_ = this->create_subscription<ackermann_msgs::msg::AckermannDrive>(
+			"steering", 10, std::bind(&Steering::topic_callback, this, _1));
+
+		std::string _ip;
+		int _port;
+		this->get_parameter("ip", _ip);
+		this->get_parameter("port", _port);
+
+		if(_ip == "") {
+			RCLCPP_ERROR(this->get_logger(), "Please provide a rosparam yaml file!");
+			rclcpp::shutdown();
+		}
+
+		this->c = std::make_shared<Can2Ethernet>(_ip, _port);
+		this->steering = std::make_shared<SteeringControl>(c);
+		this->steering->setAcceleration(std::make_pair<uint32_t, uint32_t>(10000, 10000));
+	}
+
+	~Steering() { this->steering->shutdown(); }
 };
 
 int main(int argc, char* argv[]) {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<Steering>());
-    rclcpp::shutdown();
-    return 0;
+	rclcpp::init(argc, argv);
+	rclcpp::spin(std::make_shared<Steering>());
+	rclcpp::shutdown();
+	return 0;
 }
