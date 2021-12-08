f0b47116a47af53864679f53574bf31b7893768b
Alistair English <alistair.english@protonmail.com>
2021-12-08 15:29:33 +1000

    add sine controller

---
+++ src/navigation/controllers/controllers/sine_steering.py
@@ -0,0 +1,36 @@
+import rclpy
+from rclpy.node import Node
+from rclpy.publisher import Publisher
+from rclpy.timer import Timer
+
+import math
+
+from ackermann_msgs.msg import AckermannDrive
+
+class SineControllerNode(Node):
+    def __init__(self):
+        super().__init__("SineController")
+
+        self.steering_publisher: Publisher = self.create_publisher(AckermannDrive, "steering", 1)
+        self.get_logger().info("Sine Controller")
+        self.create_timer(0.1, self.timer_cb)
+        self.count = 0
+    
+    def timer_cb(self):
+        self.translate = 2 * math.pi / 5
+        self.count += 0.1
+        steering_msg = AckermannDrive()
+        steering_msg.steering_angle = math.sin(self.count * self.translate) * math.pi
+        self.get_logger().info(f"angle: {steering_msg.steering_angle}")
+        self.steering_publisher.publish(steering_msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    sine_controller_node = SineControllerNode()
+
+    rclpy.spin(sine_controller_node)
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
