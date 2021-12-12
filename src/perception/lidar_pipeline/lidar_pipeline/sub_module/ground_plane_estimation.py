32a65807de59945cff86a2a8f307ff07c919f25f
liam-ferrante <liam.p.ferrante@gmail.com>
2021-12-12 22:31:48 +1000

    Revert "Small change"
    This reverts commit ec1add1bf2c6311c042b5991b5a496c3d4a30766.

--- src/perception/lidar_pipeline/lidar_pipeline/sub_module/ground_plane_estimation.py
+++ src/perception/lidar_pipeline/lidar_pipeline/sub_module/ground_plane_estimation.py
@@ -429,7 +429,7 @@ def new_cone_filter(distance: float, point_count: int) -> bool:
 FAR_X = 6 #m
 def get_cones(reconstructed_clusters: List[List], count: int) -> List[List]:
     cones: List[List] = []
-    ERROR_MARGIN = 0.85 # Constant
+    ERROR_MARGIN = 0.75 # Constant
     for i in range(len(reconstructed_clusters)):
         point_count = len(reconstructed_clusters[i])
         if point_count >= 1:
@@ -515,7 +515,7 @@ def get_ground_plane(point_cloud: List[NamedTuple], count: int) -> List[list]:
     ground_plane: List[List[List]] = line_extraction.get_ground_plane(segments_bins_prototype, NUM_SEGMENTS, NUM_BINS)
     print("get_ground_plane", time.time() - now)
 
-    #if VISUALISE: vis.plot_ground_lines_3D(segments_bins_prototype, ground_plane, False)
+    if VISUALISE: vis.plot_ground_lines_3D(segments_bins_prototype, ground_plane, False)
     #if VISUALISE: vis.plot_segments_fitted(segments_bins_prototype, ground_plane)
 
     now = time.time()
