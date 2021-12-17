28753eaec7715f1b041b9e41d5cc48c187772dba
liam-ferrante <liam.p.ferrante@gmail.com>
2021-12-17 18:50:56 +1000

    Auto stash before checking out "HEAD"

--- src/perception/lidar_pipeline/lidar_pipeline/sub_module/lidar_testing.py
+++ src/perception/lidar_pipeline/lidar_pipeline/sub_module/lidar_testing.py
@@ -11,6 +11,7 @@ Point = namedtuple('Point', ['x', 'y', 'z', 'intensity', 'ring'])
 #with open(path + "12-07-2021_05-15-02", 'rb') as f:
 #    point_cloud = pickle.load(f)
 
+<<<<<<< Updated upstream
 path: str = "C:/Users/liamf/OneDrive/QUT/QUTMS/QUTMS_Driverless/datasets/"
 # 24 
 with open(path + "08-12-2021_01-55-30.txt", 'rb') as f:
@@ -20,6 +21,17 @@ with open(path + "08-12-2021_01-55-30.txt", 'rb') as f:
 DISPLAY: bool = True
 VISUALISE: bool = True
 MAX_RANGE: int = 7
+=======
+path: str = "../datasets/"
+
+with open(path + "08-12-2021_12-36-46.txt", 'rb') as f:
+    line: str = f.readline()
+    point_cloud: List[namedtuple] = eval(line)
+
+DISPLAY: bool = False
+VISUALISE: bool = False
+MAX_RANGE: int = 6
+>>>>>>> Stashed changes
 
 start_time = time.time()
 gpe.lidar_init(DISPLAY, VISUALISE, "./datasets/figures", MAX_RANGE)
