caf8358d78b6b63791ee8d3ecd221901bc53bb48
liam-ferrante <liam.p.ferrante@gmail.com>
2021-12-20 09:51:56 +1000

    PEP8 Style Compliant

--- src/perception/lidar_pipeline_2/lidar_pipeline_2/lidar_perception.py
+++ src/perception/lidar_pipeline_2/lidar_pipeline_2/lidar_perception.py
@@ -1,15 +1,16 @@
-# Import Logging
-import logging
-LOGGER = logging.getLogger(__name__)
-
 # Import Modules
 import datetime
 import pathlib
 import os
 
+# Import Logging
+import logging
+LOGGER = logging.getLogger(__name__)
+
 # Import ROS2 Modules
-#import rclpy
-#from rclpy.node import Node
+# import rclpy
+# from rclpy.node import Node
+
 
 class ConePerceptionNode():
     def __init__(self):
@@ -24,16 +25,27 @@ def main(args=None):
         os.mkdir(path + '/logs')
 
     date = datetime.datetime.now().strftime('%d_%m_%Y_%H_%M_%S')
-    logging.basicConfig(filename=f'{path}/logs/{date}.log', filemode='w', format='%(asctime)s | %(levelname)s:%(name)s: %(message)s', datefmt='%I:%M:%S %p', encoding='utf-8', level=logging.INFO)
-    
+    logging.basicConfig(filename=f'{path}/logs/{date}.log',
+                        filemode='w',
+                        format='%(asctime)s | %(levelname)s:%(name)s: %(message)s',
+                        datefmt='%I:%M:%S %p',
+                        encoding='utf-8',
+                        level=logging.INFO)
+
     LOGGER.info('Hi from lidar_pipeline_2.')
 
-    #rclpy.init(args=args)
+    # Publisher Node
+
+    # rclpy.init(args=args)
+
     cone_perception_node = ConePerceptionNode()
 
-    #rclpy.spin(cone_perception_node)
+    # rclpy.spin(cone_perception_node)
 
-    #rclpy.shutdown()
+    # Destroy the node explicitly
+    cone_perception_node.destroy_node()
+
+    # rclpy.shutdown()
 
 
 if __name__ == '__main__':
