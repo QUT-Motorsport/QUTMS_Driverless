7bdd90de79fcbedb16b1cd4683b3e2af32c41a42
liam-ferrante <liam.p.ferrante@gmail.com>
2021-12-19 10:39:43 +1000

    Global Logger

--- src/perception/lidar_pipeline_2/lidar_pipeline_2/lidar_perception.py
+++ src/perception/lidar_pipeline_2/lidar_pipeline_2/lidar_perception.py
@@ -1,7 +1,11 @@
-# Import Modules
+# Import Logging
 import logging
+LOGGER = logging.getLogger(__name__)
+
+# Import Modules
 import datetime
 import pathlib
+import test
 
 # Import ROS2 Modules
 import rclpy
@@ -17,11 +21,9 @@ def main(args=None):
     # Setting up logging
     date = datetime.datetime.now().strftime('%d_%m_%Y_%H_%M_%S')
     path = pathlib.Path(__file__).parent.resolve()
-
     logging.basicConfig(filename=f'{path}/logs/{date}.log', filemode='w', format='%(asctime)s | %(levelname)s:%(name)s: %(message)s', datefmt='%I:%M:%S %p', encoding='utf-8', level=logging.INFO)
-    logger = logging.getLogger(__name__)
     
-    logger.info('Hi from lidar_pipeline_2.')
+    LOGGER.info('Hi from lidar_pipeline_2.')
 
     rclpy.init(args=args)
     cone_perception_node = ConePerceptionNode()
@@ -29,6 +31,8 @@ def main(args=None):
     rclpy.spin(cone_perception_node)
 
     rclpy.shutdown()
+    
+    test.main()
 
 
 if __name__ == '__main__':
