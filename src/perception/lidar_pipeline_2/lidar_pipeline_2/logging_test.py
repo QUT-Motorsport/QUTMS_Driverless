fca57f353754cfaf1918f889ef44d1224f9327d6
liam-ferrante <liam.p.ferrante@gmail.com>
2021-12-20 12:39:46 +1000

    Testing

--- src/perception/lidar_pipeline_2/lidar_pipeline_2/logging_test.py
+++
@@ -1,9 +0,0 @@
-import logging
-import pathlib
-path = pathlib.Path(__file__).parent.resolve()
-
-logging.basicConfig(filename=f'{path}/example.log', filemode='w', encoding='utf-8', level=logging.DEBUG)
-logging.debug('This message should go to the log file')
-logging.info('So should this')
-logging.warning('And this, too')
-logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
