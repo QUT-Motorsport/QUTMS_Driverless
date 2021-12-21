ffa7a3896a6ea9c3732f2c40f4d14d8df6aca07d
liam-ferrante <liam.p.ferrante@gmail.com>
2021-12-21 12:22:45 +1000

    Setting up constants and accepting flags

--- src/perception/lidar_pipeline_2/lidar_pipeline_2/library/lidar_manager.py
+++ src/perception/lidar_pipeline_2/lidar_pipeline_2/library/lidar_manager.py
@@ -13,15 +13,19 @@ import logging
 LOGGER = logging.getLogger(__name__)
 
 
-def detect_cones(point_cloud, print_logs, stdout_handler=None):
+def detect_cones(point_cloud, print_logs, LIDAR_RANGE, DELTA_ALPHA, BIN_SIZE, stdout_handler=None):
     # Printing logs to terminal
     if print_logs:
         LOGGER.addHandler(stdout_handler)
 
     LOGGER.info("Hi from lidar_manager.")
 
+    # Derived Constants
+    SEGMENT_COUNT = math.ceil(2 * math.pi / DELTA_ALPHA)
+    BIN_COUNT = math.ceil(LIDAR_RANGE / BIN_SIZE)
+
     start_time = time.time()
-    pc_discretised = pcp.discretise_point_cloud(point_cloud, 2*math.pi / 128, 0.14)
+    pc_discretised = pcp.discretise_point_cloud(point_cloud, DELTA_ALPHA, BIN_SIZE, SEGMENT_COUNT, BIN_COUNT)
     end_time = time.time()
 
     LOGGER.info(f'Numpy PointCloud discretised in {end_time - start_time}s')
