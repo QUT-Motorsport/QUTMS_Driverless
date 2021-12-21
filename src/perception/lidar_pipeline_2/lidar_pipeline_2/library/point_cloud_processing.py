ee0d89b0b3757f3dc8283b2646663d6a6478f78b
liam-ferrante <liam.p.ferrante@gmail.com>
2021-12-21 12:07:47 +1000

    Created point_cloud_processing.py

---
+++ src/perception/lidar_pipeline_2/lidar_pipeline_2/library/point_cloud_processing.py
@@ -0,0 +1,21 @@
+# Import Helper Modules
+import numpy as np
+
+# Import Python Modules
+import math
+
+# Import Logging
+import logging
+LOGGER = logging.getLogger(__name__)
+
+
+def discretise_point_cloud(point_cloud, DELTA_ALPHA, BIN_SIZE, SEGMENT_COUNT, BIN_COUNT):
+    # Calculating the segment index for each point
+    segments_idx = np.arctan(point_cloud['y'] / point_cloud['x']) // DELTA_ALPHA
+
+    # Calculating the bin index for each point
+    bins_idx = np.linalg.norm([point_cloud['x'], point_cloud['y']], axis=0) // BIN_SIZE
+
+    segments_bins = 
+
+    return segments_idx
