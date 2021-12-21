011be9563a3602dd745d84a790b6d8467f6851a0
liam-ferrante <liam.p.ferrante@gmail.com>
2021-12-21 22:33:23 +1000

    Detailed comments

--- src/perception/lidar_pipeline_2/lidar_pipeline_2/library/point_cloud_processing.py
+++ src/perception/lidar_pipeline_2/lidar_pipeline_2/library/point_cloud_processing.py
@@ -25,14 +25,24 @@ def get_prototype_points(seg_bin_nrm, SEGMENT_COUNT, BIN_COUNT, POINT_COUNT):
     # Split seg_bin_nrm into sub arrays for each unique segment
     split_bin_nrm = np.split(seg_bin_nrm, np.where(np.diff(seg_bin_nrm[:, 0]))[0] + 1)
 
-    segments_approx = [[[] for j in range(BIN_COUNT)] for i in range(SEGMENT_COUNT)]
-    for i in range(len(split_bin_nrm)):
-        split_split_nrm = np.split(split_bin_nrm[i], np.where(np.diff(split_bin_nrm[i][:, 1]))[0] + 1)
-        for j in range(len(split_split_nrm)):
-            min_height_idx = np.argsort(split_split_nrm[j][:, 3])[0]
-            segments_approx[int(split_split_nrm[j][0, 0])][int(split_split_nrm[j][0, 1])] = [split_split_nrm[j][min_height_idx, 2], split_split_nrm[j][min_height_idx, 3]]
+    # For every bin in every each segment a prototype point may be assigned
+    prototype_points = [[[] for j in range(BIN_COUNT)] for i in range(SEGMENT_COUNT)]
 
-    return segments_approx
+    # For each array in split_bin_nrm representing a unique segment
+    for i in range(len(split_bin_nrm)):
+        # Split split_bin_nrm[i] into sub arrays for each unique bin
+        split_split_nrm = np.split(split_bin_nrm[i], np.where(np.diff(split_bin_nrm[i][:, 1]))[0] + 1)
+
+        # For each array in split_split_nrm representing a unique bin
+        for j in range(len(split_split_nrm)):
+            # Find index of point with lowest height in current bin and segment
+            min_height_idx = np.argsort(split_split_nrm[j][:, 3])[0]
+
+            # For the prototype points nested index location current segment
+            # and bin, set value to be [norm, height] of point at index above
+            prototype_points[int(split_split_nrm[j][0, 0])][int(split_split_nrm[j][0, 1])] = [split_split_nrm[j][min_height_idx, 2], split_split_nrm[j][min_height_idx, 3]]
+
+    return prototype_points
 
 # Notes
 # 1. For get_prototype_points() if needed you can revert back to using a numpy
