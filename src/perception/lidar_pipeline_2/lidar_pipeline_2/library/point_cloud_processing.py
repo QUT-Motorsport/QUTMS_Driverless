905471c5fa3a6c3d5b26a6ac0b9a7575cd316c4b
liam-ferrante <liam.p.ferrante@gmail.com>
2021-12-21 19:22:07 +1000

    Created function using numpy operations

--- src/perception/lidar_pipeline_2/lidar_pipeline_2/library/point_cloud_processing.py
+++ src/perception/lidar_pipeline_2/lidar_pipeline_2/library/point_cloud_processing.py
@@ -1,20 +1,24 @@
 # Import Helper Modules
 import numpy as np
 
-# Import Logging
-import logging
+import time
 
-from numpy.lib.function_base import copy
-LOGGER = logging.getLogger(__name__)
+from numpy.core.fromnumeric import shape
 
 
-def discretise_point_cloud(point_cloud, DELTA_ALPHA, BIN_SIZE):
+def get_discretised_positions(point_cloud, DELTA_ALPHA, BIN_SIZE):
     # Calculating the segment index for each point
     segments_idx = np.arctan(point_cloud['y'] / point_cloud['x']) / DELTA_ALPHA
     np.nan_to_num(segments_idx, copy=False, nan=((np.pi / 2) / DELTA_ALPHA))  # Limit arctan x->inf = pi/2
 
     # Calculating the bin index for each point
-    bins_idx = np.linalg.norm([point_cloud['x'], point_cloud['y']], axis=0) / BIN_SIZE
-    np.nan_to_num(bins_idx, copy=False, nan=1)
+    point_norms = np.linalg.norm([point_cloud['x'], point_cloud['y']], axis=0)
+    bins_idx = point_norms / BIN_SIZE
+    np.nan_to_num(bins_idx, copy=False, nan=0)
 
-    return [segments_idx, bins_idx]
+    return np.column_stack((segments_idx.astype(int, copy=False), bins_idx.astype(int, copy=False), point_norms))
+
+
+def get_prototype_points(segments_bins_norms, SEGMENT_COUNT, BIN_COUNT, POINT_COUNT):
+    pass
+    return []
