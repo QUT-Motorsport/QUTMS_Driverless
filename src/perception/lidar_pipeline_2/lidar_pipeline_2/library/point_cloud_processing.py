e6cdf4eb08b1ac9231d21794b1c1097153fb8c6d
liam-ferrante <liam.p.ferrante@gmail.com>
2021-12-22 10:58:27 +1000

    Improved logic, code, comments

--- src/perception/lidar_pipeline_2/lidar_pipeline_2/library/point_cloud_processing.py
+++ src/perception/lidar_pipeline_2/lidar_pipeline_2/library/point_cloud_processing.py
@@ -2,14 +2,10 @@
 import numpy as np
 
 
-def get_discretised_positions(point_cloud, DELTA_ALPHA, BIN_SIZE):
+def get_discretised_positions(point_cloud, point_norms, DELTA_ALPHA, BIN_SIZE):
     # Calculating the segment index for each point
     segments_idx = np.arctan(point_cloud['y'] / point_cloud['x']) / DELTA_ALPHA
     np.nan_to_num(segments_idx, copy=False, nan=((np.pi / 2) / DELTA_ALPHA))  # Limit arctan x->inf = pi/2
-
-    # Calculating the normal of each point
-    point_norms = np.linalg.norm([point_cloud['x'], point_cloud['y']], axis=0)
-    np.nan_to_num(point_norms, copy=False, nan=0)
 
     # Calculating the bin index for each point
     bins_idx = point_norms / BIN_SIZE
@@ -18,33 +14,29 @@ def get_discretised_positions(point_cloud, DELTA_ALPHA, BIN_SIZE):
     return np.column_stack((segments_idx.astype(int, copy=False), bins_idx.astype(int, copy=False), point_norms, point_cloud['z']))
 
 
-def get_prototype_points(seg_bin_nrm, SEGMENT_COUNT, BIN_COUNT, POINT_COUNT):
+def get_prototype_points(seg_bin_nrm_z, SEGMENT_COUNT, BIN_COUNT, POINT_COUNT):
     # Sorting norms in descending order by segment idx then bin idx
-    seg_bin_nrm = seg_bin_nrm[np.lexsort((seg_bin_nrm[:, 1], seg_bin_nrm[:, 0]))]
+    seg_bin_nrm_z = seg_bin_nrm_z[np.lexsort((seg_bin_nrm_z[:, 1], seg_bin_nrm_z[:, 0]))]
 
     # Split seg_bin_nrm into sub arrays for each unique segment
-    split_bin_nrm = np.split(seg_bin_nrm, np.where(np.diff(seg_bin_nrm[:, 0]))[0] + 1)
+    split_bin_nrm_z = np.split(seg_bin_nrm_z, np.where(np.diff(seg_bin_nrm_z[:, 0]))[0] + 1)
 
     # For every bin in every each segment a prototype point may be assigned
     prototype_points = [[[] for j in range(BIN_COUNT)] for i in range(SEGMENT_COUNT)]
 
     # For each array in split_bin_nrm representing a unique segment
-    for i in range(len(split_bin_nrm)):
+    for i in range(len(split_bin_nrm_z)):
         # Split split_bin_nrm[i] into sub arrays for each unique bin
-        split_split_nrm = np.split(split_bin_nrm[i], np.where(np.diff(split_bin_nrm[i][:, 1]))[0] + 1)
+        split_split_nrm_z = np.split(split_bin_nrm_z[i], np.where(np.diff(split_bin_nrm_z[i][:, 1]))[0] + 1)
 
         # For each array in split_split_nrm representing a unique bin
-        for j in range(len(split_split_nrm)):
+        for j in range(len(split_split_nrm_z)):
             # Find index of point with lowest height in current bin and segment
-            min_height_idx = np.argsort(split_split_nrm[j][:, 3])[0]
+            min_height_idx = np.argsort(split_split_nrm_z[j][:, 3])[0]
 
-            # For the prototype points nested index location current segment
-            # and bin, set value to be [norm, height] of point at index above
-            print(len(prototype_points), len(prototype_points[0]))
-            print(i, j, int(split_split_nrm[j][0, 0]), int(split_split_nrm[j][0, 1]))
-            print(split_split_nrm[j])
-            print(prototype_points[int(split_split_nrm[j][0, 0])][int(split_split_nrm[j][0, 1])])
-            prototype_points[int(split_split_nrm[j][0, 0])][int(split_split_nrm[j][0, 1])] = [split_split_nrm[j][min_height_idx, 2], split_split_nrm[j][min_height_idx, 3]]
+            # Set pos corresponding to current segment and bin in nested array
+            # to [norm, height] of point corresponding to min_height_idx above
+            prototype_points[int(split_split_nrm_z[j][0, 0])][int(split_split_nrm_z[j][0, 1])] = [split_split_nrm_z[j][min_height_idx, 2], split_split_nrm_z[j][min_height_idx, 3]]
 
     return prototype_points
 
