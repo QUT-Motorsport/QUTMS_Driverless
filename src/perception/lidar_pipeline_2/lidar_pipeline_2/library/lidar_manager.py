70836b73ac33bd260730b4f81c34a5dc7ab37ae5
liam-ferrante <liam.p.ferrante@gmail.com>
2021-12-22 10:59:03 +1000

    Smartly removing out of range points by computed necessary norms

--- src/perception/lidar_pipeline_2/lidar_pipeline_2/library/lidar_manager.py
+++ src/perception/lidar_pipeline_2/lidar_pipeline_2/library/lidar_manager.py
@@ -13,7 +13,7 @@ import logging
 LOGGER = logging.getLogger(__name__)
 
 
-def detect_cones(point_cloud, print_logs, LIDAR_RANGE, DELTA_ALPHA, BIN_SIZE, POINT_COUNT, stdout_handler=None):
+def detect_cones(point_cloud, point_norms, print_logs, LIDAR_RANGE, DELTA_ALPHA, BIN_SIZE, POINT_COUNT, stdout_handler=None):
     # Printing logs to terminal
     if print_logs:
         LOGGER.addHandler(stdout_handler)
@@ -25,13 +25,13 @@ def detect_cones(point_cloud, print_logs, LIDAR_RANGE, DELTA_ALPHA, BIN_SIZE, PO
     BIN_COUNT = math.ceil(LIDAR_RANGE / BIN_SIZE)
 
     start_time = time.time()
-    segments_bins_norms = pcp.get_discretised_positions(point_cloud, DELTA_ALPHA, BIN_SIZE)
+    segments_bins_norms_z = pcp.get_discretised_positions(point_cloud, point_norms, DELTA_ALPHA, BIN_SIZE)
     end_time = time.time()
 
     LOGGER.info(f'Numpy PointCloud discretised in {end_time - start_time}s')
 
     start_time = time.time()
-    prototype_points = pcp.get_prototype_points(segments_bins_norms, SEGMENT_COUNT, BIN_COUNT, POINT_COUNT)
+    prototype_points = pcp.get_prototype_points(segments_bins_norms_z, SEGMENT_COUNT, BIN_COUNT, POINT_COUNT)
     end_time = time.time()
 
     LOGGER.info(f'Prototype Points computed in {end_time - start_time}s')
