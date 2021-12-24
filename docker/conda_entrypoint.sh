db1a9d51bba5699b9b6445e30744dcfbd43e570b
Alistair English <alistair.english@protonmail.com>
2021-12-24 17:46:21 +1000

    conda weird

--- docker/conda_entrypoint.sh
+++ docker/conda_entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-set -e
+
+# apparently `set -e` ("strict mode") breaks conda activate, see:
+# https://pythonspeed.com/articles/activate-conda-dockerfile/#another-working-solution
 
 source "/home/developer/mambaforge/bin/activate"
-conda activate driverless_env
+conda activate driverless_env   
+
 exec "$@"
