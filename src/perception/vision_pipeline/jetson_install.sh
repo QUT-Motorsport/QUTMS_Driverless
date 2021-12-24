28da262913568bb7294c5ae865eafc7fe4fb52ec
Alistair English <alistair.english@protonmail.com>
2021-12-24 16:47:55 +1000

    -y

--- src/perception/vision_pipeline/jetson_install.sh
+++ src/perception/vision_pipeline/jetson_install.sh
@@ -3,5 +3,6 @@ conda activate driverless_env
 
 # REMEMBER to update deps in conda_requirements as well
 # (these deps are the same as conda_requirements with the exception of pytorch and torchvision)
-mamba install opencv==4.5.2
-mamba install requests==2.26.0
+mamba install -y \
+    opencv==4.5.2 \
+    requests==2.26.0
