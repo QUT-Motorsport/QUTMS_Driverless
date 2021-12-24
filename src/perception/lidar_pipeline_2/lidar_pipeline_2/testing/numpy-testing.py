bbd3f7db0fa1aa25be02746ac2480944573b5954
liam-ferrante <liam.p.ferrante@gmail.com>
2021-12-25 09:44:34 +1000

    More testing

--- src/perception/lidar_pipeline_2/lidar_pipeline_2/testing/numpy-testing.py
+++ src/perception/lidar_pipeline_2/lidar_pipeline_2/testing/numpy-testing.py
@@ -3,11 +3,19 @@ from numpy.lib.function_base import copy
 
 
 def test():
-    point_cloud = np.array([(1.34255, 2.6543345, 3.65433456), (2.2345, 3.3245, 2.7645), (2.436, 4.73, 5.5)], dtype=[('x', 'float'), ('y', 'float'), ('z', 'float')])
+    point_cloud = np.array([(2.2, 3.3, 1.1), (1.1, 1.1, 2.2), (3.3, 2.2, 3.3)], dtype=[('x', 'float'), ('y', 'float'), ('z', 'float')])
     print(point_cloud[[0, 1, 2]])
     a = np.array([[1, 2], [3, 4]])
     b = np.array([[5, 6]])
     a = np.concatenate((a, b), axis=0)
+    
+    hi = np.array([[2, 9], [2, 9], [3, 5], [2, 7], [3, 3], [2, 7]])
+    print(hi)
+    un, idx = np.unique(hi, return_index=True, axis=0)
+    print('YAS', un)
+    print('YAS', idx)
+    # print('YAS', idx)
+    # print('NAN', hi[idx])
     
     print(a)
 
