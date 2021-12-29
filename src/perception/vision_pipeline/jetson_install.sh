de6013ffd9340ae2583b5b65edeafe2105ac8450
Alistair English <alistair.english@protonmail.com>
2021-12-30 09:22:32 +1000

    change jetson install script to build from source

--- src/perception/vision_pipeline/jetson_install.sh
+++ src/perception/vision_pipeline/jetson_install.sh
@@ -13,17 +13,16 @@ apt-get update && apt-get install -y --no-install-recommends \
     libavformat-dev \
     libswscale-dev
 
-cd /home/developer
-wget https://nvidia.box.com/shared/static/fjtbno0vpo676a25cgvuqc1wty0fkkg6.whl \
-    -O torch-1.10.0-cp36-cp36m-linux_aarch64.whl
-pip3 install torch-1.10.0-cp36-cp36m-linux_aarch64.whl
+git clone --recursive --branch v1.9.1 http://github.com/pytorch/pytorch
+cd pytorch
+mamba install --file requirements.txt
+python3 setup.py install
 
 git clone --branch v0.11.1 https://github.com/pytorch/vision torchvision
-
 cd torchvision
-export BUILD_VERSION=0.11.1;
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64 ${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}};
-python3 setup.py install --user
+export BUILD_VERSION=0.11.1
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64 ${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+python3 setup.py install
 
 # REMEMBER to update deps in conda_requirements as well
 # (these deps are the same as conda_requirements with the exception of pytorch and torchvision)
@@ -34,4 +33,4 @@ mamba install -y \
     requests==2.26.0 \
     tqdm>=4.41.0 \
     pandas>=1.1.4 \
-    seaborn>=0.11.0 \
+    seaborn>=0.11.0
