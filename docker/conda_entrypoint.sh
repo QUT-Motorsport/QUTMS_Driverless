ba1e81716b96414af8ea0cb5fbd0a80fc62e16f9
Alistair English <alistair.english@protonmail.com>
2022-01-06 20:49:20 +1000

    change entrypoints to source workspace if available

--- docker/conda_entrypoint.sh
+++ docker/conda_entrypoint.sh
@@ -6,4 +6,7 @@
 source "/home/developer/mambaforge/bin/activate"
 conda activate driverless_env
 
+# source workspace setup if available
+test -f "/home/developer/driverless_ws/install/setup.bash" && source "/home/developer/driverless_ws/install/setup.bash"
+
 exec "$@"
--- docker/galactic_entrypoint.sh
+++ docker/galactic_entrypoint.sh
@@ -3,4 +3,8 @@ set -e
 
 # setup ros2 environment
 source "/opt/ros/galactic/install/setup.bash"
+
+# source workspace setup if available
+test -f "/home/developer/driverless_ws/install/setup.bash" && source "/home/developer/driverless_ws/install/setup.bash"
+
 exec "$@"
