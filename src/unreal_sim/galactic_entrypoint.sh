c28a302fca01d3d59e19f1d04623c5ad907cc5f3
Alastair Bradford <albradford2468@gmail.com>
2022-01-22 11:35:22 +1000

    removed sim_base, fixed galactic entry

--- src/unreal_sim/galactic_entrypoint.sh
+++ src/unreal_sim/galactic_entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # setup ros2 environment
-source "/opt/ros/galactic/install/setup.bash"
+source "/opt/ros/galactic/setup.bash"
 
 # source workspace setup if available
 test -f "/home/developer/driverless_ws/install/setup.bash" && source "/home/developer/driverless_ws/install/setup.bash"
