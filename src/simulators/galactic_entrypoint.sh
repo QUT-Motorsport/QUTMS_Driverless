b07935431c2da2bd5b1d6cf925950717c9e3c3b5
Alastair Bradford <albradford2468@gmail.com>
2022-01-24 18:00:36 +1000

    renamed "unreal_sim" folder, added EUFS sim + container

--- src/simulators/galactic_entrypoint.sh
+++ src/simulators/galactic_entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 # setup ros2 environment
 source "/opt/ros/galactic/setup.bash"
+export "EUFS_MASTER=/home/developer/driverless_ws/src/simulators"
 
 # source workspace setup if available
 test -f "/home/developer/driverless_ws/install/setup.bash" && source "/home/developer/driverless_ws/install/setup.bash"
