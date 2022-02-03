0cda1130984790244ad60c7b767593f8ab63ca88
Alastair Bradford <albradford2468@gmail.com>
2022-02-03 18:35:51 +1000

    added scipy to conda, commented spline code

--- src/navigation/spline_test/spline_test/node_spline.py
+++ src/navigation/spline_test/spline_test/node_spline.py
@@ -1,44 +1,55 @@
 
+# import ROS2 libraries
 import rclpy
 from rclpy.node import Node
 from rclpy.publisher import Publisher
-
+from cv_bridge import CvBridge
+# import ROS2 message libraries
 from sensor_msgs.msg import Image
 from ackermann_msgs.msg import AckermannDrive
+# import custom message libraries
 from driverless_msgs.msg import Cone, ConeDetectionStamped
 
-from cv_bridge import CvBridge
+# translate ROS image messages to OpenCV
+cv_bridge = CvBridge()
 
+# other python libraries
 from math import sqrt, atan2, pi, sin, cos
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-import scipy.interpolate as scipy_interpolate
+import matplotlib.pyplot as plt # plotting splines
+import scipy.interpolate as scipy_interpolate # for spline calcs
 from typing import Tuple, List, Optional
 import time
 
+# import required sub modules
 from .point import Point
 
-Colour = Tuple[int, int, int]
 
-cv_bridge = CvBridge()
-
+# image display geometry
 SCALE = 20
-WIDTH = 20*SCALE  # 10m either side
-HEIGHT = 20*SCALE  # 20m forward
-
+WIDTH = 20*SCALE # 10m either side
+HEIGHT = 20*SCALE # 20m forward
 ORIGIN = Point(0, 0)
 IMG_ORIGIN = Point(int(WIDTH/2), HEIGHT)
 
-YELLOW_DISP_COLOUR: Colour = (0, 255, 255)  # bgr - yellow
-BLUE_DISP_COLOUR: Colour = (255, 0, 0)  # bgr - blue
-ORANGE_DISP_COLOUR: Colour = (0, 165, 255)  # bgr - orange
+# display colour constants
+Colour = Tuple[int, int, int]
+YELLOW_DISP_COLOUR: Colour = (0, 255, 255) # bgr - yellow
+BLUE_DISP_COLOUR: Colour = (255, 0, 0) # bgr - blue
+ORANGE_DISP_COLOUR: Colour = (0, 165, 255) # bgr - orange
 
 LEFT_CONE_COLOUR = Cone.BLUE
 RIGHT_CONE_COLOUR = Cone.YELLOW
 
 
 def robot_pt_to_img_pt(x: float, y: float) -> Point:
+    """
+    Converts a relative depth from the camera into image coords
+    * param x: x coord
+    * param y: y coord
+    * return: Point int pixel coords
+    """
     return Point(
         int(round(WIDTH/2 - y*SCALE)),
         int(round(HEIGHT - x*SCALE)),
@@ -56,35 +67,45 @@ def cone_to_point(cone: Cone) -> Point:
     )
 
 
-def approximate_b_spline_path(x: list, y: list, n_path_points: int,
-                              degree: int = 3) -> tuple:
+def approximate_b_spline_path(
+    x: list, 
+    y: list, 
+    n_path_points: int,
+    degree: int = 3
+) -> Tuple[list, list]:
     """
-    approximate points with a B-Spline path
-
-    :param x: x position list of approximated points
-    :param y: y position list of approximated points
-    :param n_path_points: number of path points
-    :param degree: (Optional) B Spline curve degree
-    :return: x and y position list of the result path
+    ADAPTED FROM: https://github.com/AtsushiSakai/PythonRobotics/blob/master/PathPlanning/BSplinePath/bspline_path.py \n
+    Approximate points with a B-Spline path
+    * param x: x position list of approximated points
+    * param y: y position list of approximated points
+    * param n_path_points: number of path points
+    * param degree: (Optional) B Spline curve degree
+    * return: x and y position list of the result path
     """
-    t = range(len(x))
-    x_tup = scipy_interpolate.splrep(t, x, k=degree)
-    y_tup = scipy_interpolate.splrep(t, y, k=degree)
 
-    x_list = list(x_tup)
+    t: int = range(len(x))
+    # interpolate for the length of the input cone list
+    x_list = list(scipy_interpolate.splrep(t, x, k=degree))
+    y_list = list(scipy_interpolate.splrep(t, y, k=degree))
+
+    # add 4 'zero' components to align matrices
     x_list[1] = x + [0.0, 0.0, 0.0, 0.0]
-
-    y_list = list(y_tup)
     y_list[1] = y + [0.0, 0.0, 0.0, 0.0]
 
     ipl_t = np.linspace(0.0, len(x) - 1, n_path_points)
-    rx = scipy_interpolate.splev(ipl_t, x_list)
-    ry = scipy_interpolate.splev(ipl_t, y_list)
+    spline_x = scipy_interpolate.splev(ipl_t, x_list)
+    spline_y = scipy_interpolate.splev(ipl_t, y_list)
 
-    return rx, ry
+    return spline_x, spline_y
 
 
 def midpoint(p1: list, p2: list):
+    """
+    Retrieve midpoint between two points 
+    * param p1: [x,y] coords of point 1
+    * param p2: [x,y] coords of point 2
+    * return: x,y tuple of midpoint coord
+    """
     return (p1[0]+p2[0])/2, (p1[1]+p2[1])/2
 
 
@@ -102,6 +123,7 @@ class SimpleControllerNode(Node):
 
         self.get_logger().info("Simple Controller Node Initalised")
 
+        # instance var so plot figure isn't recreated each loop
         self.fig = plt.figure()
 
 
@@ -111,10 +133,11 @@ class SimpleControllerNode(Node):
 
         cones: List[Cone] = msg.cones
 
+        # create black image
         debug_img = np.zeros((HEIGHT, WIDTH, 3), dtype=np.uint8)
+
         yellow_list: List[float] = []
         blue_list: List[float] = []
-
         for cone in cones:
             if cone.color == Cone.YELLOW:
                 colour = YELLOW_DISP_COLOUR
@@ -125,6 +148,7 @@ class SimpleControllerNode(Node):
             else:
                 colour = (255, 255, 255)
             
+            # draws location of cone w/ colour
             cv2.drawMarker(
                 debug_img, 
                 robot_pt_to_img_pt(cone.location.x, cone.location.y).to_tuple(),
@@ -138,31 +162,38 @@ class SimpleControllerNode(Node):
         start: float = time.time()
 
         yellow_x: List[float] = []
-        yellow_y = []
-        blue_x = []
-        blue_y = []
-        tx = []
-        ty = []
+        yellow_y: List[float] = []
+        blue_x: List[float] = []
+        blue_y: List[float] = []
+        tx: List[float] = []
+        ty: List[float] = []
+        n_spline_pts: int = 100
 
+        # can't interpolate with less than 2 points
         if len(yellow_list) > 1 and len(blue_list) > 1:
+            # indice degree of interpolation determined by num cones
             y_degree = len(yellow_list)-1
             b_degree = len(blue_list)-1
-
+            # cubic (3rd degree) maximum
             if y_degree > 3: y_degree = 3
             if b_degree > 3: b_degree = 3
 
+            # sort by closest cones to join waypoints
             yellow_sort = sorted(yellow_list, key=lambda x: (x[0],x[1]))
             blue_sort = sorted(blue_list, key=lambda x: (x[0],x[1]))
-            for i in yellow_sort:
-                yellow_x.append(-i[1])
+            for i in yellow_sort: # each cone coord
+                # ref frame so x is forward from the car (this is graph axis y)
+                yellow_x.append(-i[1]) # negative because we mixed up yellow/blue track sides
                 yellow_y.append(i[0])
             for i in blue_sort:
                 blue_x.append(-i[1])
                 blue_y.append(i[0])
-            yx, yy = approximate_b_spline_path(yellow_x, yellow_y, 100, degree=y_degree)
-            bx, by = approximate_b_spline_path(blue_x, blue_y, 100, degree=b_degree)
+            # retrieves spline lists (x,y)
+            yx, yy = approximate_b_spline_path(yellow_x, yellow_y, n_spline_pts, degree=y_degree)
+            bx, by = approximate_b_spline_path(blue_x, blue_y, n_spline_pts, degree=b_degree)
 
-            for i in range(100):
+            # find midpoint between splines at each point to make target path
+            for i in range(n_spline_pts):
                 mid_x, mid_y = midpoint([yx[i], yy[i]], [bx[i], by[i]])
                 tx.append(mid_x)
                 ty.append(mid_y)
@@ -207,8 +238,10 @@ class SimpleControllerNode(Node):
                 y=closest_right.location.y + 2,
             )
         
+        # overwrite target if there was a spline target path
+        # uses the 2 closest method if not
         if tx != []:
-            target = Point(ty[50], -tx[50])
+            target = Point(ty[30], -tx[30])
 
 
         if target is not None:
@@ -248,7 +281,7 @@ class SimpleControllerNode(Node):
             plt.plot(yx, yy, '-y')
             plt.plot(bx, by, '-b')
             plt.plot(tx, ty, '-r')
-            plt.plot(tx[50], ty[50], 'or')
+            plt.plot(tx[30], ty[30], 'or')
             plt.grid(True)
             plt.axis("equal")
 
