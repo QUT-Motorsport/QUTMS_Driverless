a3f2ad823fc4cdfdc0b327414bf22076e7229576
Alastair Bradford <albradford2468@gmail.com>
2022-02-23 23:21:23 +1000

    why tf is depth map 2x further than real

--- src/perception/vision_pipeline/vision_pipeline/node_zed_simulator.py
+++ src/perception/vision_pipeline/vision_pipeline/node_zed_simulator.py
@@ -1,9 +1,18 @@
 # import ROS2 libraries
 import rclpy
 from rclpy.node import Node
+from cv_bridge import CvBridge
 # import ROS2 message libraries
 from sensor_msgs.msg import Image, CameraInfo
 from rclpy.publisher import Publisher
+
+# other python libraries
+import cv2
+import numpy as np
+
+# translate ROS image messages to OpenCV
+cv_bridge = CvBridge()
+
 
 class ZedNode(Node):
     def __init__(self):
@@ -26,7 +35,11 @@ class ZedNode(Node):
     
 
     def depth_callback(self, depth_msg: Image):
-        self.depth_msg = depth_msg
+        depth_frame: np.ndarray = cv_bridge.imgmsg_to_cv2(depth_msg, desired_encoding='32FC1')
+        # for some reason, depth is 2x in the depth camera
+        new_depth = np.divide(depth_frame, 2)
+        self.depth_msg = cv_bridge.cv2_to_imgmsg(new_depth, encoding="32FC1")
+        
     
     def rgb_callback(self, colour_msg: Image):
         self.camera_info.header = colour_msg.header
@@ -35,7 +48,7 @@ class ZedNode(Node):
         self.rgb_img_info_publisher.publish(self.camera_info)
         self.rgb_img_publisher.publish(colour_msg)
         self.depth_img_publisher.publish(self.depth_msg)
-
+        self.get_logger().info("Published RBG, Info, Depth")
 
 
 def main(args=None):
