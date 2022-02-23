e9f7cee81e114ea64ee8666ef03dbc825a108119
Liam Ferrante <liam.p.ferrante@gmail.com>
2022-02-24 09:39:30 +1000

    Space

--- src/perception/lidar_pipeline_2/lidar_pipeline_2/library/ground_plane_estimator.py
+++ src/perception/lidar_pipeline_2/lidar_pipeline_2/library/ground_plane_estimator.py
@@ -170,7 +170,7 @@ def get_ground_lines_3(seg_proto_points, T_M, T_M_SMALL, T_B, T_RMSE, REGRESS_BE
         idx += 1
 
     if len(new_line_points) > 1 and m_new != None and b_new != None:
-        estimated_lines.append((m_new, b_new, new_line_points[0][0],new_line_points[0][1], new_line_points[len(new_line_points) - 1][0], new_line_points[len(new_line_points) - 1][1], len(new_line_points)))
+        estimated_lines.append((m_new, b_new, new_line_points[0][0], new_line_points[0][1], new_line_points[len(new_line_points) - 1][0], new_line_points[len(new_line_points) - 1][1], len(new_line_points)))
 
     return estimated_lines
 
