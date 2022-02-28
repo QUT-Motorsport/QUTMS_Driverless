50d2a0211a22383c20f903c72ca06af73edaea20
Lachie Masson <lachlanmasson@gmail.com>
2022-02-28 16:57:37 +1000

    Added Dynamic model as well as added coefficients. Unsure about a couple see notes in model.yaml

--- src/navigation/MPC/MPC.py
+++ src/navigation/MPC/MPC.py
@@ -3,69 +3,23 @@
 # Importing some libraries
 import time
 import turtle
-from utils.SystemModels import KinematicBicycleModel
+from utils.SystemModels import *
 
 # Model Predictive Controller class
 class MPC:
 
 	# Init
 	def __init__(self):
+		raise NotImplementedError()
 
-		self.vehicle = KinematicBicycleModel()
+	# Predict
+	def predict(self):
+		raise NotImplementedError()
 
-		self.s = turtle.getscreen()
-		self.t = turtle.Turtle()
-		#self.t.penup()
-
-		self.v = 0
-		self.psi = 0
-
-		#self.vehicle.GetNextState(2,-5)
-
-		self.Predict()
-
-	def f(self):
-		self.v = self.v + 1
-  
-	def b(self):
-   		self.v = self.v - 1
-	  
-	def l(self):
-	    self.psi = self.psi - 1
-	  
-	def r(self):
-	    self.psi = self.psi + 1
-
-	def Predict(self):
-
-		# To do: 
-		# get next track point 
-		# loop through different optimised options for input velocity and rate of steering change
-		# return new state for car
-
-		while(True):
-
-			# getting key press
-			self.s.listen()
-			self.s.onkeypress(self.f, "Up")  # when up is pressed pen will go up
-			self.s.onkeypress(self.b, "Down")  # when down is pressed pen will go down
-			self.s.onkeypress(self.l, "Left")  # when left is pressed pen will go left
-			self.s.onkeypress(self.r, "Right")  # when right is pressed pen will go right
-
-			# getting state change
-			self.vehicle.GetNextState(self.v,self.psi)
-
-			# setting heading 
-			self.t.setheading(self.vehicle.theta)
-			self.t.goto(self.vehicle.x, self.vehicle.y)
-
-			time.sleep(0.5)
-
-
-
-# Main 
+# Main
 def main():
-	mpc = MPC()
+	DyB = DynamicBicycleModel()
+	DyB.GetNextState(10,1)
 
 if __name__ == "__main__":
 	main() 
--- src/navigation/MPC/utils/SystemModels.py
+++ src/navigation/MPC/utils/SystemModels.py
@@ -1,22 +1,29 @@
-# This is a library that contains the various kinematic and dynamic models used within MPC.py 
+# Contains the Kinematic bicycle model and Dynamic bicycle model
 
 import yaml
 import numpy as np
-# class that represents the 'kinematic bicycle model' for the 'centre of gravity' (CoG) reference point
-class KinematicBicycleModel:
 
-	""" 
+
+# Kinematic bicycle model around the centre of gravity
+class KinematicBicycleModel:
+    """
+
 	State [x, y, theta, delta]:
 
 		x = vehicles relative x position
-		y = vehicles relative y postition 
+		y = vehicles relative y postition
 		theta = vehicles heading angle
 		delta = vehicles steering angle
 
 	Input [v, psi]:
 
 		v = velocity of the vehicle
-		psi = rate of steering change
+		psi = rate of steering angle change
+
+	Coefficients:
+
+		lF = 0 # dist from CoG ref to front axle
+		lR = 0 # dist from CoG ref to rear axle
 
 	GetNextState(v, psi):
 
@@ -24,92 +31,214 @@ class KinematicBicycleModel:
 
 	"""
 
-	# state variables
-	x = 0
-	y = 0 
-	theta = 0
-	delta = 0
-	slipAngle = 0 # 
+    # state variables
+    x = 0
+    y = 0
+    theta = 0
+    delta = 0
+    slipAngle = 0
 
-	# time derivative
-	dt = 1
+    # input variables
+    v = 0
+    psi = 0
 
-	# input variables
-	v = 0
-	psi = 0
+    # time derivative
+    dt = 1
 
-	# state array
-	State = np.array([x, y, theta, delta])
+    # state array
+    State = np.array([x, y, theta, delta])
 
-	# input array
-	Input = np.array([v, psi])
+    # input array
+    Input = np.array([v, psi])
 
-	# vehicle characteristics:
-	lF = 0 # dist from CoG ref to front axle
-	lR = 0 # dist from CoG ref to rear axle
-	maxVelocity = 0 # maximum velocity of the car 
-	maxSteeringAngle = 0 # +- maximum steering angle of the car
+    # vehicle characteristics:
+    lF = 0
+    lR = 0
+    maxVelocity = 0  # maximum velocity of the car
+    maxSteeringAngle = 0  # +- maximum steering angle of the car
 
-	# initialises the class and its constants
-	def __init__(self):
-		print("[INFO] Reading in vehicle characteristics and constraints from model.yaml")
+    # initialises the class and vehicle coefficients
+    def __init__(self):
+        print("[INFO] Reading in vehicle characteristics and constraints from model.yaml")
 
-		# reading in .yaml file
-		with open(r'utils/model.yaml') as file:
-			Yaml = yaml.load(file, Loader = yaml.FullLoader)
+        # reading in .yaml file
+        with open(r'utils/model.yaml') as file:
+            Yaml = yaml.load(file, Loader=yaml.FullLoader)
 
-			self.lF = Yaml['lF']
-			self.lR = Yaml['lR']
-			self.maxVelocity = Yaml['maxV']
-			self.maxSteeringAngle = Yaml['maxSA']
+            self.lF = Yaml['lF']
+            self.lR = Yaml['lR']
+            self.maxVelocity = Yaml['maxV']
+            self.maxSteeringAngle = Yaml['maxSA']
 
-	# checks vehicle constraints
-	def CheckConstraints(self, v, psi):
-		
-		if self.delta > self.maxSteeringAngle:
-			self.delta = self.maxSteeringAngle
+    # checks vehicle constraints
+    def CheckConstraints(self):
 
-		if self.delta < -self.maxSteeringAngle:
-			self.delta = -self.maxSteeringAngle
+        if self.delta > self.maxSteeringAngle:
+            self.delta = self.maxSteeringAngle
 
-	# gets the next state of the car
-	def GetNextState(self, v, psi):
+        if self.delta < -self.maxSteeringAngle:
+            self.delta = -self.maxSteeringAngle
 
-		# applying change of steering rate to steering angle (degrees/sec)
-		self.delta = self.delta + (self.dt * psi)
+        if self.v > self.maxVelocity:
+            self.v = self.maxVelocity
 
-		# converting steering angle to radians (np.trig takes a rad)
-		self.delta = np.deg2rad(self.delta)
+    # gets the next state of the car
+    def GetNextState(self, v, psi):
 
-		# getting slip angle
-		self.slipAngle = np.arctan(self.lR / (self.lR + self.lF) * np.tan(self.delta))
+        # applying new velocity
+        self.v = v
 
-    	# getting next state
-		self.x = self.x + self.dt * (v * np.cos(self.theta + self.slipAngle) ) 
-		self.y = self.y + self.dt * (v * np.sin(self.theta + self.slipAngle) ) 
-		self.theta = self.theta + self.dt * v / self.lF * np.sin(self.slipAngle)
+        # applying change of steering rate to steering angle (rads/sec)
+        self.delta = self.delta + (self.dt * psi)
 
-		# updating State array (returning theta in degrees)
-		self.State = np.array([self.x, self.y, np.rad2deg(self.theta), self.delta])
+        # checking constraints
+        self.CheckConstraints()
 
-		#print(self.State)
+        # getting slip angle
+        self.slipAngle = np.arctan(self.lR / (self.lR + self.lF) * np.tan(self.delta))
 
-		return self.State
+        # getting next
+        self.x = self.x + self.dt * (self.v * np.cos(self.theta + self.slipAngle))
+        self.y = self.y + self.dt * (self.v * np.sin(self.theta + self.slipAngle))
+        self.theta = self.theta + self.dt * self.v / self.lF * np.sin(self.slipAngle)
+
+        # updating State
+        self.State = np.array([self.x, self.y, self.theta, self.delta])
+
+        return self.State
 
 
-# class that represents the 'dynamic bicycle model' for the 'centre of gravity' (CoG) reference point
+# Dynamic bicycle model around the centre of gravity
 class DynamicBicycleModel:
-	
-	# initialises the class and its constants
-	def __init__(self):
-		print("[INFO] Reading in vehicle characteristics and constraints from model.yaml")
+    """
 
-		# reading in .yaml file
-		with open(r'utils/model.yaml') as file:
-			Yaml = yaml.load(file, Loader = yaml.FullLoader)
+	State [x, y, phi, vX, vY, omega] in the inertial frame
 
-			self.lF = Yaml['lF']
-			self.lR = Yaml['lR']
-			self.maxVelocity = Yaml['maxV']
-			self.maxSteeringAngle = Yaml['maxSA']
+		x = vehicles x position
+		y = vehicles y position
+		phi = heading angle of the car
+		vX = longitudinal velocity of the vehicle
+		vY = lateral velocity of the vehicle
+		r = yaw rate
+		psi = steering angle of the vehicle
+		t = driver command corresponding to a desired acceleration
 
+
+	Input [d, psi]:
+
+		_psi = rate of steering angle change
+		_t = applied driver command
+
+	Coefficients:
+
+		m = mass of the vehicle
+		lF = 0 # dist from CoG ref to front axle
+		lR = 0 # dist from CoG ref to rear axle11
+		cB, cC, cD = parameters describe the semi-emperical curve for the tires
+		cD = drag coefficient
+		cRR = rolling resistance coefficient
+		cDTM = drive train motor coefficient
+
+		etNextState(d, psi):
+
+	function that takes in the velocity and rate of steering change at a time and computes the next state for the vehicle
+
+	"""
+
+    # state variables
+    x = 0
+    y = 0
+    phi = 0
+    vX = 0.01
+    vY = 0.01
+    r = 0
+    delta = 0
+    t = 0
+
+    # input variables
+    _delta = 0
+    _t = 0
+
+    # time derivative
+    dt = 1
+
+    # state array
+    State = np.array([x, y, phi, vX, vY, r])
+
+    # input array
+    Input = np.array([_delta, _t])
+
+    # vehicle characteristics:
+    m = 0
+    cB = 0
+    cC = 0
+    cD = 0
+    lF = 0
+    lR = 0
+    iZ = 0
+    cRR = 0
+    cDTM = 0
+    maxVelocity = 0  # maximum velocity of the car
+    maxSteeringAngle = 0  # +- maximum steering angle of the car
+
+    # initialises the class and its constants
+    def __init__(self):
+        print("[INFO] Reading in vehicle characteristics and constraints from model.yaml")
+
+        # reading in .yaml file
+        with open(r'utils/model.yaml') as file:
+            Yaml = yaml.load(file, Loader=yaml.FullLoader)
+
+            self.m = Yaml['m']
+            self.cB = Yaml['cB']
+            self.cC = Yaml['cC']
+            self.cD = Yaml['cD']
+            self.lF = Yaml['lF']
+            self.lR = Yaml['lR']
+            self.iZ = Yaml['iZ']
+            self.cRR = Yaml['cRR']
+            self.cDTM = Yaml['cDTM']
+            self.maxVelocity = Yaml['maxV']
+            self.maxSteeringAngle = Yaml['maxSA']
+
+    # checks vehicle constraints
+    def CheckConstraints(self):
+
+        if self.delta > self.maxSteeringAngle:
+            self.delta = self.maxSteeringAngle
+
+        if self.delta < -self.maxSteeringAngle:
+            self.delta = -self.maxSteeringAngle
+
+    # gets the next state of the car
+    def GetNextState(self, _delta, _t):
+
+        # applying change in steering angle
+        self.delta = self.delta + self.dt * _delta
+
+        # applying change in driver command
+        self.t = self.t + self.dt * _t
+
+        aR = np.arctan((self.vY - (self.lR * self.r)) / self.vX)
+        aF = np.arctan((self.vY - (self.lF * self.r)) / self.vX) - self.delta
+
+        # assuming that the coefficients for both wheels are the same
+        FrY = self.cD * np.sin(self.cC * np.arctan(self.cB * aR))
+        FfY = self.cD * np.sin(self.cC * np.arctan(self.cB * aF))
+
+        Fx = self.cDTM * self.t - self.cRR - (self.cD * self.vX ** 2)
+
+        # checking constraints
+        # self.CheckConstraints()
+
+        self.x = self.x + self.dt * (self.vX * np.cos(self.phi) - self.vY * np.sin(self.phi))
+        self.y = self.y + self.dt * (self.vX * np.sin(self.phi) + self.vY * np.cos(self.phi))
+        self.phi = self.phi + self.dt * self.r
+
+        self.vX = self.vX + self.dt * (1 / self.m * (Fx - (FfY * np.sin(self.delta)) + (self.m * self.vY * self.r)))
+        self.vY = self.vY + self.dt * (1 / self.m * (FrY + (FfY * np.cos(self.delta)) - (self.m * self.vX * self.r)))
+        self.r = self.r + self.dt * (1 / self.iZ * (FfY * self.lF * np.cos(self.delta) - FrY * self.lF))
+
+
+if __name__ == "__main__":
+    print("[INFO] This is to be used as a library not an executable")
