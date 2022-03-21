39b6bbc5b1101d32a2d9672008a85ea4503ebfad
Liam Ferrante <liam.p.ferrante@gmail.com>
2022-03-21 18:04:18 +1000

    Comments and new visualisation functions called

--- src/perception/lidar_pipeline_2/lidar_pipeline_2/library/lidar_manager.py
+++ src/perception/lidar_pipeline_2/lidar_pipeline_2/library/lidar_manager.py
@@ -26,6 +26,7 @@ def detect_cones(
         T_B,
         T_RMSE,
         REGRESS_BETWEEN_BINS,
+        T_D_GROUND,
         T_D_MAX,
         point_count,
         create_figures,
@@ -51,8 +52,9 @@ def detect_cones(
     BIN_COUNT = math.ceil(LIDAR_RANGE / BIN_SIZE)
 
     if create_figures:
-        vis.plot_point_cloud_2D(point_cloud, point_count, working_dir, timestamp)
-        vis.plot_point_cloud_3D(point_cloud, point_count, working_dir, timestamp, animate_figures, model_car)
+        # vis.plot_point_cloud_2D(point_cloud, point_count, working_dir, timestamp)
+        # vis.plot_point_cloud_3D(point_cloud, point_count, working_dir, timestamp, animate_figures, model_car)
+        pass
 
     # Discretise point cloud for real-time performance
     start_time = time.time()
@@ -63,10 +65,10 @@ def detect_cones(
     LOGGER.info(f'Numpy PointCloud discretised in {end_time - start_time}s')
     
     if create_figures:
-        vis.plot_segments_2D(point_cloud, segments, working_dir, timestamp)
-        vis.plot_bins_2D(point_cloud, bins, working_dir, timestamp)
-        vis.plot_segments_3D(point_cloud, segments, working_dir, timestamp, animate_figures)
-        vis.plot_bins_3D(point_cloud, bins, working_dir, timestamp, animate_figures)
+        # vis.plot_segments_2D(point_cloud, segments, working_dir, timestamp)
+        # vis.plot_bins_2D(point_cloud, bins, working_dir, timestamp)
+        # vis.plot_segments_3D(point_cloud, segments, working_dir, timestamp, animate_figures)
+        # vis.plot_bins_3D(point_cloud, bins, working_dir, timestamp, animate_figures)
         pass
 
     # Calculate prototype point for every bin (if one exists)
@@ -76,8 +78,8 @@ def detect_cones(
     end_time = time.time()
 
     if create_figures:
-        vis.plot_prototype_points_2D(split_prototype_segments, prototype_segments, DELTA_ALPHA, working_dir, timestamp)
-        vis.plot_prototype_points_3D(split_prototype_segments, prototype_segments, DELTA_ALPHA, working_dir, timestamp, animate_figures)
+        # vis.plot_prototype_points_2D(split_prototype_segments, prototype_segments, DELTA_ALPHA, working_dir, timestamp)
+        # vis.plot_prototype_points_3D(split_prototype_segments, prototype_segments, DELTA_ALPHA, working_dir, timestamp, animate_figures)
         pass
 
     LOGGER.info(f'Prototype Points computed in {end_time - start_time}s')
@@ -89,7 +91,7 @@ def detect_cones(
     
     if create_figures:
         vis.plot_ground_plane_2D(ground_plane, split_prototype_segments, prototype_segments, DELTA_ALPHA, working_dir, timestamp)
-        vis.plot_ground_plane_3D()
+        vis.plot_ground_plane_3D(ground_plane, split_prototype_segments, prototype_segments, DELTA_ALPHA, working_dir, timestamp, animate_figures)
         pass
     
     # print(ground_plane)
@@ -97,8 +99,15 @@ def detect_cones(
     LOGGER.info(f'Ground Plane estimated in {end_time - start_time}s')
 
     start_time = time.time()
-    point_labels = pc.label_points_3(point_cloud, point_norms, seg_bin_z_ind, segments, ground_plane, SEGMENT_COUNT, DELTA_ALPHA, BIN_SIZE, T_D_MAX, point_count, bins)
+    point_labels = pc.label_points_3(point_cloud, point_norms, seg_bin_z_ind, segments, ground_plane, SEGMENT_COUNT, DELTA_ALPHA, BIN_SIZE, T_D_GROUND, T_D_MAX, point_count, bins)
     end_time = time.time()
+    
+    print(point_labels)
+    
+    if create_figures:
+        vis.plot_labelled_points_2D(point_cloud, point_labels, ground_plane, DELTA_ALPHA, working_dir, timestamp)
+        vis.plot_labelled_points_3D(point_cloud, point_labels, ground_plane, DELTA_ALPHA, working_dir, timestamp, animate_figures)
+        pass
 
     LOGGER.info(f'Points labelled in {end_time - start_time}s')
 
@@ -113,3 +122,7 @@ def detect_cones(
 # only import what's necessary for each file
 
 # FOR SOME REASON POINTS BEHIND x=0 DISAPPEAR DURING PROTOTYPE POINT
+
+# AHHHHHHHHHHHHHHHHHHHHHH
+# YOU NEED THE XY NORM NOT THE XYZ NORM
+# AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
