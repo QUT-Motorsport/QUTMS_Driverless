c9e3235f80e984c810c747f0264b6ab4172c8bba
Alistair English <alistair.english@protonmail.com>
2022-03-24 16:27:01 +1000

    WIP debugging runtime issues

--- src/navigation/slam/src/node_ekf.cpp
+++ src/navigation/slam/src/node_ekf.cpp
@@ -22,9 +22,16 @@
 
 #include "rclcpp/rclcpp.hpp"
 
+// USEFUL LINKS
+// https://jihongju.github.io/2018/10/05/slam-05-ekf-slam/
+// https://www.youtube.com/watch?v=X30sEgIws0g
+// https://github.com/MURDriverless/slam/
+
 
 // x, y, orientation
 #define CAR_STATE_SIZE 3
+// x, y
+#define LANDMARK_STATE_SIZE 2
 
 
 double compute_dt(builtin_interfaces::msg::Time start_, builtin_interfaces::msg::Time end_) {
@@ -35,7 +42,7 @@ double compute_dt(builtin_interfaces::msg::Time start_, builtin_interfaces::msg:
 }
 
 
-void motion_model(
+void compute_motion_model(
     double dt,
     double forward_vel,
     double theta_dot,
@@ -61,18 +68,88 @@ void motion_model(
 }
 
 void update_pred_motion_cov(
-    int state_size,
     const Eigen::MatrixXd& motion_jacobian,  // G_x
     const Eigen::MatrixXd& cov,
     Eigen::MatrixXd& pred_cov_out
 ) {
     // G = ( G_x  0 )
     //     ( 0    I )
-    // TODO: INITAL_STATE_SIZE needs to be dynamic current state size here
-    Eigen::MatrixXd G = Eigen::MatrixXd::Identity(state_size, state_size);
+    Eigen::MatrixXd G = Eigen::MatrixXd::Identity(cov.rows(), cov.cols());
     G.topLeftCorner(CAR_STATE_SIZE, CAR_STATE_SIZE) = motion_jacobian;
 
     pred_cov_out = G * cov * G.transpose();
+}
+
+std::optional<int> find_associated_landmark_idx(
+    const Eigen::MatrixXd& mu,
+    double search_x,
+    double search_y
+) {
+    // data association, uses lowest euclidian distance, within a threshold
+
+    double min_distance = 0.3;  // m, threshold
+    std::optional<int> idx = {};
+
+    for(int i=CAR_STATE_SIZE; i < mu.rows(); i += LANDMARK_STATE_SIZE) {
+        // iterate through the state, in intervals of two, since x and y
+        // for each landmark are stored in the state vector
+        double i_x = mu(i, 0);
+        double i_y = mu(i+1, 0);
+        
+        double distance = sqrt(pow(search_x - i_x, 2) + pow(search_y - i_y, 2));
+        if(distance < min_distance) {
+            min_distance = distance;
+            idx = i;
+        }
+    }
+
+    return idx;
+}
+
+void compute_expected_z(
+    const Eigen::MatrixXd& mu,
+    int landmark_idx,
+    Eigen::MatrixXd& expected_z_out,
+    Eigen::MatrixXd& observation_jacobian
+) {
+    // this is h()
+    
+    double x = mu(0, 0);
+    double y = mu(1, 0);
+    double theta = mu(2, 0);
+
+    double lm_x = mu(landmark_idx);
+    double lm_y = mu(landmark_idx+1);
+
+    double dx = lm_x - x;
+    double dy = lm_y - y;
+
+    double q = pow(dx, 2) + pow(dy, 2);
+
+    // z = (  range  )
+    //     ( bearing )
+    expected_z_out(0, 0) = sqrt(q);
+    expected_z_out(1, 0) = atan2(dy, dx) - theta;
+
+    Eigen::MatrixXd Fx = Eigen::MatrixXd::Zero(CAR_STATE_SIZE + LANDMARK_STATE_SIZE, mu.rows());
+    Fx.topLeftCorner(CAR_STATE_SIZE, CAR_STATE_SIZE) = Eigen::MatrixXd::Identity(CAR_STATE_SIZE, CAR_STATE_SIZE);
+    Fx(CAR_STATE_SIZE + 1, landmark_idx) = 1;
+    Fx(CAR_STATE_SIZE + 2, landmark_idx + 1) = 1;
+
+    Eigen::MatrixXd low_dim_jacobian(2, CAR_STATE_SIZE + LANDMARK_STATE_SIZE);
+    low_dim_jacobian(0, 0) = 1/q * -sqrt(q) * dx;
+    low_dim_jacobian(0, 1) = 1/q * -sqrt(q) * dy;
+    low_dim_jacobian(0, 2) = 0;  // 1/q * 0
+    low_dim_jacobian(0, 3) = 1/q * sqrt(q) * dx;
+    low_dim_jacobian(0, 4) = 1/q * sqrt(q) * dy;
+    low_dim_jacobian(1, 0) = 1/q * dy;
+    low_dim_jacobian(1, 1) = 1/q * -dx;
+    low_dim_jacobian(1, 2) = -1;  // 1/q * -q
+    low_dim_jacobian(1, 2) = 1/q * -q;
+    low_dim_jacobian(1, 3) = 1/q * -dy;
+    low_dim_jacobian(1, 3) = 1/q * dx;
+
+    observation_jacobian = low_dim_jacobian * Fx;
 }
 
 
@@ -83,8 +160,6 @@ class EKFNode : public rclcpp::Node {
 
         Eigen::MatrixXd mu;  // final state (mean, μ)
         Eigen::MatrixXd cov;  // final state (covariance, ∑)
-
-        int state_size = CAR_STATE_SIZE;
 
         std::optional<builtin_interfaces::msg::Time> last_sensed_control_update;
 
@@ -98,8 +173,10 @@ class EKFNode : public rclcpp::Node {
             message_filters::Synchronizer<approximate_policy>syncApproximate(approximate_policy(10), imu_sub, vel_sub);
             syncApproximate.registerCallback(&EKFNode::sensed_control_callback, this);
 
-            pred_mu = Eigen::MatrixXd::Zero(this->state_size, 1);
-            pred_cov = 0.1 * Eigen::MatrixXd::Identity(this->state_size, this->state_size);
+            // initalise state and convariance with just the car state
+            // (landmarks will be added when they are detected)
+            pred_mu = Eigen::MatrixXd::Zero(CAR_STATE_SIZE, 1);
+            pred_cov = 0.1 * Eigen::MatrixXd::Identity(CAR_STATE_SIZE, CAR_STATE_SIZE);
 
             mu = pred_mu;
             cov = pred_cov;
@@ -127,7 +204,7 @@ class EKFNode : public rclcpp::Node {
             // u vector doesnt really need to be constructed, but the concept lives here as:
             // [vel_msg->twist.linear.x, imu_msg->angular_velocity.z]
             Eigen::MatrixXd motion_jacobian = Eigen::MatrixXd::Zero(CAR_STATE_SIZE, CAR_STATE_SIZE);  // G_x
-            motion_model(
+            compute_motion_model(
                 dt,
                 vel_msg->twist.linear.x,
                 imu_msg->angular_velocity.z,
@@ -136,12 +213,60 @@ class EKFNode : public rclcpp::Node {
                 motion_jacobian
             );
             std::cout << "motion jacobian:\n" << motion_jacobian << "\n" << std::endl;
-            update_pred_motion_cov(this->state_size, motion_jacobian, this->pred_cov, this->pred_cov);
+            update_pred_motion_cov(motion_jacobian, this->pred_cov, this->pred_cov);
         }
 
         void cone_detection_callback(driverless_msgs::msg::ConeDetectionStamped msg) {
-            // TODO
-            (void) msg;
+            // Q = ( σ_r^2  0         )
+            //     ( 0      σ_theta^2 )
+            Eigen::Matrix2d Q;
+            Q << 10,   0,
+                 0 ,  10;
+
+            double x = this->pred_mu(0, 0);
+            double y = this->pred_mu(1, 0);
+            double theta = this->pred_mu(2, 0);
+
+            for(driverless_msgs::msg::Cone cone : msg.cones) {
+                // landmark (cone) position in global frame
+                double glob_lm_x = x + cone.location.x * cos(theta) - cone.location.y * sin(theta);
+                double glob_lm_y = y + cone.location.x * sin(theta) + cone.location.y * cos(theta);
+
+                std::optional<int> associated_idx = find_associated_landmark_idx(this->pred_mu, glob_lm_x, glob_lm_y);
+
+                if(!associated_idx.has_value()) {
+                    // new landmark
+                    this->pred_mu.conservativeResize(this->pred_mu.rows() + 2, Eigen::NoChange);
+                    this->pred_cov.conservativeResize(this->pred_cov.rows() + 2, pred_cov.cols() + 2);
+
+                    int new_lm_idx = this->pred_mu.rows() - 2;
+
+                    this->pred_mu(new_lm_idx, 0) = glob_lm_x;
+                    this->pred_mu(new_lm_idx + 1, 0) = glob_lm_y;
+
+                    this->pred_cov(new_lm_idx, new_lm_idx) = 0.5;
+                    this->pred_cov(new_lm_idx+1, new_lm_idx+1) = 0.5;
+
+                    associated_idx = new_lm_idx;
+                }
+
+                // z = (  range  )
+                //     ( bearing )
+                Eigen::MatrixXd z(2, 1);
+                z(0, 0) = sqrt(pow(cone.location.x, 2) + pow(cone.location.y, 2));
+                z(1, 0) = atan2(cone.location.y, cone.location.x);
+
+                Eigen::MatrixXd expected_z(2, 1);
+                Eigen::MatrixXd observation_jacobian = Eigen::MatrixXd::Zero(cov.rows(), cov.cols());  // H
+                compute_expected_z(this->pred_mu, associated_idx.value(), expected_z, observation_jacobian);
+
+                // std::cout << "H:\n" << observation_jacobian << "\n" << std::endl;
+
+                // Eigen::MatrixXd K = this->pred_cov*observation_jacobian.transpose()*((observation_jacobian*this->pred_cov*observation_jacobian.transpose() + Q).inverse());
+                // this->pred_mu = this->pred_mu + K*(z - expected_z);
+                // this->pred_cov = (Eigen::MatrixXd::Identity());
+                // std::cout << "K:\n" << K << "\n" << std::endl;
+            }
         }
 
         void print_matricies() {
@@ -185,6 +310,19 @@ int main(int argc, char ** argv) {
     imu_msg->header.stamp.sec=4;
     vel_msg->header.stamp.sec=4;
     ekf_node.sensed_control_callback(imu_msg, vel_msg);
+    ekf_node.print_matricies();
+
+    std::cout << "\n ---------- Measurement \n" << std::endl;
+    driverless_msgs::msg::ConeDetectionStamped msg;
+    
+    driverless_msgs::msg::Cone cone1;
+    cone1.location.x = 2;
+    cone1.location.y = 2;
+    cone1.color = driverless_msgs::msg::Cone::BLUE;
+
+    msg.cones = {cone1};
+
+    ekf_node.cone_detection_callback(msg);
     ekf_node.print_matricies();
 
 	return 0;
