a56136e34db85d9f5076ca4069e12280350f6a41
Tom Fraser <tr.fraser@bigpond.com>
2022-03-27 19:57:26 +1000

    Deduplicated Data

--- src/hardware/res_receiver/src/node_res_receiver.cpp
+++ src/hardware/res_receiver/src/node_res_receiver.cpp
@@ -15,15 +15,9 @@ class RESReceiver : public rclcpp::Node, public CanInterface {
 	rclcpp::Subscription<driverless_msgs::msg::Can>::SharedPtr can_sub;
 	rclcpp::Publisher<driverless_msgs::msg::RES>::SharedPtr res_pub;
 
-	typedef struct RESStatus {
-		bool sw_k2;
-		bool bt_k3;
-		bool estop;
-		bool loss_of_signal_shutdown_notice;
-		uint8_t radio_quality;
-	} RESStatus_t;
+	driverless_msgs::msg::RES res_status;
 
-	RESStatus_t res_status;
+	rclcpp::Time _internal_status_time;
 
 	void canbus_callback(const driverless_msgs::msg::Can msg) {
 		switch (msg.id) {
@@ -37,6 +31,7 @@ class RESReceiver : public rclcpp::Node, public CanInterface {
 
 			case (0x180 + RES_NODE_ID): {
 				// RES Reciever Status Packet
+				this->_internal_status_time = this->now();	// Debug information
 
 				// Store in local state
 				this->res_status.estop = msg.data[9] & (1 << 0);
@@ -45,15 +40,8 @@ class RESReceiver : public rclcpp::Node, public CanInterface {
 				this->res_status.radio_quality = msg.data[6];
 				this->res_status.loss_of_signal_shutdown_notice = msg.data[7] & (1 << 6);
 
-				// Create a packet, send it
-				driverless_msgs::msg::RES res_packet;
-				res_packet.estop = this->res_status.estop;
-				res_packet.sw_k2 = this->res_status.sw_k2;
-				res_packet.bt_k3 = this->res_status.bt_k3;
-				res_packet.radio_quality = this->res_status.radio_quality;
-				res_packet.loss_of_signal_shutdown_notice = this->res_status.loss_of_signal_shutdown_notice;
-
-				this->res_pub->publish(res_packet);
+				// Publish state
+				this->res_pub->publish(this->res_status);
 			} break;
 
 			default:
