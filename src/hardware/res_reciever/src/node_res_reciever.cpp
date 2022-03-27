94208a9bdcbc7acec13265de738ead4f403892a1
Tom Fraser <tr.fraser@bigpond.com>
2022-03-27 13:51:30 +1000

    Added RES Topic Msg
    Added use to node_res_receiver

--- src/hardware/res_reciever/src/node_res_reciever.cpp
+++ src/hardware/res_reciever/src/node_res_reciever.cpp
@@ -2,6 +2,7 @@
 
 #include "can_interface.hpp"
 #include "driverless_msgs/msg/can.hpp"
+#include "driverless_msgs/msg/res.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 const int RES_NODE_ID = 0x011;
@@ -12,14 +13,14 @@ class RESReciever : public rclcpp::Node, public CanInterface {
    private:
 	rclcpp::Publisher<driverless_msgs::msg::Can>::SharedPtr can_pub;
 	rclcpp::Subscription<driverless_msgs::msg::Can>::SharedPtr can_sub;
+	rclcpp::Publisher<driverless_msgs::msg::RES>::SharedPtr res_pub;
 
 	typedef struct RESStatus {
 		bool sw_k2;
 		bool bt_k3;
 		bool estop;
-		bool estop_advance_notice;
+		bool loss_of_signal_shutdown_notice;
 		uint8_t radio_quality;
-		rclcpp::Time status_epoch;
 	} RESStatus_t;
 
 	RESStatus_t res_status;
@@ -36,12 +37,23 @@ class RESReciever : public rclcpp::Node, public CanInterface {
 
 			case (0x180 + RES_NODE_ID): {
 				// RES Reciever Status Packet
+
+				// Store in local state
 				this->res_status.estop = msg.data[9] & (1 << 0);
 				this->res_status.sw_k2 = msg.data[0] & (1 << 1);
 				this->res_status.bt_k3 = msg.data[0] & (1 << 2);
 				this->res_status.radio_quality = msg.data[6];
-				this->res_status.estop_advance_notice = msg.data[7] & (1 << 6);
-				this->res_status.status_epoch = rclcpp::Node::now();
+				this->res_status.loss_of_signal_shutdown_notice = msg.data[7] & (1 << 6);
+
+				// Create a packet, send it
+				driverless_msgs::msg::RES res_packet;
+				res_packet.estop = this->res_status.estop;
+				res_packet.sw_k2 = this->res_status.sw_k2;
+				res_packet.bt_k3 = this->res_status.bt_k3;
+				res_packet.radio_quality = this->res_status.radio_quality;
+				res_packet.loss_of_signal_shutdown_notice = this->res_status.loss_of_signal_shutdown_notice;
+
+				this->res_pub->publish(res_packet);
 			} break;
 
 			default:
@@ -55,6 +67,7 @@ class RESReciever : public rclcpp::Node, public CanInterface {
 		this->can_pub = this->create_publisher<driverless_msgs::msg::Can>("canbus_carbound", 10);
 		this->can_sub = this->create_subscription<driverless_msgs::msg::Can>(
 			"canbus_rosbound", 10, std::bind(&RESReciever::canbus_callback, this, _1));
+		this->res_pub = this->create_publisher<driverless_msgs::msg::RES>("res_status", 10);
 	}
 };
 
