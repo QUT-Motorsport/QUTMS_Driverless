c57e8fcc8814090b2c09db7e88c4d829692fd353
Tom Fraser <tr.fraser@bigpond.com>
2022-03-29 13:12:45 +1000

    Setup sequence done

--- src/hardware/res_receiver/src/node_res_receiver.cpp
+++ src/hardware/res_receiver/src/node_res_receiver.cpp
@@ -74,6 +74,9 @@ class RESReceiver : public rclcpp::Node, public CanInterface {
 		RCLCPP_INFO(this->get_logger(), "Attemping to start res");
 		uint8_t p[8] = {0x80, RES_NODE_ID, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 		this->can_pub->publish(this->_d_2_f(0x00, false, p));
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		uint8_t p2[8] = {0x01, RES_NODE_ID, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+		this->can_pub->publish(this->_d_2_f(0x00, false, p2));
 		RCLCPP_INFO(this->get_logger(), "res startup complete");
 	}
 };
