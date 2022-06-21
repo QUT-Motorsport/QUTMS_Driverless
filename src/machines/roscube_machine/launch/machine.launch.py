2a4ef4dfd6a50510300e725be2900723a9836438
Alistair English <alistair.english@protonmail.com>
2022-06-22 09:15:09 +1000

    on car config

--- src/machines/roscube_machine/launch/machine.launch.py
+++ src/machines/roscube_machine/launch/machine.launch.py
@@ -3,13 +3,12 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        # TODO: include sensors launch file
+        Node(
+            package='canbus',
+            executable='canbus',
+        ),
         Node(
             package='rosboard',
             executable='rosboard_node',
-        ),
-        Node(
-            package='controllers',
-            executable='simple',
         ),
     ])
