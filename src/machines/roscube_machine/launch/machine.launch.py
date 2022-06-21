83f7b3f12491fac5dcec938cbe294a8f887da684
Alistair English <alistair.english@protonmail.com>
2022-06-22 09:34:57 +1000

    list

--- src/machines/roscube_machine/launch/machine.launch.py
+++ src/machines/roscube_machine/launch/machine.launch.py
@@ -6,7 +6,7 @@ def generate_launch_description():
         Node(
             package="canbus",
             executable="canbus",
-            parameters="/home/developer/driverless_ws/src/hardware/canbus/config/canbus.yaml",  # TODO: use auto getter for path
+            parameters=["/home/developer/driverless_ws/src/hardware/canbus/config/canbus.yaml"],  # TODO: use auto getter for path
         ),
         Node(
             package="rosboard",
