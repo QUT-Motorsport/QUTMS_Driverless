fac0909c73a863cf0a4e320d7ceea4b0743c30e0
Alistair English <alistair.english@protonmail.com>
2022-06-22 12:34:48 +1000

    tuning

--- src/navigation/controllers/controllers/node_sine.py
+++ src/navigation/controllers/controllers/node_sine.py
@@ -8,6 +8,8 @@ import math
 from ackermann_msgs.msg import AckermannDrive
 
 
+interval = 0.02
+
 class SineControllerNode(Node):
     def __init__(self):
         super().__init__("SineController")
@@ -15,15 +17,14 @@ class SineControllerNode(Node):
         self.steering_publisher: Publisher = self.create_publisher(
             AckermannDrive, "steering", 1)
         self.get_logger().info("Sine Controller")
-        self.create_timer(0.01, self.timer_cb)
+        self.create_timer(interval, self.timer_cb)
         self.count = 0
 
     def timer_cb(self):
         self.translate = 2 * math.pi / 5
-        self.count += 0.01
+        self.count += interval
         steering_msg = AckermannDrive()
-        steering_msg.steering_angle = math.sin(
-            self.count * self.translate) * math.pi
+        steering_msg.steering_angle = math.sin(self.count * self.translate) * math.pi
         # self.get_logger().info(f"angle: {steering_msg.steering_angle}")
         self.steering_publisher.publish(steering_msg)
 
