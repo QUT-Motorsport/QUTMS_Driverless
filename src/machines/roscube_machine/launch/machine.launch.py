8f6ba1ff901d349be6d9d6a0618291993ead8456
Alistair English <alistair.english@protonmail.com>
2022-07-11 15:18:52 +1000

    convert paths to strings

--- src/machines/roscube_machine/launch/machine.launch.py
+++ src/machines/roscube_machine/launch/machine.launch.py
@@ -21,12 +21,12 @@ def generate_launch_description():
             ),
             IncludeLaunchDescription(
                 launch_description_source=PythonLaunchDescriptionSource(
-                    launch_file_path=get_package_share_path("sensors") / "launch" / "sensors.launch.py"
+                    launch_file_path=str(get_package_share_path("sensors") / "launch" / "sensors.launch.py")
                 ),
             ),
             IncludeLaunchDescription(
                 launch_description_source=PythonLaunchDescriptionSource(
-                    launch_file_path=get_package_share_path("models") / "launch" / "robot_description.launch.py"
+                    launch_file_path=str(get_package_share_path("models") / "launch" / "robot_description.launch.py")
                 ),
                 launch_arguments=[
                     ("urdf_model", "qev3.urdf.xacro"),
