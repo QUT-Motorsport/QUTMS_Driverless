e5a45b7b45218f216bce66facce5c3f5cfe1ebd5
Alastair Bradford <albradford2468@gmail.com>
2022-07-29 21:04:49 +1000

    manual override with testing:=true for missions

--- src/missions/missions/missions/base_mission_class.py
+++ src/missions/missions/missions/base_mission_class.py
@@ -10,17 +10,22 @@ class BaseMission(Node):
     def __init__(self):
         super().__init__("mission")
 
+        testing_override = self.declare_parameter("testing", False).get_parameter_value().bool_value
+        self.get_logger().info("Auto mission override: " + str(testing_override))
+        if testing_override:
+            self.r2d = True
+
         self.create_subscription(Can, "/can_rosbound", self.can_callback, 10)
 
         self.publisher: Publisher = self.create_publisher(Can, "/can_carbound", 10)
 
     def can_callback(self, can_msg: Can):
         # listen to EBS check success state
-        if can_msg.data[0] == 0:  # 'state' id not ready
-            self.get_logger().info("waiting on r2d")
+        if can_msg.data[0] == 0 and not self.r2d:  # 'state' id not ready
+            self.get_logger().debug("waiting on r2d")
 
-        # next, listen to the RES 'go' state CAN msg.
+        # next, listen to the RES 'go' stateW CAN msg.
         # send can msg to EBS VCU
         elif can_msg.data[0] == 1 and not self.r2d:  # state id ready and haven't previously set r2d
-            self.get_logger().info("r2d pressed")
+            self.get_logger().debug("r2d pressed")
             self.r2d = True
