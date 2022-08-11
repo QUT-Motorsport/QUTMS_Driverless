7eec5fefed48ec7f65d869c5bbc75c7b8f7f5124
Alastair Bradford <albradford2468@gmail.com>
2022-08-11 15:51:52 +1000

    commented out packages required for trackdrive

--- src/missions/missions/launch/trackdrive.launch.py
+++ src/missions/missions/launch/trackdrive.launch.py
@@ -10,7 +10,11 @@ def generate_launch_description():
     return LaunchDescription(
         [
             testing,
-            Node(package="missions", executable="trackdrive", parameters=[{"testing": LaunchConfiguration("testing")}]),
+            Node(
+                package="missions",
+                executable="trackdrive",
+                parameters=[{"testing": LaunchConfiguration("testing")}],
+            ),
             Node(
                 package="controllers",
                 executable="reactive_control",
@@ -21,11 +25,15 @@ def generate_launch_description():
             # )
             # Node(
             #     package="vision_pipeline",
-            #     executable="trt_detector",
+            #     executable="torch_detector",
             # ),
             # Node(
-            #     package="lidar_pipeline",
+            #     package="lidar_pipeline_2",
             #     executable="lidar_processing",
+            # ),
+            # Node(
+            #     package="fusion",
+            #     executable="fusion",
             # ),
         ]
     )
