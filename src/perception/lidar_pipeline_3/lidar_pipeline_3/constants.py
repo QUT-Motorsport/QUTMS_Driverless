a538b39803f808932d3645e5faf815af411e5f25
Liam Ferrante <liam.p.ferrante@gmail.com>
2022-08-23 16:43:38 +1000

    Added parameter

--- src/perception/lidar_pipeline_3/lidar_pipeline_3/constants.py
+++ src/perception/lidar_pipeline_3/lidar_pipeline_3/constants.py
@@ -1,4 +1,8 @@
 import pathlib
 
+# Config Parameters
 WORKING_DIR: str = str(pathlib.Path(__file__).parent.resolve())
 OUTPUT_DIR: str = WORKING_DIR + '/output'
+
+# Algorithm Parameters
+LIDAR_RANGE = 100            # Max range of points to process (metres)
