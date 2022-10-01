6111693cc6236b0eea6ed27a0913b150888d00fe
Alastair Bradford <albradford2468@gmail.com>
2022-10-01 21:37:53 +1000

    prep work for replacing fusion node with slam + publish map so far each loop

--- src/navigation/baby_slam/baby_slam/node_slam.py
+++ src/navigation/baby_slam/baby_slam/node_slam.py
@@ -1,49 +1,26 @@
-from math import atan2, cos, hypot, sin
+from math import atan2, cos, hypot, sin, pi
 import time
 
 import numpy as np
 from sklearn.neighbors import KDTree
 from transforms3d.euler import quat2euler
 
-from cv_bridge import CvBridge
 import message_filters
 import rclpy
 from rclpy.node import Node
 from rclpy.publisher import Publisher
 
 from driverless_msgs.msg import Cone, ConeDetectionStamped, ConeWithCovariance, TrackDetectionStamped
-from geometry_msgs.msg import Point as ROSPoint
+from geometry_msgs.msg import Point
 from geometry_msgs.msg import PoseWithCovarianceStamped
 
 from driverless_common.cone_props import ConeProps
-from driverless_common.point import Point
 
 from typing import List, Tuple
 
-# translate ROS image messages to OpenCV
-cv_bridge = CvBridge()
-
-# image display geometry
-SCALE = 4
-HEIGHT = (10 + 170) * SCALE
-WIDTH = (50 + 50) * SCALE
-
-
-def coord_to_img(x: float, y: float) -> Point:
-    """
-    Converts a relative depth from the camera into image coords
-    * param x: x coord
-    * param y: y coord
-    * return: Point int pixel coords
-    """
-    return Point(
-        int(round(50 * SCALE + x * SCALE)),
-        int(round(170 * SCALE - y * SCALE)),
-    )
-
 
 def wrap_to_pi(angle: float) -> float:  # in rads
-    return (angle + np.pi) % (2 * np.pi) - np.pi
+    return (angle + pi) % (2 * pi) - pi
 
 
 def predict(pose_msg: PoseWithCovarianceStamped, R: np.ndarray) -> Tuple[np.ndarray]:
@@ -114,7 +91,7 @@ def update(
     Sigma: np.ndarray,
 ) -> Tuple[np.ndarray, np.ndarray]:
 
-    i = index * 2 + 3
+    i = index * 2 + 3  # landmark index, first 3 are robot, each landmark has 2 values 
     muL = mu[i : i + 2]  # omit colour from location mean
 
     r = hypot(mu[0] - muL[0], mu[1] - muL[1])  # range to landmark
@@ -142,7 +119,9 @@ def update(
     mu = mu + addon[0]  # because this was 2D... had to take 'first' element
     Sigma = (np.eye(sig_len) - K @ G) @ Sigma
 
-    track[index, :2] = mu[i : i + 2]  # track is just mu without robot
+    track[index, :2] = mu[i : i + 2]  # track for this cone is just cone's mu
+
+    track[index, 3] += 1  # increment cone's seen count
 
     return mu, Sigma, track
 
@@ -150,33 +129,105 @@ def update(
 class EKFSlam(Node):
     R = np.diag([0.05, 0.05, 0.05])  # very confident of odom (cause its OP)
     Q = np.diag([1, 0.8]) ** 2  # detections are a bit meh
-    radius = 3  # nn kdtree nearch
+    radius = 2.3  # nn kdtree nearch
     leaf = 30  # nodes per tree before it starts brute forcing?
 
     mu = np.array([3.0, 0.0, 0.0])  # initial pose
     Sigma: np.ndarray = np.diag([0.01, 0.01, 0.01])
     track: np.ndarray = []
-    blue_indexes: List[int] = []
-    yellow_indexes: List[int] = []
-    orange_indexes: List[int] = []
+    # blue_indexes: List[int] = []
+    # yellow_indexes: List[int] = []
+    # orange_indexes: List[int] = []
 
     def __init__(self):
         super().__init__("ekf_slam")
 
         # sync subscribers
-        pose_sub = message_filters.Subscriber(self, PoseWithCovarianceStamped, "/zed2i/zed_node/pose_with_covariance")
-        detection_sub = message_filters.Subscriber(self, ConeDetectionStamped, "/fusion/cone_detection")
-        synchronizer = message_filters.ApproximateTimeSynchronizer(
-            fs=[pose_sub, detection_sub], queue_size=20, slop=0.2
-        )
-        synchronizer.registerCallback(self.callback)
+        # pose_sub = message_filters.Subscriber(self, PoseWithCovarianceStamped, "/zed2i/zed_node/pose_with_covariance")
+        # vision_sub = message_filters.Subscriber(self, ConeDetectionStamped, "/vision/cone_detection")
+        # lidar_sub = message_filters.Subscriber(self, ConeDetectionStamped, "/lidar/cone_detection")
+        # vision_synchronizer = message_filters.ApproximateTimeSynchronizer(
+        #     fs=[pose_sub, detection_sub], queue_size=20, slop=0.2
+        # )
+        # vision_synchronizer.registerCallback(self.callback)
+        # lidar_synchronizer = message_filters.ApproximateTimeSynchronizer(
+        #     fs=[pose_sub, detection_sub], queue_size=20, slop=0.2
+        # )
+        # lidar_synchronizer.registerCallback(self.callback)
 
+        self.create_subscription(PoseWithCovarianceStamped, "/zed2i/zed_node/pose_with_covariance", self.pose_callback, 1)
+        self.create_subscription(ConeDetectionStamped, "/vision/cone_detection", self.callback, 1)
+        self.create_subscription(ConeDetectionStamped, "/lidar/cone_detection", self.callback, 1)
+        self.last_pose = PoseWithCovarianceStamped()
+        
         # slam publisher
         self.slam_publisher: Publisher = self.create_publisher(TrackDetectionStamped, "/slam/track", 1)
 
         self.get_logger().info("---SLAM node initialised---")
 
-    def callback(self, pose_msg: PoseWithCovarianceStamped, detection_msg: ConeDetectionStamped):
+
+    def pose_callback(self, pose_msg: PoseWithCovarianceStamped):
+        self.last_pose = pose_msg
+
+
+    # remove landmarks we haven't seen in a while
+    def flush_map(self):
+        # only remove landmarks if they are behind us
+
+        # get the robot's heading vector
+        heading = np.array([cos(self.mu[2]), sin(self.mu[2])])
+        # get the robot's position vector
+        position = np.array([self.mu[0], self.mu[1]])
+
+        # get the landmark position vectors
+        # so if the landmark is behind the robot, the dot product will be negative
+        landmark_position_vectors = self.track[:, :2] - position
+        # get the dot product of the landmark position vector and the robot heading vector
+        dot_products = np.dot(landmark_position_vectors, heading)
+        # get the indexes of the landmarks that are behind the robot
+        behind_idxs = np.where(dot_products < 0)[0]        
+        
+        # get indexes of landmarks we haven't seen in a while
+        noisy_idxs = np.where(self.track[:, 3] < 10)[0]
+
+        # remove noisy and behind landmarks
+        idxs_to_remove = np.concatenate((behind_idxs, noisy_idxs)) # gets any indexes behind and noisy
+        unique, count = np.unique(idxs_to_remove, return_counts=True) # gets unique indexes
+        duplicated_idxs = unique[count > 1] # only gets indexes that are duplicated (behind and noisy)
+
+        if len(duplicated_idxs) > 0:
+            # remove landmarks from mu and Sigma
+            self.mu = np.delete(self.mu, [duplicated_idxs * 2 + 3, duplicated_idxs * 2 + 4], axis=0)
+
+            # remove landmarks from Sigma
+            self.Sigma = np.delete(self.Sigma, [duplicated_idxs * 2 + 3, duplicated_idxs * 2 + 4], axis=0)  # rows
+            self.Sigma = np.delete(self.Sigma, [duplicated_idxs * 2 + 3, duplicated_idxs * 2 + 4], axis=1)  # columns
+
+            # remove landmarks from track
+            self.track = np.delete(self.track, duplicated_idxs, axis=0)
+
+
+    def get_neighbourhood(self, det: ConeProps, mapx: float, mapy: float) -> bool:
+        on_map = False
+
+        neighbourhood = KDTree(self.track[:, :2], leaf_size=self.leaf)
+        check = np.reshape([mapx, mapy], (1, -1))  # turn into a 2D row array
+        ind = neighbourhood.query_radius(check, r=self.radius)  # check neighbours in radius
+        close = ind[0]  # index from the single colour list
+        if close.size != 0:
+            on_map = True
+            self.mu, self.Sigma, self.track = update(
+                self.track, close[0], det.sense_rb, self.Q, self.mu, self.Sigma
+            )
+            if self.track[close[0]][2] == Cone.UNKNOWN and det.colour != Cone.UNKNOWN:  # updated cone was a lidar detection
+                self.track[close[0]][2] = det.colour  # override colour
+
+        return on_map
+
+
+    # def callback(self, pose_msg: PoseWithCovarianceStamped, detection_msg: ConeDetectionStamped):
+    def callback(self, detection_msg: ConeDetectionStamped):
+        pose_msg = self.last_pose
         self.get_logger().debug("Received detection")
 
         # predict car location (pretty accurate odom)
@@ -185,56 +236,58 @@ class EKFSlam(Node):
         self.Sigma[0:3, 0:3] = SigmaR
 
         # process detected cones
-        for cone in detection_msg.cones_with_cov:
-            det = ConeProps(cone.cone)  # detection with properties
-            if det.range > 12 or det.colour == Cone.UNKNOWN:
-                continue  # out of range dont care
+        for cone in detection_msg.cones:
+            det = ConeProps(cone)  # detection with properties
 
             mapx = self.mu[0] + det.range * cos(self.mu[2] + det.bearing)
             mapy = self.mu[1] + det.range * sin(self.mu[2] + det.bearing)
 
             on_map = False  # by default its a new detection
 
-            same_track = []  # start empty for this colour
-            if self.track != []:
-                same_track: np.ndarray = self.track[self.track[:, 2] == det.colour]  # extract same colours
+            if len(self.track) != 0:
+                on_map = self.get_neighbourhood(det, mapx, mapy)
 
-            if len(same_track) != 0:  # this spline has been populated with cones
-                neighbourhood = KDTree(same_track[:, :2], leaf_size=self.leaf)
-                check = np.reshape([mapx, mapy], (1, -1))  # turn into a 2D row array
-                ind = neighbourhood.query_radius(check, r=self.radius)  # check neighbours in radius
-                close = ind[0]  # index from the single colour list
-                if close.size != 0:
-                    on_map = True
-                    # update the index of the matched cone on the track
-                    if det.colour == 0:
-                        self.mu, self.Sigma, self.track = update(
-                            self.track, self.blue_indexes[close[0]], det.sense_rb, self.Q, self.mu, self.Sigma
-                        )
-                    elif det.colour == 1:
-                        self.mu, self.Sigma, self.track = update(
-                            self.track, self.yellow_indexes[close[0]], det.sense_rb, self.Q, self.mu, self.Sigma
-                        )
-                    elif det.colour == 2:
-                        self.mu, self.Sigma, self.track = update(
-                            self.track, self.orange_indexes[close[0]], det.sense_rb, self.Q, self.mu, self.Sigma
-                        )
+            # same_track = []  # start empty for this colour
+            # if self.track != []:
+            #     same_track: np.ndarray = self.track[self.track[:, 2] == det.colour]  # extract same colours
+
+            # if len(same_track) != 0:  # this spline has been populated with cones
+            #     neighbourhood = KDTree(same_track[:, :2], leaf_size=self.leaf)
+            #     check = np.reshape([mapx, mapy], (1, -1))  # turn into a 2D row array
+            #     ind = neighbourhood.query_radius(check, r=self.radius)  # check neighbours in radius
+            #     close = ind[0]  # index from the single colour list
+            #     if close.size != 0:
+            #         on_map = True
+            #         # update the index of the matched cone on the track
+            #         if det.colour == Cone.BLUE:
+            #             self.mu, self.Sigma, self.track = update(
+            #                 self.track, self.blue_indexes[close[0]], det.sense_rb, self.Q, self.mu, self.Sigma
+            #             )
+            #         elif det.colour == Cone.YELLOW:
+            #             self.mu, self.Sigma, self.track = update(
+            #                 self.track, self.yellow_indexes[close[0]], det.sense_rb, self.Q, self.mu, self.Sigma
+            #             )
+            #         elif det.colour == Cone.ORANGE_BIG:
+            #             self.mu, self.Sigma, self.track = update(
+            #                 self.track, self.orange_indexes[close[0]], det.sense_rb, self.Q, self.mu, self.Sigma
+            #             )
 
             if not on_map:
                 if self.track == []:  # first in this list
-                    self.track = np.array([mapx, mapy, det.colour])
+                    self.track = np.array([mapx, mapy, det.colour, 1])
                     self.track = np.reshape(self.track, (1, -1))  # turn 2D
                 else:  # otherwise append vertically
-                    self.track = np.vstack([self.track, [mapx, mapy, det.colour]])
-                # get i relative to the whole track
-                if det.colour == 0:
-                    self.blue_indexes.append(len(self.track) - 1)
-                elif det.colour == 1:
-                    self.yellow_indexes.append(len(self.track) - 1)
-                elif det.colour == 2:
-                    self.orange_indexes.append(len(self.track) - 1)
+                    self.track = np.vstack([self.track, [mapx, mapy, det.colour, 1]])
                 # initialise new landmark
                 self.mu, self.Sigma = init_landmark(det.sense_rb, self.Q, self.mu, self.Sigma)
+
+                # # get i relative to the whole track
+                # if det.colour == Cone.BLUE:
+                #     self.blue_indexes.append(len(self.track) - 1)
+                # elif det.colour == Cone.YELLOW:
+                #     self.yellow_indexes.append(len(self.track) - 1)
+                # elif det.colour == Cone.ORANGE_BIG:
+                #     self.orange_indexes.append(len(self.track) - 1)
 
         # TODO: Ordering track boundary cone lines from start to finish
 
@@ -247,12 +300,13 @@ class EKFSlam(Node):
 
             curr_cov: np.ndarray = self.Sigma[cov_i : cov_i + 2, cov_i : cov_i + 2]  # 2x2 covariance to plot
 
-            cone_msg = Cone(location=ROSPoint(x=cone[0], y=cone[1], z=0.0), color=int(cone[2]))
+            cone_msg = Cone(location=Point(x=cone[0], y=cone[1], z=0.0), color=int(cone[2]))
             cone_cov = curr_cov.flatten().tolist()
 
             track_msg.cones.append(ConeWithCovariance(cone=cone_msg, covariance=cone_cov))
 
         self.slam_publisher.publish(track_msg)
+        self.flush_map()
 
 
 def main(args=None):
