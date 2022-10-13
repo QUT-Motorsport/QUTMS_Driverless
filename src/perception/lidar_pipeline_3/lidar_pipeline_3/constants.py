a83b7dd9f385806fb07e98d53b2129d5911a6f7c
Liam Ferrante <liam.p.ferrante@gmail.com>
2022-10-13 12:16:52 +1000

    Added constants for visualiser

--- src/perception/lidar_pipeline_3/lidar_pipeline_3/constants.py
+++ src/perception/lidar_pipeline_3/lidar_pipeline_3/constants.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import math
 import pathlib
 
 # Config Parameters
@@ -8,10 +9,22 @@ FIGURES_DIR: str = "/figures"
 
 # Algorithm Parameters
 LIDAR_RANGE = 22.5  # Max range of points to process (metres)
+DELTA_ALPHA = (2 * math.pi) / 128  # Delta angle of segments
+BIN_SIZE = 0.14  # Size of bins
 
-# Misc
-class Colour(Enum):
-    MINT = (0.188, 0.992, 0.761, 1.0)  # 30fdc3
-    BLUE = (0.612, 0.863, 0.996, 1.0)  # 9cdcfe
-    LIGHT_GREY = (0.145, 0.181, 0.247, 1.0)  # 252e3f
-    DARK_GREY = (0.122, 0.149, 0.188, 1.0)  # 1f2630
+# Visualiser
+# Default Values
+dark_grey = (0.122, 0.149, 0.188, 1.0)  # 1f2630
+light_grey = (0.145, 0.181, 0.247, 1.0)  # 252e3f
+blue = (0.612, 0.863, 0.996, 1.0)  # 9cdcfe
+mint = (0.188, 0.992, 0.761, 1.0)  # 30fdc3
+
+# Custom Colours
+mint_hex = "#30fdc3"
+yellow_hex = "#F4D44D"
+red_hex = "#F45060"
+blue_hex = "#636EFA"
+
+# Colour Sets
+colours_01 = [mint_hex, yellow_hex, red_hex, blue_hex]
+colours_01 = [blue_hex, yellow_hex, red_hex]
