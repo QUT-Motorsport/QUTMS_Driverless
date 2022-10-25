e424f27e9ad886bb6f0df00209ba65f4b46088f6
Alastair Bradford <albradford2468@gmail.com>
2022-10-25 18:16:33 +1000

    fixes

--- src/common/driverless_common/setup.py
+++ src/common/driverless_common/setup.py
@@ -12,8 +12,8 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        (os.path.join("share", package_name), glob("launch/*.launch.py")),
-        (os.path.join("share", package_name), glob("rviz/*.rviz")),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
+        (os.path.join("share", package_name, "rviz"), glob("rviz/*.rviz")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
