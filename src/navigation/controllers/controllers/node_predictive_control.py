31614ef0d2612ac8df69ca4f67c5adb885e7d602
Alastair Bradford <albradford2468@gmail.com>
2022-11-02 01:26:37 +1000

    so this is a cool controller now, smoother than reactive and doesnt need path fitting

--- src/navigation/controllers/controllers/node_predictive_control.py
+++ src/navigation/controllers/controllers/node_predictive_control.py
@@ -1,185 +1,179 @@
-from math import atan2, cos, pi, sin, sqrt
+from math import atan, atan2, pi, sqrt
+import time
 
+import cv2
+import numpy as np
+
+from cv_bridge import CvBridge
+import message_filters
 import rclpy
 from rclpy.node import Node
 from rclpy.publisher import Publisher
 
 from ackermann_msgs.msg import AckermannDrive
 from driverless_msgs.msg import Cone, ConeDetectionStamped
+from geometry_msgs.msg import TwistWithCovarianceStamped
+from sensor_msgs.msg import Image
 
-from driverless_common.point import Point
+from driverless_common.draw import *
+from driverless_common.point import Point, cone_to_point, dist
 
 from typing import List, Optional, Tuple
 
 Colour = Tuple[int, int, int]
 
+# translate ROS image messages to OpenCV
+cv_bridge = CvBridge()
 
 ORIGIN = Point(0, 0)
 
 LEFT_CONE_COLOUR = Cone.BLUE
 RIGHT_CONE_COLOUR = Cone.YELLOW
 
-
-def dist(a: Point, b: Point) -> float:
-    return sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2)
+WEIGHT = -1
 
 
-def cone_to_point(cone: Cone) -> Point:
-    return Point(
-        cone.location.x,
-        cone.location.y,
-    )
+def closest_point_on_curve(point: Point, curve: np.ndarray) -> Point:
+    # find the closest point on the curve to the cone
+    closest_point = ORIGIN
+    min_dist = float("inf")
+    for i in range(len(curve)):
+        p = Point(i / 10, curve[i])
+        d = dist(point, p)
+        if d < min_dist:
+            min_dist = d
+            closest_point = p
+    return closest_point
 
 
-class PredictiveController(Node):
-    Kp_ang: float = -2.5
-    Kp_vel: float = 2
-    vel_max: float = 2  # m/s = 7.2km/h
-    vel_min: float = vel_max / 2  # m/s
+class BetterReactiveController(Node):
     throttle_max: float = 0.1
     prev_steering_angle: float = 0
+    vel_max: float = 8  # m/s = 7.2km/h
     Kp_prev: float = 0.4
+    Kp_dist: float = 0.05
+    Kp_angle: float = -20
+    # create black image
+    debug_img = np.zeros((HEIGHT, WIDTH, 3), dtype=np.uint8)
 
     def __init__(self):
-        super().__init__("predictive_controller")
+        super().__init__("better_reactive_controller")
 
-        ebs_test = self.declare_parameter("ebs_control", False).get_parameter_value().bool_value
-        self.get_logger().info("EBS Control: " + str(ebs_test))
-        if ebs_test:
-            self.Kp_ang = -0.1  # shallow steering, straight line
-            self.vel_max = 45 / 3.6  # 40km/h in m/s
-            self.Kp_vel = 1
-            self.vel_min = self.vel_max / 2  # m/s
-            self.throttle_max = 0.2
+        # self.create_subscription(ConeDetectionStamped, "/vision/cone_detection", self.callback, 1)
 
+        # debug image
+        self.create_subscription(Image, "/debug_imgs/vision_det_img", self.img_callback, 1)
         # sync subscribers
-        self.create_subscription(ConeDetectionStamped, "/vision/cone_detection", self.callback, 1)
+        vel_sub = message_filters.Subscriber(self, TwistWithCovarianceStamped, "/imu/velocity")
+        detection_sub = message_filters.Subscriber(self, ConeDetectionStamped, "/vision/cone_detection")
+        synchronizer = message_filters.ApproximateTimeSynchronizer(
+            fs=[detection_sub, vel_sub],
+            queue_size=30,
+            slop=0.3,
+        )
+        synchronizer.registerCallback(self.callback)
+
+        self.debug_img_publisher: Publisher = self.create_publisher(Image, "/debug_imgs/control_img", 1)
 
         # publishers
         self.control_publisher: Publisher = self.create_publisher(AckermannDrive, "/driving_command", 1)
 
-        self.get_logger().info("---Reactive Controller Node Initalised---")
+        self.get_logger().info("---Better Reactive Controller Node Initalised---")
 
-    def callback(self, cone_msg: ConeDetectionStamped):
+    def img_callback(self, img_msg: Image):
+        self.debug_img = cv_bridge.imgmsg_to_cv2(img_msg, desired_encoding="bgr8")
+
+    def callback(self, cone_msg: ConeDetectionStamped, vel_msg: TwistWithCovarianceStamped):
         self.get_logger().debug("Received detection")
+        start: float = time.perf_counter()  # begin a timer
 
         # safety critical, set to 0 if not good detection
         control_msg = AckermannDrive()
         control_msg.speed = 0.0
         control_msg.steering_angle = 0.0
         control_msg.acceleration = 0.0
-        control_msg.jerk = 1.0  # use as brake
-
-        # weighting for track bounds
-        left_weight = 0
-        right_weight = 0
+        control_msg.jerk = 0.0  # use as brake
 
         cones: List[Cone] = cone_msg.cones
         left_cones = [c for c in cones if c.color == LEFT_CONE_COLOUR]
         right_cones = [c for c in cones if c.color == RIGHT_CONE_COLOUR]
 
-        # when blue cones is not empty
-        if left_cones:
-            left_weight = left_cones / len(cones)
-        # when yellow cones is not empty
-        if left_cones:
-            right_weight = -left_cones / len(cones)
+        # compute 10 quadratic curves based on different steering angles
+        # find the best curve that reduces the error from curve to each cone
+        # use the steering angle of that curve
+        A: np.ndarray = np.linspace(-0.1, 0.1, 15)
+        x: np.ndarray = np.linspace(0, 10, 100)
+        curves: List[np.ndarray] = []
+        # compute the error for each curve
+        errors: List[float] = []
+        for a in A:
+            y = a * x**2
+            curves.append(y)
 
-        # weight each cone by distance from origin
-        left_distances = [dist(ORIGIN, cone_to_point(c)) for c in left_cones]
-        right_distances = [dist(ORIGIN, cone_to_point(c)) for c in right_cones]
+            error = 0
+            for cone in left_cones:
+                # find the closest point on the curve to the cone
+                closest_point: Point = closest_point_on_curve(cone_to_point(cone), y)
+                # compute the error
+                error += dist(cone_to_point(cone), closest_point) * WEIGHT / len(left_cones)
 
-        # sum weighted distances
-        # less weight to further away cones - inverse proportion
-        left_weighted_distance = sum([1 / d * left_weight for d in left_distances])
-        right_weighted_distance = sum([1 / d * right_weight for d in right_distances])
+            for cone in right_cones:
+                # find the closest point on the curve to the cone
+                closest_point: Point = closest_point_on_curve(cone_to_point(cone), y)
+                # compute the error
+                error += dist(cone_to_point(cone), closest_point) * -WEIGHT / len(right_cones)
 
-        closest_left: Optional[Cone] = None
-        closest_right: Optional[Cone] = None
-        if len(left_cones) > 1:
-            # grab second closest
-            closest_left = sorted(left_cones, key=lambda c: dist(ORIGIN, cone_to_point(c)))[1]
-        elif len(left_cones) > 0:
-            closest_left = left_cones[0]
+            errors.append(error)
 
-        if len(right_cones) > 1:
-            closest_right = sorted(right_cones, key=lambda c: dist(ORIGIN, cone_to_point(c)))[1]
-        elif len(right_cones) > 0:
-            closest_right = right_cones[0]
+            for i in range(len(x)):
+                # draw each element in target spline
+                cv2.drawMarker(
+                    self.debug_img,
+                    loc_to_img_pt(x[i], y[i]).to_tuple(),
+                    (0, 0, 255),
+                    markerType=cv2.MARKER_SQUARE,
+                    markerSize=1,
+                    thickness=2,
+                )
 
-        closest_left: Optional[Cone] = None
-        closest_right: Optional[Cone] = None
-        if len(left_cones) > 1:
-            # grab second closest
-            closest_left = sorted(left_cones, key=lambda c: dist(ORIGIN, cone_to_point(c)))[1]
-        elif len(left_cones) > 0:
-            closest_left = left_cones[0]
+        # error += self.Kp_prev * self.prev_steering_angle
 
-        if len(right_cones) > 1:
-            closest_right = sorted(right_cones, key=lambda c: dist(ORIGIN, cone_to_point(c)))[1]
-        elif len(right_cones) > 0:
-            closest_right = right_cones[0]
-
-        # if we have two cones, check if they are greater than 5 meters apart
-        if closest_left is not None and closest_right is not None:
-            if dist(cone_to_point(closest_left), cone_to_point(closest_right)) > 5:
-                # if so - remove the furthest cone from the targeting
-                left_dist = dist(ORIGIN, cone_to_point(closest_left))
-                right_dist = dist(ORIGIN, cone_to_point(closest_right))
-                if left_dist <= right_dist:
-                    closest_right = None
-                else:
-                    closest_left = None
-
-        target: Optional[Point] = None
-        if closest_left is not None and closest_right is not None:
-            target = Point(
-                x=closest_left.location.x + (closest_right.location.x - closest_left.location.x) / 2,
-                y=closest_left.location.y + (closest_right.location.y - closest_left.location.y) / 2,
+        # find the best curve
+        best_curve: np.ndarray = curves[np.abs(errors).argmin()]
+        for i in range(len(x)):
+            # draw each element in target spline
+            cv2.drawMarker(
+                self.debug_img,
+                loc_to_img_pt(x[i], best_curve[i]).to_tuple(),
+                (0, 255, 0),
+                markerType=cv2.MARKER_SQUARE,
+                markerSize=1,
+                thickness=2,
             )
-        elif closest_left is not None:
-            target = Point(
-                x=closest_left.location.x,
-                y=closest_left.location.y - 2,
-            )
-        elif closest_right is not None:
-            target = Point(
-                x=closest_right.location.x,
-                y=closest_right.location.y + 2,
-            )
+        self.debug_img_publisher.publish(cv_bridge.cv2_to_imgmsg(self.debug_img, encoding="bgr8"))
 
-        if target is not None:
-            # steering control
-            steering_angle = self.Kp_ang * ((pi / 2) - atan2(target.x, target.y))
-            self.get_logger().debug(f"Target angle: {steering_angle}")
+        best_curve_steering_angle: float = A[np.abs(errors).argmin()] * self.Kp_angle
 
-            # # velocity control
-            # vel = sqrt(vel_msg.twist.twist.linear.x**2 + vel_msg.twist.twist.linear.y**2)
-            # self.get_logger().info(f"True vel: {vel}")
+        # velocity control
+        vel = sqrt(vel_msg.twist.twist.linear.x**2 + vel_msg.twist.twist.linear.y**2)
+        # increase proportionally as it approaches target
+        throttle_scalar: float = 1 - (vel / self.vel_max)
+        if throttle_scalar > 0:
+            calc_throttle = self.throttle_max * throttle_scalar
+        elif throttle_scalar <= 0:
+            calc_throttle = 0.0  # if its over maximum, cut throttle
 
-            # # target velocity proportional to angle
-            # target_vel: float = self.vel_max - (abs(steering_angle)) * self.Kp_vel
-            # if target_vel < self.vel_min:
-            #     target_vel = self.vel_min
-            # self.get_logger().info(f"Target vel: {target_vel}")
-
-            # # increase proportionally as it approaches target
-            # throttle_scalar: float = 1 - (vel / target_vel)
-            # if throttle_scalar > 0:
-            #     calc_throttle = self.throttle_max * throttle_scalar
-            # elif throttle_scalar <= 0:
-            #     calc_throttle = 0.0  # if its over maximum, cut throttle
-
-            # publish message
-            control_msg.steering_angle = steering_angle
-            control_msg.acceleration = self.throttle_max
-
+        # publish message
+        control_msg.steering_angle = best_curve_steering_angle
+        control_msg.acceleration = calc_throttle
         self.control_publisher.publish(control_msg)
+
+        self.get_logger().info(f"Total Time: {str(time.perf_counter() - start)}\n")  # log time
 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = ReactiveController()
+    node = BetterReactiveController()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
