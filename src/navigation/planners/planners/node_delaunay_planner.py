d62956d87af8f9ec4180ef6f631aa56082498970
Alastair Bradford <albradford2468@gmail.com>
2022-11-05 21:33:56 +1000

    got midline spines working. takes centre of each crossing line. now publishes del lines too

--- src/navigation/planners/planners/node_delaunay_planner.py
+++ src/navigation/planners/planners/node_delaunay_planner.py
@@ -1,21 +1,29 @@
 from math import pi
+import time
 
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial import Delaunay
+from scipy.interpolate import UnivariateSpline
 
 import rclpy
 from rclpy.node import Node
 from rclpy.publisher import Publisher
 
 from driverless_msgs.msg import Cone, ConeWithCovariance, PathPoint, PathStamped, TrackDetectionStamped
+from geometry_msgs.msg import Point
+from visualization_msgs.msg import Marker
+from std_msgs.msg import ColorRGBA
 
-from typing import List
+from driverless_common.marker import delaunay_marker_msg
+
+from typing import List, Tuple
 
 LEFT_CONE_COLOUR = Cone.BLUE
 RIGHT_CONE_COLOUR = Cone.YELLOW
 
 MAX_CONE_DISTANCE = 10
+S = 0.1
 
 
 def is_internal(simplice: np.ndarray, track: np.ndarray) -> bool:
@@ -45,6 +53,44 @@ def angle(v1: np.ndarray, v2: np.ndarray, v3: np.ndarray) -> float:
     return np.arccos(dot / norm)
 
 
+def approximate_b_spline_path(
+    x: np.ndarray, 
+    y: np.ndarray, 
+    n_path_points: int
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """
+    ADAPTED FROM: https://github.com/AtsushiSakai/PythonRobotics/blob/master/PathPlanning/BSplinePath/bspline_path.py \n
+    """
+
+    distances = calc_distance_vector(x, y)
+
+    spl_i_x = UnivariateSpline(distances, x, k=3, s=S)
+    spl_i_y = UnivariateSpline(distances, y, k=3, s=S)
+
+    sampled = np.linspace(0.0, distances[-1], n_path_points)
+    return evaluate_spline(sampled, spl_i_x, spl_i_y)
+
+
+def calc_distance_vector(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    dx, dy = np.diff(x), np.diff(y)
+    distances = np.cumsum([np.hypot(idx, idy) for idx, idy in zip(dx, dy)])
+    distances: np.ndarray = np.concatenate(([0.0], distances))
+    distances /= distances[-1]
+    return distances
+
+
+def evaluate_spline(sampled: np.ndarray, spl_i_x: UnivariateSpline, spl_i_y: UnivariateSpline):
+    x = spl_i_x(sampled)
+    y = spl_i_y(sampled)
+    dx = spl_i_x.derivative(1)(sampled)
+    dy = spl_i_y.derivative(1)(sampled)
+    heading = np.arctan2(dy, dx)
+    ddx = spl_i_x.derivative(2)(sampled)
+    ddy = spl_i_y.derivative(2)(sampled)
+    curvature = (ddy * dx - ddx * dy) / np.power(dx * dx + dy * dy, 2.0 / 3.0)
+    return np.array(x), y, heading, curvature,
+
+
 class TrackPlanner(Node):
     fig = plt.figure()
     ax = fig.add_subplot()
@@ -57,11 +103,13 @@ class TrackPlanner(Node):
 
         # publishers
         self.path_publisher: Publisher = self.create_publisher(PathStamped, "/planner/path", 1)
+        self.delaunay_publisher: Publisher = self.create_publisher(Marker, "/markers/delaunay_lines", 1)
 
         self.get_logger().info("---Delaunay Planner Node Initalised---")
 
     def callback(self, track_msg: TrackDetectionStamped):
         self.get_logger().debug("Received track")
+        start = time.perf_counter()
 
         cones_with_cov: List[ConeWithCovariance] = track_msg.cones
 
@@ -80,19 +128,20 @@ class TrackPlanner(Node):
         # # compute delaunay triangulation
         tri = Delaunay(track)
         # only get internal triangles
-        internal_triangles = [t for t in tri.simplices if is_internal(t, track)]
+        internal_triangles = np.array([t for t in tri.simplices if is_internal(t, track)])
 
         # only take triangles with all sides shorter than MAX_CONE_DISTANCE
-        internal_triangles = [
+        internal_triangles = np.array([
             t
             for t in internal_triangles
             if np.linalg.norm(track[t[0]] - track[t[1]]) < MAX_CONE_DISTANCE
             and np.linalg.norm(track[t[1]] - track[t[2]]) < MAX_CONE_DISTANCE
             and np.linalg.norm(track[t[2]] - track[t[0]]) < MAX_CONE_DISTANCE
-        ]
+        ])
 
         # get all lines that go to a different coloured cone
-        lines = []
+        lines: list = []
+        disp_lines: list = []
         for t in internal_triangles:
             # get triangle vertices
             v1 = track[t[0]]
@@ -104,20 +153,78 @@ class TrackPlanner(Node):
             c3 = cones[t[2]].color
             # check if any are different
             if c1 != c2:
-                lines.append([v1, v2])
+                lines.append(np.vstack((v1, v2)))
             if c2 != c3:
-                lines.append([v2, v3])
+                lines.append(np.vstack((v2, v3)))
             if c3 != c1:
-                lines.append([v3, v1])
-        lines = np.array(lines)
+                lines.append(np.vstack((v3, v1)))
+            disp_lines.append(np.vstack((v1, v2)))
+            disp_lines.append(np.vstack((v2, v3)))
+            disp_lines.append(np.vstack((v3, v1)))
+
+        # get midpoint of each line
+        midpoints = np.array([(l[0] + l[1]) / 2 for l in lines])
+        # get unique midpoints
+        midpoints = np.unique(midpoints, axis=0)
+
+        # order midpoints by distance to each other
+        ordered_midpoints = [midpoints[0]]
+        midpoints = np.delete(midpoints, 0, axis=0)
+        while len(midpoints) != 0:
+            # get closest midpoint to last ordered midpoint
+            closest = np.argmin(np.linalg.norm(midpoints - ordered_midpoints[-1], axis=1))
+            # add to ordered midpoints
+            ordered_midpoints.append(midpoints[closest])
+            # remove from midpoints
+            midpoints = np.delete(midpoints, closest, axis=0)
+        ordered_midpoints = np.array(ordered_midpoints)
+
+        # print(len(midpoints))
+        if len(ordered_midpoints) <= 3:
+            curvatures = np.zeros(len(ordered_midpoints))
+            path = np.hstack((ordered_midpoints, curvatures.reshape(-1, 1)))
+
+        else:
+            # interpolate midpoints with a B-Spline
+            points = len(ordered_midpoints) * 4
+            rix, riy, heading, curvature = approximate_b_spline_path(ordered_midpoints[:, 0], ordered_midpoints[:, 1], points)
+            path = np.vstack((rix, riy, curvature)).T
+        
+        # publish path
+        path_msg: list[PathPoint] = []
+        for i in path:
+            path_point = PathPoint()
+            path_point.location = Point(x=i[0], y=i[1], z=0.0)
+            path_point.turn_intensity = 0.0
+            path_msg.append(path_point)
+
+        self.path_publisher.publish(PathStamped(path=path_msg))
+
+        # print(f"Time taken: {time.perf_counter() - start}")
+
+        # publish delaunay lines
+        # make pairs of points for each line
+        delaunay_lines = []
+        track_colours = []
+        for l in disp_lines:
+            delaunay_lines.append(Point(x=l[0][0], y=l[0][1], z=0.0))
+            delaunay_lines.append(Point(x=l[1][0], y=l[1][1], z=0.0))
+            track_colours.append(ColorRGBA(r=0.1, g=0.1, b=0.1, a=0.2))
+            track_colours.append(ColorRGBA(r=0.1, g=0.1, b=0.1, a=0.2))
+
+        # make marker message
+        marker_msg = delaunay_marker_msg(delaunay_lines, track_colours)
+        self.delaunay_publisher.publish(marker_msg)
 
         # make plot
         self.ax.clear()
         self.ax.triplot(track[:, 0], track[:, 1], internal_triangles)
-        self.ax.plot(track[:, 0], track[:, 1], "or", markersize=6)
-        # self.ax.plot(lines[:, :, 0], lines[:, :, 1], "g", linewidth=2)
+        for line in lines:
+            self.ax.plot(line[:, 0], line[:, 1], "g-")
+        self.ax.plot(ordered_midpoints[:, 0], ordered_midpoints[:, 1], "ro")
+        self.ax.plot(path[:,0], path[:,1], "r-")
 
-        plt.pause(0.05)
+        plt.pause(0.03)
         plt.show(block=False)
 
 
