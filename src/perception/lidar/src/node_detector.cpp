c395672318bcab876b295fe59039566d45d3c93a
Alastair Bradford <albradford2468@gmail.com>
2022-11-09 14:16:49 +1000

    DB scan clustering

--- src/perception/lidar/src/node_detector.cpp
+++ src/perception/lidar/src/node_detector.cpp
@@ -1,51 +1,47 @@
-#include "rclcpp/rclcpp.hpp"
-#include "rclcpp/clock.hpp"
-#include <rclcpp/logging.hpp>
-#include <rclcpp/time.hpp>
-
-#include <ctime>
-
-#include "sensor_msgs/msg/point_cloud2.hpp"
-
-#include <pcl_conversions/pcl_conversions.h>
-
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
-#include <pcl/point_cloud.h>
 #include <pcl/filters/crop_box.h>
 #include <pcl/filters/extract_indices.h>
 #include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/io/pcd_io.h>
 #include <pcl/kdtree/kdtree.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
-#include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/segmentation/extract_clusters.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+#include <ctime>
+#include <rclcpp/logging.hpp>
+#include <rclcpp/time.hpp>
 
 #include "../PCL_DBSCAN/dbscan.hpp"
+#include "rclcpp/clock.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
 
 using std::placeholders::_1;
 
-class LiDARProcessor : public rclcpp::Node{
+class LiDARProcessor : public rclcpp::Node {
    private:
     rclcpp::Clock::SharedPtr rosclock = this->get_clock();
-    
+
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr cloud_sub;
 
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr ground_pub;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr non_ground_pub;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr cluster_pub;
 
-    void cloud_callback(const sensor_msgs::msg::PointCloud2::ConstPtr &msg)
-    {
+    void cloud_callback(const sensor_msgs::msg::PointCloud2::ConstPtr& msg) {
         clock_t start = clock();
         // Process the point cloud
         pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
         pcl::fromROSMsg(*msg, *cloud);
 
-        // Create the filtering object 
-        pcl::PointCloud<pcl::PointXYZI>::Ptr cropped (new pcl::PointCloud<pcl::PointXYZI>);
-        pcl::PointCloud<pcl::PointXYZI>::Ptr ground_crop (new pcl::PointCloud<pcl::PointXYZI>);
-        pcl::PointCloud<pcl::PointXYZI>::Ptr non_ground_crop (new pcl::PointCloud<pcl::PointXYZI>);
+        // Create the filtering object
+        pcl::PointCloud<pcl::PointXYZI>::Ptr cropped(new pcl::PointCloud<pcl::PointXYZI>);
+        pcl::PointCloud<pcl::PointXYZI>::Ptr ground_crop(new pcl::PointCloud<pcl::PointXYZI>);
+        pcl::PointCloud<pcl::PointXYZI>::Ptr non_ground_crop(new pcl::PointCloud<pcl::PointXYZI>);
 
         // Crop window of interest
         pcl::CropBox<pcl::PointXYZI> scan_cropper;
@@ -82,17 +78,16 @@ class LiDARProcessor : public rclcpp::Node{
         seg.setInputCloud(ground_crop);
         seg.segment(*inliers, *coefficients);
 
-        if (inliers->indices.size() == 0)
-        {
+        if (inliers->indices.size() == 0) {
             RCLCPP_INFO(this->get_logger(), "Could not estimate a planar model for this scan.");
             return;
         }
-        
+
         pcl::PointCloud<pcl::PointXYZI>::Ptr ground_seg(new pcl::PointCloud<pcl::PointXYZI>);
         pcl::PointCloud<pcl::PointXYZI>::Ptr non_ground_seg(new pcl::PointCloud<pcl::PointXYZI>);
         // extract ground points and non-ground points
         pcl::ExtractIndices<pcl::PointXYZI> extract;
-        pcl::PointIndices::Ptr filtered_idxs (new pcl::PointIndices ());
+        pcl::PointIndices::Ptr filtered_idxs(new pcl::PointIndices());
         extract.setInputCloud(ground_crop);
         extract.setIndices(inliers);
         // ground seg
@@ -113,7 +108,7 @@ class LiDARProcessor : public rclcpp::Node{
         // // Euclidean clustering objects
         // std::vector<pcl::PointIndices> cluster_indices;
         // pcl::EuclideanClusterExtraction<pcl::PointXYZI> ec;
-        // ec.setClusterTolerance(0.6); 
+        // ec.setClusterTolerance(0.6);
         // ec.setMinClusterSize(20);
         // ec.setMaxClusterSize(200);
         // ec.setSearchMethod(tree);
@@ -122,7 +117,8 @@ class LiDARProcessor : public rclcpp::Node{
 
         // pcl::PointCloud<pcl::PointXYZI>::Ptr clusters(new pcl::PointCloud<pcl::PointXYZI>);
         // int j = 0;
-        // for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
+        // for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end
+        // (); ++it)
         // {
         //     pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZI>);
         //     for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); ++pit)
@@ -131,14 +127,33 @@ class LiDARProcessor : public rclcpp::Node{
         //     cloud_cluster->height = 1;
         //     cloud_cluster->is_dense = true;
 
-        //     std::cout << "PointCloud representing the Cluster: " << cloud_cluster->size () << " data points." << std::endl;
-        //     j++;
-        //     *clusters += *cloud_cluster;
+        //     std::cout << "PointCloud representing the Cluster: " << cloud_cluster->size () << " data points." <<
+        //     std::endl; j++; *clusters += *cloud_cluster;
         // }
 
-        
+        // pointcloud to vector of tuples
+        std::vector<std::tuple<float, float, float>> points;
+        for (auto point : non_ground->points) {
+            points.push_back(std::make_tuple(point.x, point.y, point.z));
+        }
         // DBSCAN clustering
+        std::vector<std::vector<size_t>> db_idxs = dbscan(points, 1, 8);
 
+        // Create clusters
+        pcl::PointCloud<pcl::PointXYZI>::Ptr clusters(new pcl::PointCloud<pcl::PointXYZI>);
+        for (auto idxs : db_idxs) {
+            pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_cluster(new pcl::PointCloud<pcl::PointXYZI>);
+            for (auto idx : idxs) {
+                cloud_cluster->push_back((*non_ground)[idx]);
+            }
+            cloud_cluster->width = cloud_cluster->size();
+            cloud_cluster->height = 1;
+            cloud_cluster->is_dense = true;
+
+            std::cout << "PointCloud representing the Cluster: " << cloud_cluster->size() << " data points."
+                      << std::endl;
+            *clusters += *cloud_cluster;
+        }
 
         // Publish the ground point cloud
         auto ground_msg = std::make_shared<sensor_msgs::msg::PointCloud2>();
@@ -169,7 +184,7 @@ class LiDARProcessor : public rclcpp::Node{
         ground_pub = this->create_publisher<sensor_msgs::msg::PointCloud2>("/velodyne/ground", 1);
         non_ground_pub = this->create_publisher<sensor_msgs::msg::PointCloud2>("/velodyne/non_ground", 1);
         cluster_pub = this->create_publisher<sensor_msgs::msg::PointCloud2>("/velodyne/clusters", 1);
-        RCLCPP_INFO(this->get_logger(),"---Initialised Lidar Detector Node---");
+        RCLCPP_INFO(this->get_logger(), "---Initialised Lidar Detector Node---");
     }
 };
 
