16d5ef3f9ec1b341ad031d91e71e583d7ea77bed
Alistair English <alistair.english@protonmail.com>
2022-11-09 16:10:48 +1000

    extern n other lies

--- src/common/can/include/canopen.hpp
+++ src/common/can/include/canopen.hpp
@@ -9,9 +9,4 @@ void sdo_read(uint8_t node_id, uint16_t index, uint8_t sub_index, uint32_t *can_
 void sdo_write(uint8_t node_id, uint16_t index, uint8_t sub_index, uint8_t *data, size_t data_size,
                uint32_t *can_packet_id, uint8_t *out);
 
-std::map<uint8_t, size_t> can_open_size_map = {
-    {1, 0x4F},
-    {2, 0x4B},
-    {3, 0x47},
-    {4, 0x43},
-};
+extern std::map<uint8_t, size_t> can_open_size_map;
