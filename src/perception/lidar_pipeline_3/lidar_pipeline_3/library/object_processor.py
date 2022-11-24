2a1ac12f4c10b9eb90b1bb01247731ae5dd5975b
Liam Ferrante <liam.p.ferrante@gmail.com>
2022-11-24 18:04:51 +1000

    Aight this is significantly better and is doing what I gave up on last time

--- src/perception/lidar_pipeline_3/lidar_pipeline_3/library/object_processor.py
+++ src/perception/lidar_pipeline_3/lidar_pipeline_3/library/object_processor.py
@@ -29,6 +29,40 @@ def group_points(object_points):
     return object_centers, objects
 
 
+def reconstruct_objects_2(point_cloud, segments, bins, object_centers, objects):
+    obj_norms = np.linalg.norm(object_centers, axis=1)
+    obj_segs, obj_bins = pcp.get_discretised_positions(object_centers[:, 0], object_centers[:, 1], obj_norms)
+
+    # Upside down floor devision
+    bin_search_half = -((const.CONE_DIAM // const.BIN_SIZE) // -2)
+    seg_widths = -((2 * (const.BIN_SIZE * obj_bins) * np.tan(const.DELTA_ALPHA / 2)) // -2)
+    seg_search_half = np.floor_divide(const.CONE_DIAM, seg_widths)
+
+    reconstructed_objs = []
+    for i in range(object_centers.shape[0]):
+        curr_seg = obj_segs[i]
+        curr_bin = obj_bins[i]
+
+        segments_ind = (curr_seg - seg_search_half[i] <= segments) * (segments <= curr_seg + seg_search_half[i])
+        bins_ind = (curr_bin - bin_search_half <= bins) * (bins <= curr_bin + bin_search_half)
+        search_points = point_cloud[segments_ind * bins_ind]
+
+        distances = np.linalg.norm(
+            np.column_stack((search_points["x"], search_points["y"])) - object_centers[i], axis=1
+        )
+        matching_points = search_points[distances <= const.CONE_DIAM / 2]
+        matching_points = np.vstack((matching_points, objects[i]))
+
+        zeros = np.zeros((objects[i].shape[0], 1))
+        test = np.hstack([objects[i], zeros])
+        obj = np.vstack([xyz[norms <= CONE_DIAM / 2], test])
+        unq_obj = np.unique(obj, axis=0)
+
+        reconstructed_objs.append(matching_points)  # add error margin?
+
+    return reconstructed_objs
+
+
 def reconstruct_objects(point_cloud, object_centers, objects, DELTA_ALPHA, CONE_DIAM, BIN_SIZE):
     # center_norms = np.linalg.norm(object_centers, axis=1)
     # segment_widths = 2 * np.multiply(center_norms, np.tan(DELTA_ALPHA / 2))
