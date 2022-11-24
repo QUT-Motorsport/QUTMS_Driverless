a6924e42d83f8de05a26fdc829873da8d714c93a
Liam Ferrante <liam.p.ferrante@gmail.com>
2022-11-24 17:20:34 +1000

    Created get_bin and fit_error functions

--- src/perception/lidar_pipeline_3/lidar_pipeline_3/library/ground_plane_estimator.py
+++ src/perception/lidar_pipeline_3/lidar_pipeline_3/library/ground_plane_estimator.py
@@ -5,3 +5,22 @@ import numpy as np
 
 from .. import constants as const
 from .cy_library import total_least_squares as tls
+
+# Returns bin idx of a point from its norm
+def get_bin(norm, BIN_SIZE):
+    return math.floor(norm / BIN_SIZE)
+
+# Returns the RMSE of a line fit to a set of points
+def fit_error(m, b, points):
+    num_points = len(points)
+
+    sse = 0
+    for i in range(num_points):
+        x = points[i][0]
+        best_fit = m * x + b
+
+        observed = points[i][1]
+        sse += (best_fit - observed) ** 2
+
+    # root mean square return
+    return math.sqrt(sse / num_points)
