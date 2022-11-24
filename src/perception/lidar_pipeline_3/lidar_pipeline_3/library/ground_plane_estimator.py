37308d019ac6de1ee93e8bcb052544a36f60b068
Liam Ferrante <liam.p.ferrante@gmail.com>
2022-11-24 17:21:13 +1000

    Created get_ground_lines function

--- src/perception/lidar_pipeline_3/lidar_pipeline_3/library/ground_plane_estimator.py
+++ src/perception/lidar_pipeline_3/lidar_pipeline_3/library/ground_plane_estimator.py
@@ -24,3 +24,67 @@ def fit_error(m, b, points):
 
     # root mean square return
     return math.sqrt(sse / num_points)
+
+# The Incremental Algorithm
+def get_ground_lines(proto_seg_points):
+    estimated_lines = []
+    new_line_points = []
+    lines_created = 0
+
+    idx = 0
+    while idx < len(proto_seg_points):
+        m_new = None
+        b_new = None
+
+        new_point = proto_seg_points[idx]
+        if len(new_line_points) >= 2:
+            new_line_points.append(new_point)
+
+            [m_new, b_new] = tls.fit_line(new_line_points)
+
+            m_b_check = abs(m_new) <= const.T_M and (abs(m_new) > const.T_M_SMALL or abs(b_new) <= const.T_B)
+            if not (m_b_check and fit_error(m_new, b_new, new_line_points) <= const.T_RMSE):
+                new_line_points.pop()  # Remove the point we just added
+
+                [m_new, b_new] = tls.fit_line(new_line_points)
+
+                m_b_check = abs(m_new) <= const.T_M and (abs(m_new) > const.T_M_SMALL or abs(b_new) <= const.T_B)
+                if m_b_check and fit_error(m_new, b_new, new_line_points) <= const.T_RMSE:
+                    estimated_lines.append(
+                        (
+                            m_new,
+                            b_new,
+                            new_line_points[0],
+                            new_line_points[-1],
+                            get_bin(new_line_points[0][0], const.BIN_SIZE),
+                        )
+                    )
+                    lines_created += 1
+
+                new_line_points = []
+
+                if const.REGRESS_BETWEEN_BINS:
+                    idx -= 2
+                else:
+                    idx -= 1
+
+        else:
+            if (
+                len(new_line_points) == 0
+                or math.atan((new_point[1] - new_line_points[-1][1]) / (new_point[0] - new_line_points[-1][0]))
+                <= const.T_M
+            ):
+                new_line_points.append(new_point)
+
+        idx += 1
+
+    if len(new_line_points) > 1 and m_new != None and b_new != None:
+        estimated_lines.append(
+            (m_new, b_new, new_line_points[0], new_line_points[-1], get_bin(new_line_points[0][0], const.BIN_SIZE))
+        )
+
+    # If no ground lines were identified in segment, return 0
+    if len(estimated_lines) > 0:
+        return estimated_lines
+    else:
+        return 0
