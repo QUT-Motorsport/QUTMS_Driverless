fc77e67e90763e3c7db6e55f33719bdf25e7c7e5
Alastair Bradford <albradford2468@gmail.com>
2022-11-24 21:08:47 +1000

    fixed stuff, spline going

--- src/navigation/controllers/controllers/node_reactive_vector_control.py
+++ src/navigation/controllers/controllers/node_reactive_vector_control.py
@@ -1,24 +1,27 @@
-import numpy as np
+from math import sqrt
 
+from colour import Color
+import cv2
+import numpy as np
+from scipy.interpolate import UnivariateSpline
+
+from cv_bridge import CvBridge
+import message_filters
 import rclpy
 from rclpy.node import Node
 from rclpy.publisher import Publisher
 
 from ackermann_msgs.msg import AckermannDrive
 from driverless_msgs.msg import Cone, ConeDetectionStamped, Reset
+from geometry_msgs.msg import TwistStamped
+from sensor_msgs.msg import Image
+from visualization_msgs.msg import Marker, MarkerArray
 
+from driverless_common.draw import draw_map, draw_markers, draw_steering, loc_to_img_pt
+from driverless_common.marker import marker_array_from_cone_detection, marker_array_from_map, path_marker_msg
 from driverless_common.point import Point, cone_to_point, dist
-from driverless_common.draw import coord_to_img_pt
 
 from typing import List, Optional, Tuple
-
-import cv2
-from sensor_msgs.msg import Image
-from colour import Color
-from cv_bridge import CvBridge
-from visualization_msgs.msg import Marker, MarkerArray
-from driverless_common.draw import draw_map, draw_markers, draw_steering
-from driverless_common.marker import marker_array_from_cone_detection, marker_array_from_map, path_marker_msg
 
 cv_bridge = CvBridge()  # translate ROS image messages to OpenCV
 
@@ -34,20 +37,81 @@ ORIGIN = Point(0, 0)
 LEFT_CONE_COLOUR = Cone.BLUE
 RIGHT_CONE_COLOUR = Cone.YELLOW
 
-left_mid_col = (245,215,66)
-right_mid_col = (66,194,245)
+left_mid_col = (245, 215, 66)
+right_mid_col = (66, 194, 245)
+S = 0.5
+
+
+def approximate_b_spline_path(
+    x: np.ndarray, y: np.ndarray, n_path_points: int, order: int
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """
+    ADAPTED FROM: https://github.com/AtsushiSakai/PythonRobotics/blob/master/PathPlanning/BSplinePath/bspline_path.py \n
+    Approximates a B-Spline path through the given points.
+    * param x: The x coordinates of the cones.
+    * param y: The y coordinates of the cones.
+    * param n_path_points: The number of spline points to generate.
+    * return: The x, y, heading and curvature of points on the spline.
+    """
+
+    distances = calc_distance_vector(x, y)
+
+    spl_i_x = UnivariateSpline(distances, x, k=order, s=S)
+    spl_i_y = UnivariateSpline(distances, y, k=order, s=S)
+
+    sampled = np.linspace(0.0, distances[-1], n_path_points)
+    return evaluate_spline(sampled, spl_i_x, spl_i_y)
+
+
+def calc_distance_vector(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    """
+    Calculates the distance vector between the given points.
+    * param x: The x coordinates of the points.
+    * param y: The y coordinates of the points.
+    * return: The distance vector between the given points.
+    """
+    dx, dy = np.diff(x), np.diff(y)
+    distances = np.cumsum([np.hypot(idx, idy) for idx, idy in zip(dx, dy)])
+    distances: np.ndarray = np.concatenate(([0.0], distances))
+    distances /= distances[-1]
+    return distances
+
+
+def evaluate_spline(sampled: np.ndarray, spl_i_x: UnivariateSpline, spl_i_y: UnivariateSpline):
+    """
+    Evaluates the given spline at the given points.
+    * param sampled: The points to evaluate the spline at.
+    * param spl_i_x: The x spline.
+    * param spl_i_y: The y spline.
+    * return: The x, y, heading and curvature of points on the spline.
+    """
+    x = spl_i_x(sampled)
+    y = spl_i_y(sampled)
+    dx = spl_i_x.derivative(1)(sampled)
+    dy = spl_i_y.derivative(1)(sampled)
+    heading = np.arctan2(dy, dx)
+    ddx = spl_i_x.derivative(2)(sampled)
+    ddy = spl_i_y.derivative(2)(sampled)
+    curvature = (ddy * dx - ddx * dy) / np.power(dx * dx + dy * dy, 2.0 / 3.0)
+    return (
+        np.array(x),
+        y,
+        heading,
+        curvature,
+    )
 
 
 class VectorReactiveController(Node):
-    Kp_ang: float = 2
+    Kp_ang: float = 0.5
     Kp_vel: float = 2
-    vel_max: float = 2  # m/s = 7.2km/h
+    vel_max: float = 4  # m/s = 7.2km/h
     vel_min: float = vel_max / 2  # m/s
     throttle_max: float = 0.2
     target_cone_count = 3
     r2d: bool = True
-    in_dist: float = 1.5 # m
-    mid_dist: float = 5 # m
+    in_dist: float = 2  # m
+    mid_dist: float = 5  # m
+    prev_angle: float = 0
 
     def __init__(self):
         super().__init__("vector_reactive_controller")
@@ -61,15 +125,25 @@ class VectorReactiveController(Node):
             self.vel_min = self.vel_max / 2  # m/s
             self.throttle_max = 0.2
 
-        self.create_subscription(ConeDetectionStamped, "/vision/cone_detection", self.callback, 1)
-        #self.create_subscription(ConeDetectionStamped, "/slam/local", self.callback, 1)
+        # subscribers for sim
+        vel_sub = message_filters.Subscriber(self, TwistStamped, "/imu/velocity")
+        detection_sub = message_filters.Subscriber(self, ConeDetectionStamped, "/sim/cone_detection")
+        synchronizer = message_filters.ApproximateTimeSynchronizer(
+            fs=[detection_sub, vel_sub],
+            queue_size=30,
+            slop=0.3,
+        )
+        synchronizer = message_filters.ApproximateTimeSynchronizer(fs=[detection_sub, vel_sub], queue_size=20, slop=0.2)
+        synchronizer.registerCallback(self.callback)
+
+        # for on car
+        # self.create_subscription(ConeDetectionStamped, "/slam/local", self.callback, 1)
 
         self.reset_sub = self.create_subscription(Reset, "/reset", self.reset_callback, 10)
 
         self.control_publisher: Publisher = self.create_publisher(AckermannDrive, "/driving_command", 1)
 
         self.vector_publisher: Publisher = self.create_publisher(Image, "/debug_imgs/vector_reactive_img", 1)
-
 
         self.get_logger().info("---Reactive Controller Node Initalised---")
         self.get_logger().info("---Awaing Ready to Drive command *OVERRIDDEN*---")
@@ -78,7 +152,8 @@ class VectorReactiveController(Node):
         self.prev_steering_angle = 0
         self.r2d = True
 
-    def callback(self, cone_msg: ConeDetectionStamped):
+    def callback(self, cone_msg: ConeDetectionStamped, vel_msg: TwistStamped):
+        # def callback(self, cone_msg: ConeDetectionStamped):
         self.get_logger().debug("Received detection")
 
         # safety critical, set to 0 if not good detection
@@ -86,7 +161,7 @@ class VectorReactiveController(Node):
         control_msg.speed = 0.0
         control_msg.steering_angle = 0.0
         control_msg.acceleration = 0.0
-        control_msg.jerk = 1.0
+        control_msg.jerk = 0.0
 
         cones: List[Cone] = cone_msg.cones
 
@@ -103,60 +178,129 @@ class VectorReactiveController(Node):
 
         if len(left_cones) > 0:
             closest_left = sorted(left_cones, key=lambda c: dist(ORIGIN, cone_to_point(c)))
-           
-            for i in range(0, len(closest_left)-1):
+
+            for i in range(0, len(closest_left) - 1):
                 p1 = cone_to_point(closest_left[i])
-                p2 = cone_to_point(closest_left[i+1])
-                diff = p2-p1
-                R = (diff / (dist(ORIGIN, diff))) * self.in_dist
-                temp = R
+                p2 = cone_to_point(closest_left[i + 1])
+
+                cv2.line(
+                    debug_img,
+                    loc_to_img_pt(p1.x, p1.y).to_tuple(),
+                    loc_to_img_pt(p2.x, p2.y).to_tuple(),
+                    left_mid_col,
+                    thickness=2,
+                )
+
+                diff = p2 - p1
+                mult = 1.0 / dist(ORIGIN, diff)
+                Rtemp = diff * mult
+
+                R = Rtemp * self.in_dist
+                temp = R * 1.0
                 temp.x = R.y
                 temp.y = -R.x
-                
-                midPoint = ((p1+p2) / 2.0) + temp 
+
+                self.get_logger().debug(f"{diff}, {Rtemp},{R}, {mult}")
+
+                midSeg = p1 + (diff * 0.5)
+                midPoint = midSeg + temp
                 midpoints.append(midPoint)
 
+                cv2.line(
+                    debug_img,
+                    loc_to_img_pt(midSeg.x, midSeg.y).to_tuple(),
+                    loc_to_img_pt(midPoint.x, midPoint.y).to_tuple(),
+                    (0, 255, 0),
+                    thickness=2,
+                )
+
                 cv2.drawMarker(
-                    debug_img, 
-                    coord_to_img_pt(midPoint.x, midPoint.y).to_tuple(), 
-                    left_mid_col, 
-                    markerType=cv2.MARKER_TRIANGLE_UP, 
-                    markerSize=5, 
-                    thickness=5
+                    debug_img,
+                    loc_to_img_pt(midPoint.x, midPoint.y).to_tuple(),
+                    left_mid_col,
+                    markerType=cv2.MARKER_TRIANGLE_UP,
+                    markerSize=5,
+                    thickness=5,
                 )
 
         if len(right_cones) > 0:
             closest_right = sorted(right_cones, key=lambda c: dist(ORIGIN, cone_to_point(c)))
 
-            for i in range(0, len(closest_right)-1):
+            for i in range(0, len(closest_right) - 1):
                 p1 = cone_to_point(closest_right[i])
-                p2 = cone_to_point(closest_right[i+1])
+                p2 = cone_to_point(closest_right[i + 1])
 
-                diff = p2-p1
-                R = (diff / (dist(ORIGIN, diff))) * self.in_dist
-                temp = R
-                temp.x = -R.y
-                temp.y = R.x
-                
-                midPoint = ((p1+p2) / 2.0) + temp 
-                midpoints.append(midPoint)
-
-                cv2.drawMarker(
-                    debug_img, 
-                    coord_to_img_pt(midPoint.x, midPoint.y).to_tuple(), 
-                    right_mid_col, 
-                    markerType=cv2.MARKER_TRIANGLE_UP, 
-                    markerSize=5, 
-                    thickness=2
+                cv2.line(
+                    debug_img,
+                    loc_to_img_pt(p1.x, p1.y).to_tuple(),
+                    loc_to_img_pt(p2.x, p2.y).to_tuple(),
+                    right_mid_col,
+                    thickness=2,
                 )
 
-        self.get_logger().debug(f"Midpoints: {midpoints}")
+                diff = p2 - p1
+                mult = 1.0 / dist(ORIGIN, diff)
+                Rtemp = diff * mult
+
+                R = Rtemp * self.in_dist
+                temp = R * 1.0
+                temp.x = -R.y
+                temp.y = R.x
+
+                self.get_logger().debug(f"{diff}, {Rtemp},{R}, {mult}")
+
+                midSeg = p1 + (diff * 0.5)
+                midPoint = midSeg + temp
+                midpoints.append(midPoint)
+
+                cv2.line(
+                    debug_img,
+                    loc_to_img_pt(midSeg.x, midSeg.y).to_tuple(),
+                    loc_to_img_pt(midPoint.x, midPoint.y).to_tuple(),
+                    (125, 125, 0),
+                    thickness=2,
+                )
+
+                cv2.drawMarker(
+                    debug_img,
+                    loc_to_img_pt(midPoint.x, midPoint.y).to_tuple(),
+                    right_mid_col,
+                    markerType=cv2.MARKER_TRIANGLE_UP,
+                    markerSize=5,
+                    thickness=2,
+                )
+
+        # self.get_logger().info(f"Midpoints: {midpoints}")
 
         target: Optional[Point] = None
 
         # if we found midpoints
         if len(midpoints) > 0:
+            orderSpline = len(midpoints)
+            if orderSpline == 1:
+                orderSpline = 1
+            else:
+                orderSpline = min(2, len(midpoints))
+
             midpoints = sorted(midpoints, key=lambda c: dist(ORIGIN, c))
+
+            x = [p.x for p in midpoints]
+            y = [p.y for p in midpoints]
+
+            splineResult = approximate_b_spline_path(x, y, 15, orderSpline)
+            splineX = splineResult[0]
+            splineY = splineResult[1]
+
+            self.get_logger().debug(f"Spline {splineX} {splineY}")
+
+            for i in range(0, len(splineX) - 1):
+                cv2.line(
+                    debug_img,
+                    loc_to_img_pt(splineX[i], splineY[i]).to_tuple(),
+                    loc_to_img_pt(splineX[i + 1], splineY[i + 1]).to_tuple(),
+                    (255, 0, 0),
+                    thickness=2,
+                )
 
             target = ORIGIN
             numMid = 0
@@ -169,37 +313,50 @@ class VectorReactiveController(Node):
 
             if numMid > 0:
                 # average points
-                target = target / numMid
+                target = target * (1 / numMid)
 
-            self.get_logger().debug(f"Target: {target}")
-
-
-        target: Optional[Point] = None
+            # self.get_logger().info(f"Target: {target}")
 
         if target is not None:
-            # steering control
-            steering_angle = self.Kp_ang * np.degrees(np.arctan2(target.y, target.x))
+            steering_angle = 0.0
+            if dist(ORIGIN, target) > 4:
+                steering_angle = self.prev_angle
+            else:
+                # steering control
+                steering_angle = self.Kp_ang * np.degrees(np.arctan2(target.y, target.x))
             self.get_logger().debug(f"Target angle: {steering_angle}")
 
+            self.prev_angle = steering_angle
+
             cv2.drawMarker(
-                debug_img, 
-                coord_to_img_pt(target.x, target.y), 
-                (0,255,0), 
-                markerType=cv2.MARKER_TRIANGLE_UP, 
-                markerSize=5, 
-                thickness=2
+                debug_img,
+                loc_to_img_pt(target.x, target.y).to_tuple(),
+                (0, 255, 0),
+                markerType=cv2.MARKER_TRIANGLE_UP,
+                markerSize=5,
+                thickness=2,
             )
 
-            debug_img = draw_steering(
-                debug_img, steering_angle, 0
-            )  # draw steering angle and vel data on image
+            debug_img = draw_steering(debug_img, steering_angle, 0)  # draw steering angle and vel data on image
 
             # publish message
-            control_msg.steering_angle = steering_angle
-            control_msg.acceleration = self.throttle_max
+            control_msg.steering_angle = -1.0 * steering_angle
+
+            # velocity control
+            calc_throttle = 0.2
+            vel = sqrt(vel_msg.twist.linear.x**2 + vel_msg.twist.linear.y**2)
+            # increase proportionally as it approaches target
+            throttle_scalar: float = 1 - (vel / self.vel_max)
+            if throttle_scalar > 0:
+                calc_throttle = self.throttle_max * throttle_scalar
+            elif throttle_scalar <= 0:
+                calc_throttle = 0.0  # if its over maximum, cut throttle
+
+            control_msg.acceleration = calc_throttle
 
         self.control_publisher.publish(control_msg)
         self.vector_publisher.publish(cv_bridge.cv2_to_imgmsg(debug_img, encoding="bgr8"))
+
 
 def main(args=None):
     rclpy.init(args=args)
