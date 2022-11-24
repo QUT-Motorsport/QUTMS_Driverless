39e057778d11b80bdf1bcbdc831f95544caf1c88
Liam Ferrante <liam.p.ferrante@gmail.com>
2022-11-24 18:04:16 +1000

    Added function from LP2 and trying to understand why half of it is commented out

--- src/perception/lidar_pipeline_3/lidar_pipeline_3/library/object_processor.py
+++ src/perception/lidar_pipeline_3/lidar_pipeline_3/library/object_processor.py
@@ -27,3 +27,32 @@ def group_points(object_points):
         )
 
     return object_centers, objects
+
+
+def reconstruct_objects(point_cloud, object_centers, objects, DELTA_ALPHA, CONE_DIAM, BIN_SIZE):
+    # center_norms = np.linalg.norm(object_centers, axis=1)
+    # segment_widths = 2 * np.multiply(center_norms, np.tan(DELTA_ALPHA / 2))
+    # num_segs_to_search = np.empty(segment_widths.size, dtype=int)
+    # np.ceil(np.divide(CONE_DIAM, segment_widths), out=num_segs_to_search, casting="unsafe")
+
+    # num_bins_to_search = CONE_DIAM / BIN_SIZE
+
+    # Which segments / bins to search around
+    # center_segs, center_bins = pcp.get_discretised_positions(object_centers[:, 0], object_centers[:, 1], center_norms)
+
+    # Hacky shit, getting points in search area
+    reconstructed_objects = []
+
+    xyz = np.column_stack((point_cloud["x"], point_cloud["y"], point_cloud["z"]))
+    for i in range(len(object_centers)):
+        diff = xyz - np.append(object_centers[i], 0)
+        norms = np.linalg.norm(diff, axis=1)
+
+        zeros = np.zeros((objects[i].shape[0], 1))
+        test = np.hstack([objects[i], zeros])
+        obj = np.vstack([xyz[norms <= CONE_DIAM / 2], test])
+        unq_obj = np.unique(obj, axis=0)
+        # print(obj.size, unq_obj.size)
+        reconstructed_objects.append(unq_obj)
+
+    return reconstructed_objects
