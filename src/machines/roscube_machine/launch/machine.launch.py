585d10bc32535fb075d4e9d013a296acadf57d61
Alistair English <alistair.english@protonmail.com>
2022-12-01 01:23:35 +1000

    turn sensors back on

--- src/machines/roscube_machine/launch/machine.launch.py
+++ src/machines/roscube_machine/launch/machine.launch.py
@@ -43,11 +43,11 @@ def generate_launch_description():
                 executable="lidar_perception",
             ),
             # MISSION CONTROL NODE HERE
-            # IncludeLaunchDescription(
-            #     launch_description_source=PythonLaunchDescriptionSource(
-            #         launch_file_path=str(get_package_share_path("sensors") / "launch" / "sensors.launch.py")
-            #     ),
-            # ),
+            IncludeLaunchDescription(
+                launch_description_source=PythonLaunchDescriptionSource(
+                    launch_file_path=str(get_package_share_path("sensors") / "launch" / "sensors.launch.py")
+                ),
+            ),
             IncludeLaunchDescription(
                 launch_description_source=PythonLaunchDescriptionSource(
                     launch_file_path=str(get_package_share_path("models") / "launch" / "robot_description.launch.py")
