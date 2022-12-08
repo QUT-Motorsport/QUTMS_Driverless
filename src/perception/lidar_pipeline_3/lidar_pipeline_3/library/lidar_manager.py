26b8766e980af81092e231d50b77d587afa0481b
Liam Ferrante <liam.p.ferrante@gmail.com>
2022-12-08 19:48:32 +1000

    vis

--- src/perception/lidar_pipeline_3/lidar_pipeline_3/library/lidar_manager.py
+++ src/perception/lidar_pipeline_3/lidar_pipeline_3/library/lidar_manager.py
@@ -100,24 +100,24 @@ def locate_cones(config, point_cloud, start_time):
 
     # Create visualisations
     if config.create_figures:
-        # vis.plot_point_cloud_2D(config, point_cloud, "01_PointCloud_2D")
-        # vis.plot_segments_2D(config, point_cloud, segments, "03_PointCloudSegments_2D")
-        # vis.plot_bins_2D(config, point_cloud, bins, "05_PointCloudBins_2D")
-        # vis.plot_segments_3D(config, point_cloud, segments, "04_PointCloudSegments_3D")
-        # vis.plot_bins_3D(config, point_cloud, bins, "06_PointCloudBins_3D")
-        # vis.plot_prototype_points_2D(config, proto_segs_arr, proto_segs, "07_PrototypePoints_2D")
-        # vis.plot_prototype_points_3D(config, proto_segs_arr, proto_segs, "08_PrototypePoints_3D")
-        # vis.plot_ground_plane_2D(config, ground_plane, proto_segs_arr, proto_segs, "09_GroundPlane_2D")
-        # vis.plot_ground_plane_3D(config, ground_plane, proto_segs_arr, proto_segs, "10_GroundPlane_3D")
-        # vis.plot_labelled_points_2D(config, point_cloud, point_labels, ground_plane, "11_LabelledPoints_2D")
-        # vis.plot_labelled_points_3D(
-        #   config, point_cloud, point_labels, ground_plane, "12_LabelledPoints_3D"
-        # )
-        # vis.plot_object_points_2D(config, object_points, "13_Object_Points_2D")
-        # vis.plot_object_centers_2D(config, object_points, object_centers, objects, object_line_dists, "14_Objects_2D")
-        # vis.plot_reconstructed_objects_2D(config, reconstructed_objects, reconstructed_centers, "14_Reconstructed_Objects")
-        # vis2.plot_cones_2D(config, point_cloud, point_labels, cone_centers, cone_points, "15_Cones")
-        # vis2.plot_cones_3D(config, point_cloud[point_norms <= 100], point_labels[point_norms <= 100], cones, "16_Cones_3D")
+        vis.plot_point_cloud_2D(config, point_cloud, "01_PointCloud_2D")
+        vis.plot_segments_2D(config, point_cloud, segments, "03_PointCloudSegments_2D")
+        vis.plot_bins_2D(config, point_cloud, bins, "05_PointCloudBins_2D")
+        vis.plot_segments_3D(config, point_cloud, segments, "04_PointCloudSegments_3D")
+        vis.plot_bins_3D(config, point_cloud, bins, "06_PointCloudBins_3D")
+        vis.plot_prototype_points_2D(config, proto_segs_arr, proto_segs, "07_PrototypePoints_2D")
+        vis.plot_prototype_points_3D(config, proto_segs_arr, proto_segs, "08_PrototypePoints_3D")
+        vis.plot_ground_plane_2D(config, ground_plane, proto_segs_arr, proto_segs, "09_GroundPlane_2D")
+        vis.plot_ground_plane_3D(config, ground_plane, proto_segs_arr, proto_segs, "10_GroundPlane_3D")
+        vis.plot_labelled_points_2D(config, point_cloud, point_labels, ground_plane, "11_LabelledPoints_2D")
+        vis.plot_labelled_points_3D(
+          config, point_cloud, point_labels, ground_plane, "12_LabelledPoints_3D"
+        )
+        vis.plot_object_points_2D(config, object_points, "13_Object_Points_2D")
+        vis.plot_object_centers_2D(config, object_points, object_centers, objects, object_line_dists, "14_Objects_2D")
+        vis.plot_reconstructed_objects_2D(config, reconstructed_objects, reconstructed_centers, "14_Reconstructed_Objects")
+        vis2.plot_cones_2D(config, point_cloud, point_labels, cone_centers, cone_points, "15_Cones")
+        vis2.plot_cones_3D(config, point_cloud[point_norms <= 100], point_labels[point_norms <= 100], cones, "16_Cones_3D")
         vis2.plot_detailed_2D(config, point_cloud, segments, bins, ground_plane[np.unique(segments)], point_labels, reconstructed_objects, reconstructed_centers, cone_intensities, cone_centers, cone_points, duration, "17_detailed_2D")
 
         # reintro structured array for lidar colouring
