e3f6484d350eec885f20d15dac947e193844b5ce
Alastair Bradford <albradford2468@gmail.com>
2022-12-22 23:53:06 +1000

    integrated sim reset into sim reset

--- src/common/sim_translators/sim_translators/node_control_to_sim.py
+++ src/common/sim_translators/sim_translators/node_control_to_sim.py
@@ -1,5 +1,5 @@
-import math
-from math import sqrt
+from math import pi, sqrt
+import time
 
 import message_filters
 import rclpy
@@ -7,10 +7,11 @@ from rclpy.node import Node
 from rclpy.publisher import Publisher
 
 from ackermann_msgs.msg import AckermannDriveStamped
+from driverless_msgs.msg import Reset
 from fs_msgs.msg import ControlCommand
 from nav_msgs.msg import Odometry
 
-from driverless_msgs import Reset
+from fs_msgs.srv import Reset as ResetService
 
 
 class ControlToSim(Node):
@@ -35,10 +36,25 @@ class ControlToSim(Node):
         # publish to sim
         self.publisher: Publisher = self.create_publisher(ControlCommand, "/fsds/control_command", 1)
 
+        # FSDS reset service
+        self.cli = self.create_client(ResetService, "/reset")
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("Reset srv not available, waiting again...")
+        self.req = ResetService.Request()
+
         self.get_logger().info("---Sim control translator initialised---")
 
     def reset_callback(self, reset_msg: Reset):
-        self.r2d = True
+        if self.r2d:
+            self.r2d = False
+            self.get_logger().info("Ready to drive: FALSE")
+        else:
+            self.get_logger().info("Ready to drive: TRUE")
+            time.sleep(1)
+            self.r2d = True
+
+        self.req.wait_on_last_task = False
+        self.future = self.cli.call_async(self.req)
 
     def callback(self, drive_msg: AckermannDriveStamped, vel_msg: Odometry):
         if not self.r2d:
@@ -52,7 +68,7 @@ class ControlToSim(Node):
         if calc_throttle <= 0:
             calc_throttle = 0.0  #  cut throttle
 
-        sim_control.steering = -0.1 * drive_msg.drive.steering_angle / math.pi
+        sim_control.steering = -0.04 * drive_msg.drive.steering_angle / pi
         sim_control.throttle = calc_throttle
         sim_control.brake = 0.0
         self.publisher.publish(sim_control)
