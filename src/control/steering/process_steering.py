e52e30a5c86dea0864e8e3b2ac58bf0d0212f0f5
Alastair <albradford2468@gmail.com>
2023-01-31 06:46:49 +1000

    data and processing

--- src/control/steering/process_steering.py
+++ src/control/steering/process_steering.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 # Read in the data
-actual = pd.read_csv('data/steering_reading copy 7.csv')
-desired = pd.read_csv('data/driving_command copy 7.csv')
+actual = pd.read_csv('src/control/steering/data/steering_reading copy 15.csv')
+desired = pd.read_csv('src/control/steering/data/driving_command copy 15.csv')
 
 # Plot the data on the same graph
 # Convert time to seconds from start
-actual['time'] = actual['time'] - actual['time'][0] - 0.3
+actual['time'] = actual['time'] - actual['time'][0]
 desired['time'] = desired['time'] - desired['time'][0]
 
 plt.plot(actual['time'], actual['steering_angle'], label='Actual')
@@ -15,4 +16,9 @@ plt.plot(desired['time'], desired['drive.steering_angle'], label='Desired')
 plt.legend()
 plt.xlabel('Time (s)')
 plt.ylabel('Steering Angle (deg)')
+
+plt.xticks(np.arange(0, max(actual['time']), 1))
+plt.yticks(np.arange(0, max(max(desired['drive.steering_angle']), max(actual['steering_angle'])), 2))
+
+plt.grid(True)
 plt.show()
