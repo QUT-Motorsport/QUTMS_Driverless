9eb5447fc9fc1290f4861f28e769fa9b2fad22ac
Liam Ferrante <liam.p.ferrante@gmail.com>
2023-02-07 12:20:40 +1000

    Typing and docstrings

--- src/perception/lidar_pipeline_3/lidar_pipeline_3/lidar_perception.py
+++ src/perception/lidar_pipeline_3/lidar_pipeline_3/lidar_perception.py
@@ -23,6 +23,15 @@ from .utils import Config
 
 
 def cone_msg(x: float, y: float) -> Cone:
+    """Create a Cone message from x and y coordinates.
+
+    Args:
+        x (float): The x coordinate of the cone (LiDAR sensor is origin).
+        y (float): The y coordinate of the cone (LiDAR sensor is origin).
+
+    Returns:
+        Cone: The cone message.
+    """
     location: Point = Point(x=x, y=y, z=const.LIDAR_HEIGHT_ABOVE_GROUND)
 
     # This LiDAR Pipeline does not identify cone colour
@@ -30,6 +39,11 @@ def cone_msg(x: float, y: float) -> Cone:
 
 
 class ConeDetectionNode(Node):
+    """Node for detecting cones and their locations from a point cloud.
+
+    Args:
+        Node (rclpy.node.Node): The ROS2 node class.
+    """
     def __init__(self, _config: Config) -> None:
         super().__init__("lidar_processor_node")
 
@@ -45,18 +59,23 @@ class ConeDetectionNode(Node):
         self.config.logger.info("Waiting for point cloud data...")
 
     def pc_callback(self, point_cloud_msg: PointCloud2) -> None:
+        """Callback function for when point cloud data is received.
+
+        Args:
+            point_cloud_msg (PointCloud2): The point cloud message.
+        """
         self.iteration += 1
 
         # Convert PointCloud2 message from LiDAR sensor to numpy array
-        start_time = time.perf_counter()
-        dtype_list = rnp.point_cloud2.fields_to_dtype(
+        start_time: float = time.perf_counter()
+        dtype_list: list = rnp.point_cloud2.fields_to_dtype(
             point_cloud_msg.fields, point_cloud_msg.point_step
         )  # x y z intensity ring
-        point_cloud = np.frombuffer(point_cloud_msg.data, dtype_list)
+        point_cloud: np.ndarray = np.frombuffer(point_cloud_msg.data, dtype_list)
 
-        cone_locations = lidar_manager.locate_cones(self.config, point_cloud, start_time)
+        cone_locations: np.ndarray = lidar_manager.locate_cones(self.config, point_cloud, start_time)
 
-        duration = time.perf_counter() - start_time
+        duration: float = time.perf_counter() - start_time
         self.average_runtime = (self.average_runtime * (self.iteration - 1) + duration) / self.iteration
         self.config.logger.info(
             f"Current Hz: {round(1 / duration, 2)}\t| Average Hz: {round(1 / self.average_runtime, 2)}"
@@ -65,13 +84,19 @@ class ConeDetectionNode(Node):
         if len(cone_locations) == 0:
             return
 
-        detected_cones = [cone_msg(cone[0], cone[1]) for cone in cone_locations]
+        detected_cones: list = [cone_msg(cone[0], cone[1]) for cone in cone_locations]
 
-        detection_msg = ConeDetectionStamped(header=point_cloud_msg.header, cones=detected_cones)
+        detection_msg: ConeDetectionStamped = ConeDetectionStamped(header=point_cloud_msg.header, cones=detected_cones)
         self.cone_publisher.publish(detection_msg)
 
 
 def real_time_stream(args: list, config: Config) -> None:
+    """Run the node for real-time data.
+
+    Args:
+        args (list): The arguments provided to the node.
+        config (Config): The configuration object.
+    """
     # Init node
     rclpy.init(args=args)
     cone_detection_node: ConeDetectionNode = ConeDetectionNode(config)
@@ -83,6 +108,9 @@ def real_time_stream(args: list, config: Config) -> None:
 
 
 def local_data_stream():
+    """Run the pipeline using exported point cloud frames instead of a ROS bag.
+    This is useful for debugging and testing (e.g., IDE breakpoints can be used).
+    """
     raise NotImplementedError()
 
 
@@ -96,7 +124,7 @@ def main(args=sys.argv[1:]):
         print("WARNING: --print_logs flag not specified")
         print("Running lidar_perception without printing to terminal...")
     else:
-        stdout_handler = logging.StreamHandler(sys.stdout)
+        stdout_handler: StreamHandler = logging.StreamHandler(sys.stdout)
         config.logger.addHandler(stdout_handler)
 
     # Log time and arguments provided
@@ -104,6 +132,7 @@ def main(args=sys.argv[1:]):
     config.logger.info(f"args = {args}")
 
     # Init data stream
+    # Check to see if video should be stitched instead of running pipeline
     if config.video_from_session:
         video_stitcher.stitch_figures(
             f"{const.OUTPUT_DIR}/{config.video_from_session}{const.FIGURES_DIR}",
