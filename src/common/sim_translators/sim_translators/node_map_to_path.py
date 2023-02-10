50fedf69c5ded5afd907054ab7a723c83d278509
Zac <zac.gillerat@hotmail.com>
2023-02-10 15:28:39 +1000

    map_to_path: Sort cones before creating splines.
    If the cones are unordered at the time splines are created, the splines will be incorrect resulting in an incorrect path. Therefore the cones must be sorted, starting from the far orange cone and ending at the close orange cone. This commit as an implementation of the sorting algorithm two-opt and uses it to sort the cones before the splines are created.

--- src/common/sim_translators/sim_translators/node_map_to_path.py
+++ src/common/sim_translators/sim_translators/node_map_to_path.py
@@ -101,6 +101,28 @@ def cone_array_with_covariance_to_cone_list(eufs_cones: ConeArrayWithCovariance)
     return internal_cones
 
 
+# Calculate the euclidian distance in n-space of the route r traversing cities c, ending at the path start.
+PATH_DISTANCE = lambda r,c: np.sum([np.linalg.norm(c[r[p]]-c[r[p-1]]) for p in range(len(r))])
+# Reverse the order of all elements from element i to element k in array r.
+TWO_OPT_SWAP = lambda r,i,k: np.concatenate((r[0:i],r[k:-len(r)+i-1:-1],r[k+1:len(r)]))
+
+def two_opt(cities,improvement_threshold): # 2-opt Algorithm adapted from https://en.wikipedia.org/wiki/2-opt
+    route = np.arange(cities.shape[0]) # Make an array of row numbers corresponding to cities.
+    improvement_factor = 1 # Initialize the improvement factor.
+    best_distance = PATH_DISTANCE(route,cities) # Calculate the distance of the initial path.
+    while improvement_factor > improvement_threshold: # If the route is still improving, keep going!
+        distance_to_beat = best_distance # Record the distance at the beginning of the loop.
+        for swap_first in range(1,len(route)-2): # From each city except the first and last,
+            for swap_last in range(swap_first+1,len(route)): # to each of the cities following,
+                new_route = TWO_OPT_SWAP(route,swap_first,swap_last) # try reversing the order of these cities
+                new_distance = PATH_DISTANCE(new_route,cities) # and check the total distance with this modification.
+                if new_distance < best_distance: # If the path distance is an improvement,
+                    route = new_route # make this the accepted best route
+                    best_distance = new_distance # and update the distance corresponding to this route.
+        improvement_factor = 1 - best_distance/distance_to_beat # Calculate how much the route has improved.
+    return route # When the route is no longer improving substantially, stop searching and return the route.
+
+
 class MapPathPlanner(Node):
     spline_len: int = 3999
 
@@ -108,7 +130,7 @@ class MapPathPlanner(Node):
         super().__init__("map_path_translator_node")
 
         # sub to track for all cone locations relative to car start point
-        self.create_subscription(ConeArrayWithCovariance, "/ground_truth/noisy_track", self.map_callback, 10)
+        self.create_subscription(ConeArrayWithCovariance, "/simulated_noise/track", self.map_callback, 10)
 
         # publishers
         self.track_publisher: Publisher = self.create_publisher(TrackDetectionStamped, "/sim/global_map", 1)
@@ -121,23 +143,18 @@ class MapPathPlanner(Node):
 
         cones = cone_array_with_covariance_to_cone_list(track_msg)
 
-        yellow_x: List[float] = []
-        yellow_y: List[float] = []
-        blue_x: List[float] = []
-        blue_y: List[float] = []
-        oranges: List[Cone] = []
-
+        yellows: List[Tuple[float, float]] = []
+        blues: List[Tuple[float, float]] = []
+        oranges: List[Tuple[float, float]] = []
         sim_track: List[ConeWithCovariance] = []
+
         for cone in cones:
             if cone.color == Cone.YELLOW:
-                yellow_x.append(cone.location.x)
-                yellow_y.append(cone.location.y)
-                cone.color = 4
+                yellows.append((cone.location.x, cone.location.y))
             elif cone.color == Cone.BLUE:
-                blue_x.append(cone.location.x)
-                blue_y.append(cone.location.y)
+                blues.append((cone.location.x, cone.location.y))
             elif cone.color == Cone.ORANGE_BIG:
-                oranges.append(cone)
+                oranges.append((cone.location.x, cone.location.y))
 
             new_cone = ConeWithCovariance()
             new_cone.cone.location = cone.location
@@ -145,26 +162,33 @@ class MapPathPlanner(Node):
             new_cone.covariance = [0.0, 0.0, 0.0, 0.0]
             sim_track.append(new_cone)
 
-        if len(yellow_x) != len(yellow_y) or len(blue_x) != len(blue_y):
-            self.get_logger().error("Cone coordinates must be balanced")
+        parsed_orange_cones = self.parse_orange_cones(oranges)
+        if len(parsed_orange_cones) == 0:
             return
+        blues.insert(0, parsed_orange_cones[0])
+        blues.append(parsed_orange_cones[1])
+        yellows.insert(0, parsed_orange_cones[2])
+        yellows.append(parsed_orange_cones[3])
 
-        # 4 orange cones: 2 blue side, 2 yellow side
-        for cone in oranges:
-            if cone.location.x > 6:  # far pair of cones
-                if cone.location.y > 0:  # blue side
-                    blue_x.insert(0, cone.location.x)
-                    blue_y.insert(0, cone.location.y)
-                else:  # yellow side
-                    yellow_x.insert(0, cone.location.x)
-                    yellow_y.insert(0, cone.location.y)
-            else:  # close pair of cones
-                if cone.location.y > 0:  # blue side
-                    blue_x.append(cone.location.x)
-                    blue_y.append(cone.location.y)
-                else:  # yellow side
-                    yellow_x.append(cone.location.x)
-                    yellow_y.append(cone.location.y)
+        # Sort the blue and yellow cones starting from the far orange cone, 
+        # and ending at the close orange cone.
+        blue_route = two_opt(np.asarray(blues), 0.001)
+        yellow_route = two_opt(np.asarray(yellows), 0.001)
+
+        blue_x = [blues[blue_route[i]][0] for i in range(len(blue_route))]
+        blue_y = [blues[blue_route[i]][1] for i in range(len(blue_route))]
+        yellow_x = [yellows[yellow_route[i]][0] for i in range(len(yellow_route))]
+        yellow_y = [yellows[yellow_route[i]][1] for i in range(len(yellow_route))]
+
+        # Check that the cones are ordered correctly, that they move, at least partially, 
+        # positively along the x axis, if they don't, reverse the order to correct it.
+        if blue_x[1] < blue_x[0]: 
+            blue_x = list(reversed(blue_x))
+            blue_y = list(reversed(blue_y))
+
+        if yellow_x[1] < yellow_x[0]: 
+            yellow_x = list(reversed(yellow_x))
+            yellow_y = list(reversed(yellow_y))
 
         # retrieves spline lists (x,y)
         yx, yy = approximate_b_spline_path(yellow_x, yellow_y, self.spline_len)
@@ -212,6 +236,26 @@ class MapPathPlanner(Node):
         sim_track_msg.header.frame_id = "track"
         sim_track_msg.cones = sim_track
         self.track_publisher.publish(sim_track_msg)
+
+    def parse_orange_cones(self, orange_cones: List[Tuple[float, float]]) -> List[Tuple[float, float]]:
+        """
+        Breaks the big orange starting cones into their position relative to the other blue/yellow cones. 
+        Returns format close_blue, far_blue, close_yellow, far_yellow. 
+        """
+        if len(orange_cones) != 4:
+            self.get_logger().fatal('parse_orange_cones called with less than 4 visible cones. Requires 4 cones.')
+            return []
+
+        blue_cones = [cone for cone in orange_cones if cone[1] > 0]
+        yellow_cones = [cone for cone in orange_cones if cone[1] < 0]
+
+        if blue_cones[0][0] > blue_cones[1][0]:
+            blue_cones[0], blue_cones[1] = blue_cones[1], blue_cones[0]
+
+        if yellow_cones[0][0] > yellow_cones[1][0]:
+            yellow_cones[0], yellow_cones[1] = yellow_cones[1], yellow_cones[0]
+
+        return [blue_cones[0], blue_cones[1], yellow_cones[0], yellow_cones[1]]
 
 
 def main(args=None):
