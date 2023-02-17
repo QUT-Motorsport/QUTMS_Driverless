a396d00464b7fb9864f32d4f4ee794ceacfa7db3
Liam Ferrante <liam.p.ferrante@gmail.com>
2023-02-17 14:39:02 +1000

    Moved to constants and plot utils

--- src/perception/lidar_pipeline_3/lidar_pipeline_3/library/visualiser_2.py
+++ src/perception/lidar_pipeline_3/lidar_pipeline_3/library/visualiser_2.py
@@ -1,6 +1,4 @@
 import math
-from PIL import Image
-from matplotlib import font_manager
 import matplotlib.colors as mpl_colors
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
@@ -8,21 +6,20 @@ import numpy as np
 
 from .. import constants as const
 from ..constants import RGBA, Colour
+from . import plot_utils as plt_u
 
-font_dirs = [const.WORKING_DIR + "/library/resources/fonts"]
-font_files = font_manager.findSystemFonts(fontpaths=font_dirs)
 
-for font_file in font_files:
-    font_manager.fontManager.addfont(font_file)
+def plot_point_cloud_2D(config, point_cloud, subtitle, name):
+    # Create Figure
 
-plt.rcParams["font.family"] = "Roboto"
+    plt_u.calibrate_axis(ax)
 
 
 def animate_figure():
     raise NotImplementedError
 
 
-def init_plot_2D(title, x_label, y_label):
+def plot_segments_2D(config, point_cloud, point_norms, segments, name):
     # Initialise figure
     fig, ax = plt.subplots(facecolor=RGBA.MS_BLUE.value)
 
@@ -31,12 +28,9 @@ def init_plot_2D(title, x_label, y_label):
     ax.set_xlabel(x_label, fontweight="bold")
     ax.set_ylabel(y_label, fontweight="bold")
 
-    # Colours
-    ax.set_facecolor(RGBA.DARK_GREY.value)
-    ax.xaxis.label.set_color(RGBA.MS_ORANGE.value)
-    ax.yaxis.label.set_color(RGBA.MS_ORANGE.value)
-    ax.tick_params(axis="x", colors=RGBA.WHITE.value)
-    ax.tick_params(axis="y", colors=RGBA.WHITE.value)
+        center_adjust = 0.5
+        if unique_segment < 0:
+            center_adjust = -0.5
 
     # Equal axis
     ax.set_aspect("equal")
@@ -44,74 +38,45 @@ def init_plot_2D(title, x_label, y_label):
     return fig, ax
 
 
-def init_plot_3D(title, xlabel, ylabel, zlabel):
-    # Initialise figure
-    fig = plt.figure(facecolor=RGBA.MS_BLUE.value)
-    ax = fig.add_subplot(projection="3d")
-    ax.grid(color="white")
+    ax.plot(
+        0,
+        0,
+        c=Colour.DARK_GREY.value,
 
-    # Strings
-    ax.set_title(title, color=RGBA.MS_ORANGE.value, fontweight="bold", fontsize=14)
-    ax.set_xlabel(xlabel, fontweight="bold")
-    ax.set_ylabel(ylabel, fontweight="bold")
-    ax.set_zlabel(zlabel, fontweight="bold")
+    plt_u.calibrate_axis(ax)
 
-    # Colours
-    ax.set_facecolor(RGBA.MS_BLUE.value)
-    ax.xaxis.set_pane_color(RGBA.DARK_GREY.value)
-    ax.yaxis.set_pane_color(RGBA.DARK_GREY.value)
-    ax.zaxis.set_pane_color(RGBA.DARK_GREY.value)
-    ax.xaxis.label.set_color(RGBA.MS_ORANGE.value)
-    ax.yaxis.label.set_color(RGBA.MS_ORANGE.value)
-    ax.zaxis.label.set_color(RGBA.MS_ORANGE.value)
-    ax.tick_params(axis="x", colors=RGBA.WHITE.value)
-    ax.tick_params(axis="y", colors=RGBA.WHITE.value)
-    ax.tick_params(axis="z", colors=RGBA.WHITE.value)
-    ax.xaxis._axinfo["grid"].update({"color": Colour.MS_BLUE.value})
-    ax.yaxis._axinfo["grid"].update({"color": Colour.MS_BLUE.value})
-    ax.zaxis._axinfo["grid"].update({"color": Colour.MS_BLUE.value})
+    ax.text(.01, .99, f'Segments: {unique_segments.shape[0]}', ha='left', va='top', c=Colour.WHITE.value, fontsize=8, transform=ax.transAxes)
 
-    # Equal axis
-    ax.set_aspect("equal")
-
-    # Set view anlge
-    ax.view_init(elev=30, azim=202)
-
-    return fig, ax
+    # Save Figure
+    plt_u.add_logo(fig, dpi=225, small=True)
 
 
-def add_colourbar(fig, plot, title, title_c, tick_c):
-    c_bar = fig.colorbar(plot)
-    c_bar.set_label(title, color=title_c, fontweight="bold", labelpad=10)
-    c_bar.ax.yaxis.set_tick_params(color=tick_c)
-    plt.setp(plt.getp(c_bar.ax.axes, "yticklabels"), color=tick_c)
+def plot_bins_2D(config, point_cloud, segments, bins, name):
 
 
-def add_logo(fig, dpi, small):
-    if small:
-        logo_size = 0.085
-    else:
-        logo_size = 0.125
-
-    # Add Logo
-    im = Image.open(const.WORKING_DIR + "/library/resources/qutms_logo_white.png")
-    #im = Image.open("C:/Users/liamf/Documents/Personal/Software Development/Python/QUTMS_Driverless/src/perception/lidar_pipeline_3/lidar_pipeline_3/library/resources/qutms_logo_white.png")
-    im_width, im_height = im.size
-
-    fig_width, fig_height = fig.get_size_inches() * dpi # fig.dpi
-    new_height = int(fig_height * logo_size)
-    im = im.resize((int(new_height * (im_width / im_height)), new_height))
-    fig.figimage(im, 10, 10, alpha=0.75, origin="upper", zorder=3)
+        if curr_bin == bins.max():
+            line_colour = RGBA.MS_ORANGE.value
 
 
-def calibrate_axis(ax, x=None, y=None, z=None):
-    if x is not None:
-        max_limit = max(np.abs([np.min(x), np.max(x), np.min(y), np.max(y), np.min(z), np.max(z)]))
-        ax.axes.set_zlim(-max_limit, max_limit)
-    else:
-        max_limit = max(np.abs([min(ax.get_xlim()), max(ax.get_xlim()), min(ax.get_ylim()), max(ax.get_ylim())]))
-    ax.axes.set_xlim(-max_limit, max_limit)
-    ax.axes.set_ylim(-max_limit, max_limit)
+    colour_set = [Colour.GREEN.value, Colour.LIGHT_BLUE.value, Colour.RED.value]
+    ax.scatter(
+        point_cloud["x"],
+        point_cloud["y"],
+        c=(bins % len(colour_set)),
+
+    ax.text(.01, .99, f'Max Bin Count: {bins.max()}', ha='left', va='top', c=Colour.MS_ORANGE.value, fontsize=8, transform=ax.transAxes)
+    ax.text(.01, .95, f'Bin Size: {const.BIN_SIZE}', ha='left', va='top', c=Colour.WHITE.value, fontsize=8, transform=ax.transAxes)
+    plt.savefig(f"{config.image_dir}/{name}.png", dpi=225)
+
+
+def plot_prototype_points_2D(config, point_norms, segments, proto_segs_arr, proto_segs, name):
+    unique_segments = np.unique(segments)
+    fig, ax = plt_u.init_plot_2D("Prototype Points", "X", "Y")
+
+
+        center_adjust = 0.5
+        if unique_segment < 0:
+            center_adjust = -0.5
 
 
 def plot_point_cloud_2D(config, point_cloud, subtitle, name):
