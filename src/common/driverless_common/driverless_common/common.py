78e9aa3aa07ce5eed7d5bef7690885e7b97e21ab
Alastair <albradford2468@gmail.com>
2023-02-19 19:26:48 +1000

    moved some functions into common. got pure pursuit working better

---
+++ src/common/driverless_common/driverless_common/common.py
@@ -0,0 +1,36 @@
+from math import atan2, cos, sin, sqrt
+
+import numpy as np
+
+from typing import List
+
+
+def dist(p1: List[float], p2: List[float]) -> float:
+    """
+    Retrieve distance between two points
+    * param p1: [x,y] coords of point 1
+    * param p2: [x,y] coords of point 2
+    * return: distance between points
+    """
+    return sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+
+
+def angle(p1: List[float], p2: List[float]) -> float:
+    """
+    Retrieve angle between two points
+    * param p1: [x,y] coords of point 1
+    * param p2: [x,y] coords of point 2
+    * return: angle in rads
+    """
+    x_disp = p2[0] - p1[0]
+    y_disp = p2[1] - p1[1]
+    return atan2(y_disp, x_disp)
+
+
+def wrap_to_pi(angle: float) -> float:
+    """
+    Wrap an angle between -pi and pi
+    * param angle: angle in rads
+    * return: angle in rads wrapped to -pi and pi
+    """
+    return (angle + np.pi) % (2 * np.pi) - np.pi
--- src/control/path_follower/path_follower/node_pure_pursuit.py
+++ src/control/path_follower/path_follower/node_pure_pursuit.py
@@ -1,8 +1,8 @@
 from math import atan2, cos, sin, sqrt
+import time
 
 import cv2
 import numpy as np
-import scipy.spatial
 from transforms3d.euler import quat2euler
 
 from cv_bridge import CvBridge
@@ -15,14 +15,13 @@ from driverless_msgs.msg import PathStamped
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from sensor_msgs.msg import Image
 
-from driverless_common.draw import draw_markers, draw_steering, loc_to_img_pt
-from driverless_common.point import Point, cone_to_point, dist
+from driverless_common.common import angle, dist, wrap_to_pi
+from driverless_common.draw import loc_to_img_pt
 from driverless_common.shutdown_node import ShutdownNode
 
 from typing import List, Tuple
 
-LOOKAHEAD = 10
-LOOKAHEAD_VEL = 10
+LOOKAHEAD = 4
 DEBUG = True
 SCALE = 80
 WIDTH = 20 * SCALE  # 15m either side
@@ -45,75 +44,49 @@ def get_wheel_position(pos_cog: List[float], heading: float) -> List[float]:
     x_axle = pos_cog[0] + cos(heading) * cog2axle
     y_axle = pos_cog[1] + sin(heading) * cog2axle
 
-    return [x_axle, y_axle]
+    return [x_axle, y_axle, heading]
 
 
 def get_RVWP(car_pos: List[float], path: np.ndarray, lookahead: float) -> np.ndarray:
     """
     Retrieve angle between two points
-    * param car_pos: [x,y] coords of point 1
+    * param car_pos: [x,y,theta] pose of the car
     * param path: [[x0,y0,i0],[x1,y1,i1],...,[xn-1,yn-1,in-1]] path points
     * param rvwp_lookahead: distance to look ahead for the RVWP
     * return: RVWP position as [x,y,i]
     """
-    pos = np.array([[car_pos[0], car_pos[1]]])
-    dists: np.ndarray = scipy.spatial.distance.cdist(
-        path[:, :2],  # search all path points for x,y cols up to 3rd col (intensity)
-        pos,
-        "euclidean",
-    )
-    min_index: int = np.where(dists == np.amin(dists))[0][0]
+    # find the closest point on the path to the car
+    close_dist: float = 100.0
+    for i, p in enumerate(path):
+        if dist(p, car_pos) < close_dist:
+            close_dist = dist(p, car_pos)
+            min_index = i
+    close = path[min_index]
 
-    # vwp_dists: np.ndarray = scipy.spatial.distance.cdist(
-    #     path[:, :2],  # search all path points for x,y cols up to 3rd col (intensity)
-    #     [path[min_index, :2]],
-    #     "euclidean",
-    # )
-    # vwp_dists = vwp_dists[vwp_dists > lookahead]
-    # rvwp = path[np.where(vwp_dists == np.amin(vp_dists))[0][0]]
-
-    if min_index + lookahead >= len(path):
-        rvwp_index: int = len(path) - 1
-    else:
-        rvwp_index: int = min_index + lookahead
-    # rvwp_index = np.argmin(np.abs(dists - lookahead))
-
-    print("close_index: ", min_index, "rvwp_index: ", rvwp_index, "num indices: ", len(path))
-    rvwp: np.ndarray = path[rvwp_index]
-
+    # find the first point on the path that is further than the lookahead distance
+    rvwp_dist: float = 100.0
+    for i, p in enumerate(path):
+        current_dist = dist(p, close)
+        if current_dist > lookahead and current_dist < rvwp_dist:
+            # get angle to check if the point is in front of the car
+            ang = angle(close, p)
+            if (car_pos[2] - ang) < np.pi / 2 and (car_pos[2] - ang) > -np.pi / 2:
+                rvwp_dist = current_dist
+                rvwp = path[i]
     return rvwp
-
-
-def angle(p1: List[float], p2: List[float]) -> float:
-    """
-    Retrieve angle between two points
-    * param p1: [x,y] coords of point 1
-    * param p2: [x,y] coords of point 2
-    * return: angle in rads
-    """
-    x_disp = p2[0] - p1[0]
-    y_disp = p2[1] - p1[1]
-    return atan2(y_disp, x_disp)
-
-
-def wrap_to_pi(angle: float) -> float:
-    """
-    Wrap an angle between -pi and pi
-    * param angle: angle in rads
-    * return: angle in rads wrapped to -pi and pi
-    """
-    return (angle + np.pi) % (2 * np.pi) - np.pi
 
 
 class PurePursuit(Node):
     path = np.array([])
-    Kp_ang: float = -16.0
+    Kp_ang: float = -5.0
     Kp_vel: float = 0.08
     vel_max: float = 2.0  # m/s
     vel_min: float = 2.0  # m/s
     throttle_max: float = 0.2
     brake_max: float = 0.12
     Kp_brake: float = 0.0
+    last_time = time.time()
+    count = 0
 
     def __init__(self):
         super().__init__("pure_pursuit_node")
@@ -153,22 +126,20 @@ class PurePursuit(Node):
 
         # rvwp control
         rvwp: List[float] = get_RVWP(position, self.path, LOOKAHEAD)
-        rvwp_close: List[float] = get_RVWP(position, self.path, 1)
-        # steering control
-        des_heading_ang = angle(position, [rvwp[0], rvwp[1]])
-        steering_angle = wrap_to_pi(theta - des_heading_ang) * self.Kp_ang
-        print(steering_angle * 3.1415 / 180 / (90 / 26))
 
-        debug_img = np.zeros((HEIGHT, WIDTH, 3), np.uint8)
+        des_heading_ang = angle(position, [rvwp[0], rvwp[1]])
+        error = wrap_to_pi(theta - des_heading_ang)
+        steering_angle = np.rad2deg(error) * self.Kp_ang
 
         if DEBUG:
+            debug_img = np.zeros((HEIGHT, WIDTH, 3), np.uint8)
             for i in range(0, len(self.path) - 1):
                 cv2.line(
                     debug_img,
                     loc_to_img_pt(self.path[i, 0] - OFFSET_X, self.path[i, 1] - OFFSET_Y).to_tuple(),
                     loc_to_img_pt(self.path[i + 1, 0] - OFFSET_X, self.path[i + 1, 1] - OFFSET_Y).to_tuple(),
                     (255, 0, 0),
-                    thickness=2,
+                    thickness=3,
                 )
 
             cv2.drawMarker(
@@ -176,20 +147,20 @@ class PurePursuit(Node):
                 loc_to_img_pt(rvwp[0] - OFFSET_X, rvwp[1] - OFFSET_Y).to_tuple(),
                 (0, 255, 0),
                 markerType=cv2.MARKER_TRIANGLE_UP,
-                markerSize=5,
-                thickness=2,
+                markerSize=7,
+                thickness=4,
             )
 
             cv2.drawMarker(
                 debug_img,
-                loc_to_img_pt(rvwp_close[0] - OFFSET_X, rvwp_close[1] - OFFSET_Y).to_tuple(),
+                loc_to_img_pt(position[0] - OFFSET_X, position[1] - OFFSET_Y).to_tuple(),
                 (0, 0, 255),
                 markerType=cv2.MARKER_TRIANGLE_UP,
-                markerSize=5,
-                thickness=2,
+                markerSize=7,
+                thickness=4,
             )
             text_angle = "Steering: " + str(round(steering_angle, 2))
-            cv2.putText(debug_img, text_angle, (10, HEIGHT - 25), cv2.FONT_HERSHEY_SIMPLEX, 5, (255, 255, 255), 2)
+            cv2.putText(debug_img, text_angle, (10, HEIGHT - 25), cv2.FONT_HERSHEY_SIMPLEX, 5, (255, 255, 255), 4)
 
             self.debug_publisher.publish(cv_bridge.cv2_to_imgmsg(debug_img, encoding="bgr8"))
 
@@ -197,8 +168,21 @@ class PurePursuit(Node):
         control_msg = AckermannDriveStamped()
         control_msg.drive.steering_angle = steering_angle
         control_msg.drive.speed = self.vel_min
-
         self.control_publisher.publish(control_msg)
+
+        self.count += 1
+        if self.count % 100 == 0:
+            print("cars coords", position)
+            print("rvwp coords", rvwp)
+            print("desired heading", np.rad2deg(des_heading_ang))
+            print("current heading", np.rad2deg(theta))
+            print("error", np.rad2deg(error))
+            print("wheel angles", np.rad2deg(steering_angle) / (90 / 26))
+            self.count = 0
+
+            print("Time: ", time.time() - self.last_time, "\n")
+
+        self.last_time = time.time()
 
 
 def main(args=None):  # begin ros node
