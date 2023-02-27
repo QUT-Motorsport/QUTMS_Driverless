ce5e8faec3e29c892993bcfa430e7286e4822376
Alastair <albradford2468@gmail.com>
2023-02-27 15:55:32 +1000

    fix urdf for cam tf

--- src/hardware/zed_driver/launch/zed2i.launch.py
+++ src/hardware/zed_driver/launch/zed2i.launch.py
@@ -31,7 +31,7 @@ def generate_launch_description():
     node_name = "zed_node"  # Zed Node name
     publish_urdf = "true"  # Publish static frames from camera URDF
     # Robot base frame. Note: overrides the parameter `pos_tracking.base_frame` in `common.yaml`.
-    base_frame = "base_link"
+    base_frame = "zed2i"
     # Position X of the camera with respect to the base frame [m].
     cam_pos_x = "0.0"
     # Position Y of the camera with respect to the base frame [m].
