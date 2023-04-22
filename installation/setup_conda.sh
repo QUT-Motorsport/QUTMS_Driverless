fdff8b7319ce4b4a57d6ae464b8ce4a3ee5f876f
b1n-ch1kn <albradford2468@gmail.com>
2023-04-22 21:24:52 +1000

    split install scripts

--- installation/setup_conda.sh
+++ installation/setup_conda.sh
@@ -12,21 +12,12 @@ sudo apt upgrade -y
 sudo apt install -y git python3-pip pre-commit mesa-utils
 
 ## Download and install mambaforge
-echo ""
-echo "---Installing mambaforge---"
-echo ""
-sleep 3
-wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh
-bash Mambaforge-$(uname)-$(uname -m).sh
-source ~/mambaforge/bin/activate
-conda config --set auto_activate_base false
-conda deactivate
-rm -rf Mambaforge-$(uname)-$(uname -m).sh
+source ~/QUTMS/QUTMS_Driverless/installation/setup_scripts/install_mambaforge.sh
 
+## Make workspace
 mkdir ~/QUTMS
 cd ~/QUTMS
-
-## Make directory for people to drop their rosbags in
+# directory for people to drop their rosbags in
 mkdir bags/
 
 ## Clone Driverless repo
@@ -35,79 +26,30 @@ echo "---Cloning Driverless repo---"
 echo ""
 sleep 3
 git clone --recurse-submodules https://github.com/QUT-Motorsport/QUTMS_Driverless.git
-cd QUTMS_Driverless
 
 ## Create driverless development environment
-echo ""
-echo "---Creating driverless env---"
-echo ""
-sleep 3
-mamba env create --name driverless_env --file installation/humble_py310_dev_env.yml
-conda activate driverless_env
-conda config --env --add channels conda-forge
-conda config --env --add channels robostack-staging
-mamba install -y --file installation/conda_requirements.txt
+source ~/QUTMS/QUTMS_Driverless/installation/setup_scripts/install_conda_env.sh
 
 ## Create an alias for ease
 echo "alias a='conda activate driverless_env && source install/setup.bash'" >> ~/.bashrc
 
 ## Check if user is going to work with vision
-echo ""
-echo "---Machine Learning Vision---"
-echo ""
-echo "Do you have an Nvidia GPU and are developing ML vision systems? ('yes' or 'no')"
-read torch
-echo ""
-if [ $torch == "yes" ]; then
-    mamba install -y pytorch torchvision pytorch-cuda=11.6 -c pytorch -c nvidia
-    echo "export LD_LIBRARY_PATH=/usr/lib/wsl/lib:$LD_LIBRARY_PATH" >> ~/.bashrc
-    echo "Download any ML models from the Google Drive into the models folder (see wiki)"
-    echo ""
-    sleep 7
-fi
+source ~/QUTMS/QUTMS_Driverless/installation/setup_scripts/install_torch_cuda.sh
 
 ## Install EUFS
-echo ""
-echo "---Installing EUFS---"
-echo ""
-sleep 3
-cd ~/QUTMS
-git clone https://github.com/QUT-Motorsport/eufs_sim.git
-git clone https://gitlab.com/eufs/eufs_msgs.git
-git clone https://github.com/QUT-Motorsport/eufs_rviz_plugins.git
-git clone https://github.com/QUT-Motorsport/qutms_msgs.git
-echo "export EUFS_MASTER=~/QUTMS" >> ~/.bashrc
-export EUFS_MASTER=~/QUTMS
-export QUTMS_WS=~/QUTMS
+source ~/QUTMS/QUTMS_Driverless/installation/setup_scripts/install_eufs_sim.sh
 
-## Build initial driverless packages
-echo ""
-echo "---Building Driverless packages---"
-echo ""
-sleep 3
-# colcon build --symlink-install --packages-up-to sim_translators mission_controller remote_control keyboard_control
+# echo ""
+# echo "---Building packages---"
+# echo ""
+# sleep 3
+# colcon build --symlink-install
 
 ## Pre commit for git
-echo ""
-echo "---Installing pre-commit---"
-echo ""
-sleep 3
-pre-commit install
+source ~/QUTMS/QUTMS_Driverless/installation/setup_scripts/install_pre-commit.sh
 
 ## Check if user wants to install WSL version of GitKraken
-echo ""
-echo "---GitKraken---"
-echo ""
-echo "Do you wish to use Gitkraken to manage your repo? If so it is recommended to install the WSL version now ('yes' or 'no')"
-read gitkraken
-echo ""
-if [ $gitkraken == "yes" ]; then
-    wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
-    sudo dpkg -i ./gitkraken-amd64.deb
-    sudo apt-get install -f -y
-    rm gitkraken-amd64.deb
-    echo ""
-fi
+source ~/QUTMS/QUTMS_Driverless/installation/install_gitkraken.sh
 
 ## Wrap up
 echo "Thank you for installing, explore the rest of the wiki"
--- installation/setup_standard.sh
+++ installation/setup_standard.sh
@@ -12,33 +12,15 @@ sudo apt upgrade -y
 sudo apt install -y git python3-pip pre-commit cmake
 
 ## Set ROS2 repositories
-echo ""
-echo "---Setting up ROS2 repositories---"
-echo ""
-sleep 3
-sudo apt install -y software-properties-common
-sudo add-apt-repository universe
-
-sudo apt update && sudo apt install curl
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+source ~/QUTMS/QUTMS_Driverless/installation/setup_scripts/install_ros_source.sh
 
 ## Install ROS2 Humble
-echo ""
-echo "---Installing ROS2 Humble---"
-echo ""
-sleep 3
-sudo apt update
-sudo apt install -y ros-humble-desktop ros-dev-tools
-sudo rosdep init
-rosdep update
-echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+source ~/QUTMS/QUTMS_Driverless/installation/setup_scripts/install_ros_humble.sh
 
+## Make workspace
 mkdir ~/QUTMS
 cd ~/QUTMS
-
-## Make directory for people to drop their rosbags in
+# directory for people to drop their rosbags in
 mkdir bags/
 
 ## Clone Driverless repo
@@ -60,68 +42,28 @@ pip install -r installation/requirements.txt
 echo "alias a='source install/setup.bash'" >> ~/.bashrc
 
 ## Check if user is going to work with vision
-echo ""
-echo "---Machine Learning Vision---"
-echo ""
-echo "Do you have an Nvidia GPU and are developing ML vision systems? ('yes' or 'no')"
-read torch
-echo ""
-if [ $torch == "yes" ]; then
-    pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu117
-    echo "export LD_LIBRARY_PATH=/usr/lib/wsl/lib:$LD_LIBRARY_PATH" >> ~/.bashrc
-    echo ""
-    echo "Download any ML models from the Google Drive into the models folder (see wiki)"
-    echo ""
-    sleep 7
-fi
+source ~/QUTMS/QUTMS_Driverless/installation/setup_scripts/install_torch_cuda.sh
 
 ## Source ROS
 source /opt/ros/humble/setup.bash
 
 ## Install EUFS
-echo ""
-echo "---Installing EUFS---"
-echo ""
-sleep 3
-cd ~/QUTMS
-git clone https://github.com/QUT-Motorsport/eufs_sim.git
-git clone https://gitlab.com/eufs/eufs_msgs.git
-git clone https://github.com/QUT-Motorsport/eufs_rviz_plugins.git
-git clone https://github.com/QUT-Motorsport/qutms_msgs.git
-echo "export EUFS_MASTER=~/QUTMS" >> ~/.bashrc
-export EUFS_MASTER=~/QUTMS
-export QUTMS_WS=~/QUTMS
+source ~/QUTMS/QUTMS_Driverless/installation/setup_scripts/install_eufs_sim.sh
 
 ## Install dependencies from src/
 rosdep install --from-paths ~/QUTMS --ignore-src -r -y
 
-echo ""
-echo "---Building packages---"
-echo ""
-sleep 3
-colcon build --symlink-install --packages-ignore 
+# echo ""
+# echo "---Building packages---"
+# echo ""
+# sleep 3
+# colcon build --symlink-install
 
 ## Pre commit for git
-echo ""
-echo "---Installing pre-commit---"
-echo ""
-sleep 3
-pre-commit install
+source ~/QUTMS/QUTMS_Driverless/installation/setup_scripts/install_pre-commit.sh
 
 ## Check if user wants to install WSL version of GitKraken
-echo ""
-echo "---GitKraken---"
-echo ""
-echo "Do you wish to use Gitkraken to manage your repo? If so it is recommended to install the WSL version now ('yes' or 'no')"
-read gitkraken
-echo ""
-if [ $gitkraken == "yes" ]; then
-    wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
-    sudo dpkg -i ./gitkraken-amd64.deb
-    sudo apt-get install -f -y
-    rm gitkraken-amd64.deb
-    echo ""
-fi
+source ~/QUTMS/QUTMS_Driverless/installation/install_gitkraken.sh
 
 ## Wrap up
 echo "Thank you for installing, explore the rest of the wiki"
