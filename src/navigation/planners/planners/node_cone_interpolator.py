cac469c6e7e886949e1de117e05825381516ed98
Damin-G <daminrg@hotmail.com>
2023-05-03 16:35:39 +1000

    Removed debugging and graphing, added comments

--- src/navigation/planners/planners/node_cone_interpolator.py
+++ src/navigation/planners/planners/node_cone_interpolator.py
@@ -69,12 +69,8 @@ class ConeInterpolator(Node):
     def map_callback(self, orderedCone_msg: ConeDetectionStamped):
         self.get_logger().debug("Received map")
 
+        # List of original cones
         ordered_cones: List[Cone] = orderedCone_msg.cones
-        # print("\nordered cones: \n\n", len(ordered_cones))
-        total_iCone_coords = []
-        originalCone_coords = []
-        for cone in range(len(ordered_cones)):
-            originalCone_coords.append((ordered_cones[cone].location.x, ordered_cones[cone].location.y))
 
         # increment numPoints_interpolated for divisions (n points = n + 1 divisions)
         numPoints_divisions = self.numPoints_interpolated + 1
@@ -83,17 +79,7 @@ class ConeInterpolator(Node):
         ordered_cones_length = len(ordered_cones) - 1
 
         # Interpolate cones between pairs of cones along a straight line
-        interpolation_has_begun = False
         for cone in range(ordered_cones_length):
-            # keep track of correct indexing whilst the list grows simultaneously
-            # if interpolation_has_begun:
-            #     cone += self.numPoints_interpolated
-            # else:
-            #     interpolation_has_begun = True
-
-            # print("\n----------------\n",ordered_cones[cone])
-            # print("\n",ordered_cones[cone + 1])
-
             # make sure cones on same side of track, skip otherwise
             if ordered_cones[cone].track_side != ordered_cones[cone + 1].track_side:
                 continue
@@ -101,8 +87,6 @@ class ConeInterpolator(Node):
             # get distance between two ordered cones
             firstCone_XY = ordered_cones[cone].location.x, ordered_cones[cone].location.y
             secondCone_XY = ordered_cones[cone + 1].location.x, ordered_cones[cone + 1].location.y
-            # print(str(firstCone_XY))
-            # print(str(secondCone_XY))
             conePair_dist = dist(firstCone_XY, secondCone_XY)
 
             # get sub-distance between cones for distance between interpolated cones and real cones
@@ -119,7 +103,6 @@ class ConeInterpolator(Node):
                     cone_of_origin[0], cone_of_origin[1], conePair_radians, interpolation_subDist
                 )
                 iCone_locations.append(iCone_coords)
-                total_iCone_coords.append(iCone_coords)
 
                 cone_of_origin = iCone_locations[iCone]
 
@@ -132,42 +115,17 @@ class ConeInterpolator(Node):
                 interpolatedCone.location.y = iCone_locations[iCone][1]
                 interpolatedCone.color = ordered_cones[cone].color
                 interpolatedCone.track_side = ordered_cones[cone].track_side
-                interpolatedCone.order = 999
 
                 # add cone to list of interpolated cones
                 interpolated_cones.append(interpolatedCone)
 
-            # add list of interpolated cones to list of all ordered cones
-            insert_index = cone + 1
-            # ordered_cones[insert_index:insert_index] = interpolated_cones
+            # Append each interpolated cone to the list of all cones
             for cone in range(len(interpolated_cones)):
                 ordered_cones.append(interpolated_cones[cone])
 
-        # print("\nordered cones: \n\n:", ordered_cones)
         # Publish list of ordered and interpolated cones
         interpolatedCones_msg = ConeDetectionStamped(cones=ordered_cones)
         self.interpolatedCones_publisher.publish(interpolatedCones_msg)
-        # print("\nordered cones: \n\n", interpolatedCones_msg.cones)
-
-        import matplotlib.pyplot as plt
-
-        # Separate the x and y coordinates
-        x = [point[0] for point in total_iCone_coords]
-        y = [point[1] for point in total_iCone_coords]
-        xo = [point[0] for point in originalCone_coords]
-        yo = [point[1] for point in originalCone_coords]
-
-        # Plot the points
-        plt.scatter(x, y)
-        plt.scatter(xo, yo)
-
-        # Add labels and a title
-        plt.xlabel("x - axis")
-        plt.ylabel("y - axis")
-        plt.title("2D Plane Plot of Points")
-
-        # Display the plot
-        plt.show()
 
 
 def main(args=None):
