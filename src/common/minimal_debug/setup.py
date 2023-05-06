eb92b75d183ad93aa42a4cc301286ba9103f19d0
b1n-ch1kn <albradford2468@gmail.com>
2023-05-06 15:11:51 +1000

    runs

--- src/common/minimal_debug/setup.py
+++ src/common/minimal_debug/setup.py
@@ -22,7 +22,7 @@ setup(
     tests_require=["pytest"],
     entry_points={
         "console_scripts": [
-            "test = minimal_debug.node_overlay:main",
+            "test = minimal_debug.test:main",
         ],
     },
 )
