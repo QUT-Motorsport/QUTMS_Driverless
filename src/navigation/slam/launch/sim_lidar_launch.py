ff19a7009a640ea489ba8ae7dc89ac4b6c6094d0
Alistair English <alistair.english@protonmail.com>
2023-05-08 15:47:37 +1000

    sim lidar config

--- src/navigation/slam/launch/sim_lidar_launch.py
+++ src/navigation/slam/launch/sim_lidar_launch.py
@@ -11,12 +11,12 @@ def generate_launch_description():
                 name="ekf_slam",
                 remappings=[
                     ("velocity", "imu/velocity"),
-                    ("cone_detection", "vision/cone_detection"),
+                    ("cone_detection", "lidar/cone_detection"),
                 ],
                 parameters=[
                     {"range_variance": 0.5},
-                    {"bearing_variance": 0.02},
-                    {"uncertanty_time_weight": 0.005},
+                    {"bearing_variance": 0.01},
+                    {"uncertanty_time_weight": 0.05},
                     {"uncertanty_heading_time_weight": 0.005},
                     {"association_dist_threshold": 2.0},
                     {"use_total_abs_vel": False},
