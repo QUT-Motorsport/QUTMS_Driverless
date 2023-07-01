a3595cb6cb7439c0f4ec44b909c2b052dcef9c29
Alastair <albradford2468@gmail.com>
2023-07-01 16:46:12 +1000

    use "passing 0" method

--- src/operations/mission_controller/mission_controller/node_trackdrive_handler.py
+++ src/operations/mission_controller/mission_controller/node_trackdrive_handler.py
@@ -12,17 +12,18 @@ from driverless_common.shutdown_node import ShutdownNode
 
 
 class TrackdriveHandler(ShutdownNode):
-    r2d: bool = False
-    laps: int = 0
-    last_lap_time: float = 0
+    r2d = False
+    laps = 0
+    last_lap_time = 0.0
+    last_x = 0.0
 
     def __init__(self):
         super().__init__("trackdrive_logic_node")
         self.create_subscription(Bool, "/system/r2d", self.r2d_callback, 10)
         self.create_subscription(PoseWithCovarianceStamped, "/slam/car_pose", self.pose_callback, 10)
 
-        self.shutdown_pub: Publisher = self.create_publisher(Shutdown, "/system/shutdown", 1)
-        self.lap_trig_pub: Publisher = self.create_publisher(UInt8, "/system/laps_completed", 1)
+        self.shutdown_pub = self.create_publisher(Shutdown, "/system/shutdown", 1)
+        self.lap_trig_pub = self.create_publisher(UInt8, "/system/laps_completed", 1)
 
         self.get_logger().info("---Trackdrive handler node initialised---")
 
@@ -36,7 +37,7 @@ class TrackdriveHandler(ShutdownNode):
         # check if car has crossed the finish line (0,0)
         # get distance from 0,0 and increment laps when within a certain threshold
         # and distance is increasing away from 0,0
-        if self.r2d and abs(msg.pose.pose.position.x) < 0.5 and abs(msg.pose.pose.position.y) < 2:
+        if self.r2d and (self.last_x <= 0 and msg.pose.pose.position.x > 0) and abs(msg.pose.pose.position.y) < 2:
             if time.time() - self.last_lap_time > 20:  # 20 seconds at least between laps
                 self.laps += 1
                 self.last_lap_time = time.time()
@@ -45,8 +46,12 @@ class TrackdriveHandler(ShutdownNode):
 
         if self.laps == 10:
             self.get_logger().info("Trackdrive mission complete")
+            # currently only works when vehicle supervisor node is running on-car
+            # TODO: sort out vehicle states for eventual environment agnostic operation
             shutdown_msg = Shutdown(finished_engage_ebs=True)
             self.shutdown_pub.publish(shutdown_msg)
+
+        self.last_x = msg.pose.pose.position.x
 
 
 def main(args=None):
