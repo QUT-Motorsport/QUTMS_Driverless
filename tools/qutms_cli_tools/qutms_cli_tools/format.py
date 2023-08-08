33e91dc8b27e00ffff3af8f44ce340af8b6c4dad
Alastair <albradford2468@gmail.com>
2023-08-08 21:49:45 +1000

    made formatter also go through eufs_sim, add more later?

--- tools/qutms_cli_tools/qutms_cli_tools/format.py
+++ tools/qutms_cli_tools/qutms_cli_tools/format.py
@@ -1,13 +1,13 @@
 import os
 import subprocess
 
-import colorama
+from qutms_cli_tools.common import Print
 
-G = colorama.Fore.GREEN
-R = colorama.Fore.RED
-Y = colorama.Fore.YELLOW
-B = colorama.Fore.BLUE
-RESET = colorama.Fore.RESET
+
+def format_repo(repo_path_relative):
+    """
+    specify repo to format
+    """
 
 
 def main():
@@ -15,24 +15,22 @@ def main():
     Format with pre-commit
     """
 
-    print(
-        G,
-        "Formatting...",
-        RESET,
-        flush=True,
-    )
-
     ws_path = os.path.expanduser(os.environ["QUTMS_WS"])
-    repo = os.path.join(ws_path, "QUTMS_Driverless")
 
-    command = ["pre-commit", "run", "--all-files"]
+    # TODO: make this an arg? with option for all
+    repos = ["QUTMS_Driverless", "eufs_sim"]
 
-    process = subprocess.Popen(command, text=True, cwd=repo)
-    try:
-        process.wait()
-    except KeyboardInterrupt:
+    for repo in repos:
+        repo_path_relative = os.path.join(ws_path, repo)
+        Print.green(f"Formatting {repo_path_relative}...")
+
+        command = ["pre-commit", "run", "--all-files"]
+        process = subprocess.Popen(command, text=True, cwd=repo_path_relative)
         try:
-            process.terminate()
-        except OSError:
-            pass
-        process.wait()
+            process.wait()
+        except KeyboardInterrupt:
+            try:
+                process.terminate()
+            except OSError:
+                pass
+            process.wait()
