853abf9bdbfad69feefb35c20303ec9398673b81
Zac Gillerat <n10484698@qut.edu.au>
2023-08-24 00:01:02 +1000

    Manage the pure_pursuit_cpp lifecycle node in trackdrive_handler.py.

--- src/operations/mission_controller/mission_controller/node_trackdrive_handler.py
+++ src/operations/mission_controller/mission_controller/node_trackdrive_handler.py
@@ -1,17 +1,17 @@
 import time
 
-import rclpy
-from rclpy.node import Node
-from rclpy.publisher import Publisher
-
 from driverless_msgs.msg import Shutdown, State
+
+import rclpy
+from driverless_common.lifecycle_service_client import LifecycleServiceClient
 from geometry_msgs.msg import PoseWithCovarianceStamped
+from lifecycle_msgs.msg import State, Transition
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.node import Node
 from std_msgs.msg import UInt8
 
-from driverless_common.shutdown_node import ShutdownNode
 
-
-class TrackdriveHandler(ShutdownNode):
+class TrackdriveHandler(Node):
     mission_started = False
     laps = 0
     last_lap_time = 0.0
@@ -19,11 +19,12 @@ class TrackdriveHandler(ShutdownNode):
 
     def __init__(self):
         super().__init__("trackdrive_logic_node")
-        self.create_subscription(State, "/system/as_status", self.state_callback, 10)
         self.create_subscription(PoseWithCovarianceStamped, "/slam/car_pose", self.pose_callback, 10)
 
         self.shutdown_pub = self.create_publisher(Shutdown, "/system/shutdown", 1)
         self.lap_trig_pub = self.create_publisher(UInt8, "/system/laps_completed", 1)
+
+        self.pure_pursuit_client = LifecycleServiceClient("pure_pursuit_cpp_node", self)
 
         self.get_logger().info("---Trackdrive handler node initialised---")
 
@@ -38,19 +39,21 @@ class TrackdriveHandler(ShutdownNode):
         # check if car has crossed the finish line (0,0)
         # get distance from 0,0 and increment laps when within a certain threshold
         # and distance is increasing away from 0,0
-        if (
-            self.mission_started
-            and (self.last_x <= 0 and msg.pose.pose.position.x > 0)
-            and abs(msg.pose.pose.position.y) < 2
-        ):
-            if time.time() - self.last_lap_time > 20:  # 20 seconds at least between laps
-                self.laps += 1
-                self.last_lap_time = time.time()
-                self.lap_trig_pub.publish(UInt8(data=self.laps))
-                self.get_logger().info(f"Lap {self.laps} completed")
+        if self.mission_started and self.last_x <= 0 < msg.pose.pose.position.x and abs(msg.pose.pose.position.y) < 2:
+            self.laps += 1
+            self.get_logger().info(f'{time.time() - self.last_lap_time}')
+            self.last_lap_time = time.time()
+            self.lap_trig_pub.publish(UInt8(data=self.laps))
+            self.get_logger().info(f"Lap {self.laps} completed")
 
-        if self.laps == 10:
+        if self.pure_pursuit_client.get_state() == State.PRIMARY_STATE_UNCONFIGURED:
+            self.pure_pursuit_client.change_state(Transition.TRANSITION_CONFIGURE)
+            self.pure_pursuit_client.change_state(Transition.TRANSITION_ACTIVATE)
+
+        if self.laps > 10:
             self.get_logger().info("Trackdrive mission complete")
+            self.pure_pursuit_client.change_state(Transition.TRANSITION_DEACTIVATE)
+            self.pure_pursuit_client.change_state(Transition.TRANSITION_INACTIVE_SHUTDOWN)
             # currently only works when vehicle supervisor node is running on-car
             # TODO: sort out vehicle states for eventual environment agnostic operation
             shutdown_msg = Shutdown(finished_engage_ebs=True)
@@ -62,6 +65,11 @@ class TrackdriveHandler(ShutdownNode):
 def main(args=None):
     rclpy.init(args=args)
     node = TrackdriveHandler()
-    rclpy.spin(node)
+
+    executor = MultiThreadedExecutor()
+    executor.add_node(node)
+    executor.spin()
+
     node.destroy_node()
+    executor.shutdown()
     rclpy.shutdown()
