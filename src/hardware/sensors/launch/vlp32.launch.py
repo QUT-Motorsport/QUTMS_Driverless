8ebc0f0470ead45efbfe4563641b0431170c5715
b1n-ch1kn <albradford2468@gmail.com>
2023-08-25 16:44:19 +1000

    change to local params rather than defaults

--- src/hardware/sensors/launch/vlp32.launch.py
+++ src/hardware/sensors/launch/vlp32.launch.py
@@ -8,28 +8,20 @@ import yaml
 
 def generate_launch_description():
     # Copied from: https://github.com/ros-drivers/velodyne/blob/galactic-devel/velodyne/launch/velodyne-all-nodes-VLP32C-launch.py
-    driver_params_file = str(
-        get_package_share_path("velodyne_driver") / "config" / "VLP32C-velodyne_driver_node-params.yaml"
-    )
+    driver_params_file = str(get_package_share_path("sensors") / "config" / "VLP32C.yaml")
     velodyne_driver_node = launch_ros.actions.Node(
         package="velodyne_driver", executable="velodyne_driver_node", output="both", parameters=[driver_params_file]
     )
 
-    convert_params_file = os.path.join(
-        get_package_share_path("velodyne_pointcloud") / "config" / "VLP32C-velodyne_convert_node-params.yaml"
-    )
+    convert_params_file = os.path.join(get_package_share_path("sensors") / "config" / "VLP32C.yaml")
     with open(convert_params_file, "r") as f:
         convert_params = yaml.safe_load(f)["velodyne_convert_node"]["ros__parameters"]
-    convert_params["calibration"] = str(
-        get_package_share_path("velodyne_pointcloud") / "params" / "VeloView-VLP-32C.yaml"
-    )
+    convert_params["calibration"] = str(get_package_share_path("sensors") / "config" / "VeloView-VLP32C.yaml")
     velodyne_convert_node = launch_ros.actions.Node(
         package="velodyne_pointcloud", executable="velodyne_convert_node", output="both", parameters=[convert_params]
     )
 
-    laserscan_params_file = str(
-        get_package_share_path("velodyne_laserscan") / "config" / "default-velodyne_laserscan_node-params.yaml"
-    )
+    laserscan_params_file = str(get_package_share_path("sensors") / "config" / "VLP32C.yaml")
     velodyne_laserscan_node = launch_ros.actions.Node(
         package="velodyne_laserscan",
         executable="velodyne_laserscan_node",
