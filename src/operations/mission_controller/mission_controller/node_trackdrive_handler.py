5d1f50557cceee069e3f94d5a7226652a0540e67
Zac <zac.gillerat@hotmail.com>
2023-09-13 14:43:59 +1000

    Restore trackdrive_handler to its original state, inheriting ShutdownNode, etc.

--- src/operations/mission_controller/mission_controller/node_trackdrive_handler.py
+++ src/operations/mission_controller/mission_controller/node_trackdrive_handler.py
@@ -7,13 +7,14 @@ from rclpy.node import Node
 
 from driverless_msgs.msg import Shutdown, State
 from geometry_msgs.msg import PoseWithCovarianceStamped
-from lifecycle_msgs.msg import State
 from std_msgs.msg import UInt8
 
+from driverless_common.common import QOS_LATEST
 from driverless_common.lifecycle_service_client import LifecycleServiceClient
+from driverless_common.shutdown_node import ShutdownNode
 
 
-class TrackdriveHandler(Node):
+class TrackdriveHandler(ShutdownNode):
     mission_started = False
     laps = 0
     last_lap_time = 0.0
@@ -22,7 +23,7 @@ class TrackdriveHandler(Node):
 
     def __init__(self):
         super().__init__("trackdrive_logic_node")
-        self.create_subscription(PoseWithCovarianceStamped, "/slam/car_pose", self.pose_callback, 10)
+        self.create_subscription(PoseWithCovarianceStamped, "/slam/car_pose", self.pose_callback, QOS_LATEST)
 
         self.shutdown_pub = self.create_publisher(Shutdown, "/system/shutdown", 1)
         self.lap_trig_pub = self.create_publisher(UInt8, "/system/laps_completed", 1)
@@ -31,34 +32,22 @@ class TrackdriveHandler(Node):
         self.reactive_controller = LifecycleServiceClient("reactive_controller_node", self)
         self.planner = LifecycleServiceClient("ordered_map_spline_node", self)
 
-        self.init_method = self.create_timer(
-            1, self.init_method_callback, callback_group=MutuallyExclusiveCallbackGroup()
-        )
-
         self.get_logger().info("---Trackdrive handler node initialised---")
 
-    # Services cannot be activated in the constructor, activating clients that would be activated in the constructor
-    # should be done here, this timer's callback is called once and then the timer is destroyed.
-    def init_method_callback(self):
-        self.get_logger().info("init_method_callback")
-
-        self.reactive_controller.activate()
-
-        self.destroy_timer(self.init_method)
-        self.init_method_called = True
-
     def state_callback(self, msg: State):
+        super().state_callback(msg)
         if not self.mission_started and msg.state == State.DRIVING and msg.mission == State.TRACKDRIVE:
             self.mission_started = True
             self.last_lap_time = time.time()
             self.lap_trig_pub.publish(UInt8(data=self.laps))
+            self.reactive_controller.activate()
             self.get_logger().info("Trackdrive mission started")
 
     def pose_callback(self, msg: PoseWithCovarianceStamped):
         # check if car has crossed the finish line (0,0)
         # get distance from 0,0 and increment laps when within a certain threshold
         # and distance is increasing away from 0,0
-        if self.last_x <= 0 < msg.pose.pose.position.x and abs(msg.pose.pose.position.y) < 2:
+        if self.mission_started and self.last_x <= 0 < msg.pose.pose.position.x and abs(msg.pose.pose.position.y) < 2:
             self.laps += 1
             self.get_logger().info(f"{time.time() - self.last_lap_time}")
             self.last_lap_time = time.time()
