67969c279a83ecd7e1a7dec5ecbada0be6528939
Alastair <albradford2468@gmail.com>
2023-10-29 16:22:49 +1000

    update launches with new steering

--- src/machines/roscube_machine/launch/machine.launch.py
+++ src/machines/roscube_machine/launch/machine.launch.py
@@ -14,7 +14,7 @@ def generate_launch_description():
             ),
             Node(
                 package="canbus",
-                executable="canbus_translator_node",
+                executable="canbus_translator_socket_node",
                 parameters=[
                     get_package_share_path("canbus") / "config" / "canbus.yaml",
                 ],
