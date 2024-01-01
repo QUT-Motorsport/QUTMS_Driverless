40f92f12de89f74956314fc9b4181df29d408d24
Alastair <albradford2468@gmail.com>
2024-01-01 20:33:40 +1000

    Launch all new nodes. Move SBG to mission

--- src/operations/mission_controller/launch/trackdrive.launch.py
+++ src/operations/mission_controller/launch/trackdrive.launch.py
@@ -15,16 +15,30 @@ def generate_launch_description():
                 executable="trackdrive_handler_node",
             ),
             Node(
-                package="controllers",
-                executable="reactive_control_node",
+                package="sbg_translator",
+                executable="sbg_translator_node",
             ),
             Node(
-                package="pure_pursuit",
-                executable="pure_pursuit_node",
+                package="map_creation",
+                executable="cone_placement_node",
+                parameters=[
+                    get_package_share_path("map_creation") / "config" / "cone_placement.yaml",
+                ],
+            ),
+            Node(
+                package="planners",
+                executable="boundary_interpolation_node",
+            ),
+            Node(
+                package="pure_pursuit_cpp",
+                executable="pure_pursuit_cpp_node",
+                parameters=[
+                    get_package_share_path("pure_pursuit_cpp") / "config" / "pure_pursuit_cpp.yaml",
+                ],
             ),
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
-                    os.path.join(nav_package_share, "launch", "cone_association_slam.launch.py")
+                    os.path.join(get_package_share_path("nav_bringup"), "launch", "slam_toolbox.launch.py")
                 )
             ),
         ]
