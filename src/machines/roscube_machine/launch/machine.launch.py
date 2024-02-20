248195cc1354632e844113dc33875dae63a263c8
Alastair Bradford <albradford2468@gmail.com>
2024-02-20 20:05:50 +1100

    using simulated navigation, cutdown processes

--- src/machines/roscube_machine/launch/machine.launch.py
+++ src/machines/roscube_machine/launch/machine.launch.py
@@ -2,44 +2,46 @@ from ament_index_python.packages import get_package_share_path
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
-from launch_ros.actions import ComposableNodeContainer
+from launch_ros.actions import ComposableNodeContainer, Node
 from launch_ros.descriptions import ComposableNode
+
 
 def generate_launch_description():
     # safety critical signals should use intra process comms for low latency
     scs_container = ComposableNodeContainer(
-        name='critcial_signal_container',
-        namespace='',
-        package='rclcpp_components',
-        executable='component_container_mt',
+        name="critcial_signal_container",
+        namespace="",
+        package="rclcpp_components",
+        executable="component_container_mt",
         composable_node_descriptions=[
             ComposableNode(
-                package='vehicle_supervisor',
-                plugin='vehicle_supervisor::ASSupervisorLaunch',
-                name='vehicle_supervisor_launch_node',
+                package="vehicle_supervisor",
+                plugin="vehicle_supervisor::ASSupervisor",
+                name="vehicle_supervisor_node",
                 parameters=[
-                    {"manual_override": False},
+                    {"manual_override": True},
                 ],
-                extra_arguments=[{'use_intra_process_comms': True}]),
+                extra_arguments=[{"use_intra_process_comms": True}],
+            ),
             ComposableNode(
-                package='steering_actuator',
-                plugin='steering_actuator::SteeringActuator',
-                name='steering_actuator_node',
+                package="steering_actuator",
+                plugin="steering_actuator::SteeringActuator",
+                name="steering_actuator_node",
                 parameters=[
                     get_package_share_path("steering_actuator") / "config" / "steering.yaml",
                 ],
-                extra_arguments=[{'use_intra_process_comms': True}]),
-            ComposableNode(
-                package='canbus',
-                plugin='canbus::CANTranslator',
-                name='canbus_translator_node',
-                parameters=[
-                    get_package_share_path("canbus") / "config" / "canbus.yaml",
-                ],
-                extra_arguments=[{'use_intra_process_comms': True}]),
+                extra_arguments=[{"use_intra_process_comms": True}],
+            ),
+            # ComposableNode(
+            #     package='canbus',
+            #     plugin='canbus::CANTranslator',
+            #     name='canbus_translator_node',
+            #     parameters=[
+            #         get_package_share_path("canbus") / "config" / "canbus.yaml",
+            #     ],
+            #     extra_arguments=[{'use_intra_process_comms': True}]),
         ],
-        output='both',
+        output="both",
     )
 
     return LaunchDescription(
@@ -60,10 +62,10 @@ def generate_launch_description():
                     get_package_share_path("velocity_controller") / "config" / "velocity_controller.yaml",
                 ],
             ),
-            Node(
-                package="lidar_pipeline",
-                executable="lidar_detector_node",
-            ),
+            # Node(
+            # package="lidar_pipeline",
+            # executable="lidar_detector_node",
+            # ),
             IncludeLaunchDescription(
                 launch_description_source=PythonLaunchDescriptionSource(
                     launch_file_path=str(get_package_share_path("sensors") / "launch" / "vlp32.launch.py")
--- src/operations/mission_controller/launch/trackdrive.launch.py
+++ src/operations/mission_controller/launch/trackdrive.launch.py
@@ -10,17 +10,17 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription(
         [
-            Node(
-                package="sbg_translator",
-                executable="sbg_translator_node",
-            ),
-            Node(
-                package="map_creation",
-                executable="cone_placement_node",
-                parameters=[
-                    get_package_share_path("map_creation") / "config" / "cone_placement.yaml",
-                ],
-            ),
+            # Node(
+            #     package="sbg_translator",
+            #     executable="sbg_translator_node",
+            # ),
+            # Node(
+            #     package="map_creation",
+            #     executable="cone_placement_node",
+            #     parameters=[
+            #         get_package_share_path("map_creation") / "config" / "cone_placement.yaml",
+            #     ],
+            # ),
             Node(
                 package="planners",
                 executable="boundary_interpolation_node",
@@ -32,10 +32,10 @@ def generate_launch_description():
                     get_package_share_path("pure_pursuit_cpp") / "config" / "pure_pursuit_cpp.yaml",
                 ],
             ),
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    os.path.join(get_package_share_path("nav_bringup"), "launch", "slam_toolbox.launch.py")
-                )
-            ),
+            # IncludeLaunchDescription(
+            #     PythonLaunchDescriptionSource(
+            #         os.path.join(get_package_share_path("nav_bringup"), "launch", "slam_toolbox.launch.py")
+            #     )
+            # ),
         ]
     )
