f6fa4551516a70c12634539d249393c769d77e98
Alastair <albradford2468@gmail.com>
2024-03-05 17:51:30 +1000

    compose vehicle supervisor. split into .hpp. inheriting supervisor for launching version

---
+++ src/hardware/vehicle_supervisor/include/component_supervisor_launch.hpp
@@ -0,0 +1,21 @@
+#ifndef VEHICLE_SUPERVISOR__COMPONENT_SUPERVISOR_LAUNCH_HPP_
+#define VEHICLE_SUPERVISOR__COMPONENT_SUPERVISOR_LAUNCH_HPP_
+
+#include "component_supervisor.hpp"
+
+namespace vehicle_supervisor {
+
+class ASSupervisorLaunch : public ASSupervisor {
+   private:    
+    void run_fsm();
+    void launch_mission();
+
+    void dvl_heartbeat_timer_callback();
+
+   public:
+    ASSupervisorLaunch(const rclcpp::NodeOptions & options);
+};
+
+}  // namespace vehicle_supervisor
+
+#endif  // VEHICLE_SUPERVISOR__COMPONENT_SUPERVISOR_LAUNCH_HPP_
