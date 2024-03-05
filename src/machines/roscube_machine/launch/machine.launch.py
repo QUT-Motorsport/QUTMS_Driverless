7873efbd048bd4b89088b8e086a1437eb8c5e113
Alastair <albradford2468@gmail.com>
2024-03-05 17:54:41 +1000

    machine launch with composed nodes

--- src/machines/roscube_machine/launch/machine.launch.py
+++ src/machines/roscube_machine/launch/machine.launch.py
@@ -2,26 +2,61 @@ from ament_index_python.packages import get_package_share_path
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
+from launch_ros.actions import ComposableNodeContainer, Node
+from launch_ros.descriptions import ComposableNode
 
 
 def generate_launch_description():
-    return LaunchDescription(
-        [
-            Node(
-                package="canbus",
-                executable="canbus_translator_socket_node",
-                parameters=[
-                    get_package_share_path("canbus") / "config" / "canbus.yaml",
-                ],
-            ),
-            Node(
+    # safety critical signals should use intra process comms for low latency
+    scs_container = ComposableNodeContainer(
+        name="critcial_signal_container",
+        namespace="",
+        package="rclcpp_components",
+        executable="component_container_mt",
+        composable_node_descriptions=[
+            ComposableNode(
                 package="vehicle_supervisor",
-                executable="vehicle_supervisor_node",
+                plugin="vehicle_supervisor::ASSupervisorLaunch",
+                name="vehicle_supervisor_launch_node",
                 parameters=[
                     {"manual_override": False},
                 ],
+                extra_arguments=[{"use_intra_process_comms": True}],
             ),
+            ComposableNode(
+                package="steering_actuator",
+                plugin="steering_actuator::SteeringActuator",
+                name="steering_actuator_node",
+                parameters=[
+                    get_package_share_path("steering_actuator") / "config" / "steering.yaml",
+                ],
+                extra_arguments=[{"use_intra_process_comms": True}],
+            ),
+            ComposableNode(
+                package="velocity_controller",
+                plugin="velocity_controller::VelocityController",
+                name="velocity_controller_node",
+                parameters=[
+                    get_package_share_path("velocity_controller") / "config" / "velocity_controller.yaml",
+                ],
+                extra_arguments=[{"use_intra_process_comms": True}],
+            ),
+            ComposableNode(
+                package="canbus",
+                plugin="canbus::CANTranslator",
+                name="canbus_translator_node",
+                parameters=[
+                    get_package_share_path("canbus") / "config" / "canbus.yaml",
+                ],
+                extra_arguments=[{"use_intra_process_comms": True}],
+            ),
+        ],
+        output="both",
+    )
+
+    return LaunchDescription(
+        [
+            scs_container,
             Node(
                 package="rosboard",
                 executable="rosboard_node",
@@ -29,20 +64,6 @@ def generate_launch_description():
             Node(
                 package="driverless_common",
                 executable="display",
-            ),
-            Node(
-                package="steering_actuator",
-                executable="steering_actuator_test_node",
-                parameters=[
-                    get_package_share_path("steering_actuator") / "config" / "steering.yaml",
-                ],
-            ),
-            Node(
-                package="velocity_controller",
-                executable="velocity_controller_node",
-                parameters=[
-                    get_package_share_path("velocity_controller") / "config" / "velocity_controller.yaml",
-                ],
             ),
             Node(
                 package="lidar_pipeline",
