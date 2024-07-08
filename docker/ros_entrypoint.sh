d513adb2122806f80da69d12a6b197f9aea2bc9a
qev3-d <team@qutmotorsport.com>
2024-07-08 20:11:24 +1000

    fixing up rmw stuff

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #set -e
 
-# echo 'export RMW_IMPLEMENTATION="rmw_cyclonedds_cpp"' >> /home/$USERNAME/.bashrc
-# echo 'export CYCLONEDDS_URI=file:///home/developer/driverless_ws/src/machines/roscube_machine/cyclonedds.xml' >> /home/$USERNAME/.bashrc
-# echo 'export ROS_DOMAIN_ID=69' >> /home/$USERNAME/.bashrc
+echo 'export RMW_IMPLEMENTATION="rmw_cyclonedds_cpp"' >> /home/developer/.bashrc
+echo 'export CYCLONEDDS_URI=file:///home/developer/driverless_ws/src/machines/roscube_machine/cyclonedds.xml' >> /home/developer/.bashrc
+echo 'export ROS_DOMAIN_ID=69' >> /home/developer/.bashrc
+
+export RMW_IMPLEMENTATION="rmw_cyclonedds_cpp"
+export CYCLONEDDS_URI="file:///home/developer/driverless_ws/src/machines/roscube_machine/cyclonedds.xml"
+export ROS_DOMAIN_ID=69
 
 test -f "/opt/ros/$ROS_DISTRO/setup.bash" && source "/opt/ros/$ROS_DISTRO/setup.bash"
 test -f "/opt/ros/$ROS_DISTRO/install/setup.bash" && source "/opt/ros/$ROS_DISTRO/install/setup.bash"
