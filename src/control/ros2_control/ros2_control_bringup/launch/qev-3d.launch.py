8eb01bbe500e14b399774106d60f2ae309f70345
Walden Leung <7531347+leungw16@users.noreply.github.com>
2024-09-16 01:16:13 +1000

    reconfigured launch file to work with a configurable list of startup controllers in chained and unchained mode

--- src/control/ros2_control/ros2_control_bringup/launch/qev-3d.launch.py
+++ src/control/ros2_control/ros2_control_bringup/launch/qev-3d.launch.py
@@ -19,12 +19,120 @@
 # Author: Dr. Denis
 #
 
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, RegisterEventHandler, TimerAction
+from launch import LaunchContext, LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, RegisterEventHandler, TimerAction
 from launch.event_handlers import OnProcessExit, OnProcessStart
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+
+
+def launch_spawners(
+    context: LaunchContext,
+    controller_config: LaunchConfiguration,
+    inactive_controller_config: LaunchConfiguration,
+    joint_state_broadcaster_spawner: Node,
+) -> list:
+    # Substitiutes LaunchConfigurations into str
+    controller_config = context.perform_substitution(controller_config)
+    inactive_controller_config = context.perform_substitution(inactive_controller_config)
+
+    spawner_return = []
+
+    # Evaluates the controller names config to determine if controllers are to be spawned individually or as a chain.
+    # Makes sure that the configuration is not empty, otherwise the spawners should be skipped
+    robot_controller_names = []
+    if not (controller_config == "" or controller_config == "[]"):
+        eval("robot_controller_names.append(" + controller_config + ")")
+    robot_controller_spawners = []
+
+    for controller in robot_controller_names:
+        if type(controller) is str:
+            robot_controller_spawners += [
+                Node(
+                    package="controller_manager",
+                    executable="spawner",
+                    arguments=[controller, "-c", "/controller_manager"],
+                )
+            ]
+        elif type(controller) is list:
+            controller_spawner_args = ["-c", "/controller_manager", "--activate-as-group"]
+            controller_spawner_args.extend(controller)
+            robot_controller_spawners += [
+                Node(
+                    package="controller_manager",
+                    executable="spawner",
+                    arguments=controller_spawner_args,
+                )
+            ]
+        else:
+            raise TypeError(
+                f"Controller {controller} input not recognized. \
+                    str or list expected but received {type(controller)}"
+            )
+
+    # Delay loading and activation of robot_controller_names after `joint_state_broadcaster`
+    delay_robot_controller_spawners_after_joint_state_broadcaster_spawner = []
+    for i, controller in enumerate(robot_controller_spawners):
+        delay_robot_controller_spawners_after_joint_state_broadcaster_spawner += [
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=(robot_controller_spawners[i - 1] if i > 0 else joint_state_broadcaster_spawner),
+                    on_exit=[controller],
+                )
+            )
+        ]
+    spawner_return.extend(delay_robot_controller_spawners_after_joint_state_broadcaster_spawner)
+
+    # Repeats the process for inactive spawners,
+    # but only if at least one active controller is configured
+    inactive_robot_controller_names = []
+    if (not (inactive_controller_config == "" or inactive_controller_config == "[]")) and len(
+        robot_controller_spawners
+    ) > 0:
+        eval("inactive_robot_controller_names.append(" + inactive_controller_config + ")")
+    inactive_robot_controller_spawners = []
+
+    for controller in inactive_robot_controller_names:
+        if type(controller) is str:
+            inactive_robot_controller_spawners += [
+                Node(
+                    package="controller_manager",
+                    executable="spawner",
+                    arguments=[controller, "-c", "/controller_manager", "--inactive"],
+                )
+            ]
+        elif type(controller) is list:
+            inactive_controller_spawner_args = ["-c", "/controller_manager", "--inactive", "--activate-as-group"]
+            inactive_controller_spawner_args.extend(controller)
+            inactive_robot_controller_spawners += [
+                Node(
+                    package="controller_manager",
+                    executable="spawner",
+                    arguments=inactive_controller_spawner_args,
+                )
+            ]
+        else:
+            raise TypeError(
+                f"Inactive controller {controller} input not recognized. \
+                    str or list expected but received {type(controller)}"
+            )
+
+    # Delay start of inactive_robot_controller_names after other controllers
+    delay_inactive_robot_controller_spawners_after_joint_state_broadcaster_spawner = []
+    for i, controller in enumerate(inactive_robot_controller_spawners):
+        delay_inactive_robot_controller_spawners_after_joint_state_broadcaster_spawner += [
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=(
+                        inactive_robot_controller_spawners[i - 1] if i > 0 else robot_controller_spawners[-1]
+                    ),
+                    on_exit=[controller],
+                )
+            )
+        ]
+    spawner_return.extend(delay_inactive_robot_controller_spawners_after_joint_state_broadcaster_spawner)
+    return spawner_return
 
 
 def generate_launch_description():
@@ -87,9 +195,17 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "robot_controller",
-            default_value="joint_trajectory_controller",
-            choices=["forward_position_controller", "joint_trajectory_controller"],
-            description="Robot controller to start.",
+            default_value='["steering_pid_controller", "drive_pid_controller", "ackermann_steering_controller"]',
+            description="Robot controller to start. Pass string to start individual \
+                controllers or python list to start in chain mode",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "inactive_robot_controller",
+            default_value="",
+            description="Inactive robot controller to start. Pass string to start individual \
+                controllers or python list to start in chain mode",
         )
     )
 
@@ -102,6 +218,7 @@ def generate_launch_description():
     use_mock_hardware = LaunchConfiguration("use_mock_hardware")
     mock_sensor_commands = LaunchConfiguration("mock_sensor_commands")
     robot_controller = LaunchConfiguration("robot_controller")
+    inactive_robot_controller = LaunchConfiguration("inactive_robot_controller")
 
     # Get URDF via xacro
     robot_description_content = Command(
@@ -153,36 +270,6 @@ def generate_launch_description():
         arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
     )
 
-    robot_controller_names = ["steering_pid_controller", "drive_pid_controller", "ackermann_steering_controller"]
-    controller_spawner_args = ["-c", "/controller_manager", "--activate-as-group"]
-    controller_spawner_args.extend(robot_controller_names)
-    robot_controller_spawners = [
-        Node(
-            package="controller_manager",
-            executable="spawner",
-            arguments=controller_spawner_args,
-        )
-    ]
-    # for controller in robot_controller_names:
-    #     robot_controller_spawners += [
-    #         Node(
-    #             package="controller_manager",
-    #             executable="spawner",
-    #             arguments=[controller, "-c", "/controller_manager"],
-    #         )
-    #     ]
-
-    # inactive_robot_controller_names = ["add_some_controller_name"]
-    # inactive_robot_controller_spawners = []
-    # for controller in inactive_robot_controller_names:
-    #     inactive_robot_controller_spawners += [
-    #         Node(
-    #             package="controller_manager",
-    #             executable="spawner",
-    #             arguments=[controller, "-c", "/controller_manager", "--inactive"],
-    #         )
-    #     ]
-
     # Delay loading and activation of `joint_state_broadcaster` after start of ros2_control_node
     delay_joint_state_broadcaster_spawner_after_ros2_control_node = RegisterEventHandler(
         event_handler=OnProcessStart(
@@ -196,34 +283,6 @@ def generate_launch_description():
         )
     )
 
-    # Delay loading and activation of robot_controller_names after `joint_state_broadcaster`
-    delay_robot_controller_spawners_after_joint_state_broadcaster_spawner = []
-    for i, controller in enumerate(robot_controller_spawners):
-        delay_robot_controller_spawners_after_joint_state_broadcaster_spawner += [
-            RegisterEventHandler(
-                event_handler=OnProcessExit(
-                    target_action=(robot_controller_spawners[i - 1] if i > 0 else joint_state_broadcaster_spawner),
-                    on_exit=[controller],
-                )
-            )
-        ]
-
-    # Delay start of inactive_robot_controller_names after other controllers
-    # delay_inactive_robot_controller_spawners_after_joint_state_broadcaster_spawner = []
-    # for i, controller in enumerate(inactive_robot_controller_spawners):
-    #     delay_inactive_robot_controller_spawners_after_joint_state_broadcaster_spawner += [
-    #         RegisterEventHandler(
-    #             event_handler=OnProcessExit(
-    #                 target_action=(
-    #                     inactive_robot_controller_spawners[i - 1]
-    #                     if i > 0
-    #                     else robot_controller_spawners[-1]
-    #                 ),
-    #                 on_exit=[controller],
-    #             )
-    #         )
-    #     ]
-
     return LaunchDescription(
         declared_arguments
         + [
@@ -231,7 +290,9 @@ def generate_launch_description():
             robot_state_pub_node,
             rviz_node,
             delay_joint_state_broadcaster_spawner_after_ros2_control_node,
+            OpaqueFunction(
+                function=launch_spawners,
+                args=[robot_controller, inactive_robot_controller, joint_state_broadcaster_spawner],
+            ),
         ]
-        + delay_robot_controller_spawners_after_joint_state_broadcaster_spawner
-        # + delay_inactive_robot_controller_spawners_after_joint_state_broadcaster_spawner
     )
