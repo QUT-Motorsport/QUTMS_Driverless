8f762ec059f8357f79dabe9f0ab72bf26f82c398
ClaudiaK <claudiakiss005@gmail.com>
2024-10-11 23:19:49 +1000

    filled in correct package and setup info reversed lidar code change

--- src/navigation/slam_gridmap/setup.py
+++ src/navigation/slam_gridmap/setup.py
@@ -4,7 +4,7 @@ package_name = "slam_gridmap"
 
 setup(
     name=package_name,
-    version="0.0.0",
+    version="1.0.0",
     packages=find_packages(exclude=["test"]),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
@@ -13,9 +13,9 @@ setup(
     install_requires=["setuptools"],
     zip_safe=True,
     maintainer="yurikun1033",
-    maintainer_email="claudiakiss005@gmail.com",
-    description="TODO: Package description",
-    license="TODO: License declaration",
+    maintainer_email="team@qutmotorsport.com",
+    description="real-time SLAM cone detection operating on gridmaps",
+    license="MIT",
     tests_require=["pytest"],
     entry_points={
         "console_scripts": ["gridmap_to_code_detection = slam_gridmap.node_gridmap_to_code_detection:main"],
