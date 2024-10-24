c9875379fbff395895285d848a2d01193740e236
Alastair <albradford2468@gmail.com>
2024-10-25 01:55:23 +1000

    Starting a docker compose which allows passthrough of local folders into the workspace for quicker development

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 #set -e
 
-echo 'export RMW_IMPLEMENTATION="rmw_cyclonedds_cpp"' >> /home/developer/.bashrc
-echo 'export CYCLONEDDS_URI=file:///home/developer/driverless_ws/cyclonedds.xml' >> /home/developer/.bashrc
-echo 'export ROS_DOMAIN_ID=69' >> /home/developer/.bashrc
-
-export RMW_IMPLEMENTATION="rmw_cyclonedds_cpp"
-export CYCLONEDDS_URI="file:///home/developer/driverless_ws/cyclonedds.xml"
-export ROS_DOMAIN_ID=69
-
 test -f "/opt/ros/$ROS_DISTRO/setup.bash" && source "/opt/ros/$ROS_DISTRO/setup.bash"
 test -f "/opt/ros/$ROS_DISTRO/install/setup.bash" && source "/opt/ros/$ROS_DISTRO/install/setup.bash"
 
--- tools/play_bag.sh
+++ tools/play_bag.sh
@@ -8,9 +8,11 @@ source install/setup.bash
 # BAG_CMD="ros2 bag play -s mcap /mnt/e/rosbag2_2024_07_25-03_24_19/ --clock -p" # lidar on roll hoop test
 # BAG_CMD="ros2 bag play -s mcap /mnt/e/rosbag2_2024_07_25-05_04_46/ --clock -p" # half lap
 # BAG_CMD="ros2 bag play -s mcap /mnt/e/rosbag2_2024_07_25-05_18_42/ --clock -p" # other half of lap
-BAG_CMD="ros2 bag play -s mcap src/rosbag2_2024_10_17-01_36_17 -p" # 2.5 laps
+# BAG_CMD="ros2 bag play -s mcap /mnt/e/rosbag2_2024_09_04-05_14_43 --clock -p" # 2.5 laps
 # BAG_CMD="ros2 bag play -s mcap /mnt/e/rosbag2_2024_09_24-06_08_59/ --clock -p" # 1 lap, loop close crashing
 # BAG_CMD="ros2 bag play -s mcap /mnt/e/rosbag2_2024_09_24-02_16_55/ --clock -p" # ebs test run
+BAG_CMD="ros2 bag play -s mcap /mnt/e/rosbag2_2024_10_18-01_10_29/ --clock -p" # 9 laps, recording from 1st corner
+# BAG_CMD="ros2 bag play -s mcap /mnt/e/rosbag2_2024_10_18-01_40_49 --clock -p" # 10 laps recording from 2nd lap, 1st corner
 
 REMAPPINGS=(
     # /tf:=/tf_old
@@ -21,14 +23,14 @@ REMAPPINGS=(
 )
 
 TOPICS=(
-    # /imu/odometry # needed for RL
-    # /vehicle/wheel_twist # needed for RL
-    # # /lidar/objects
-    # /lidar/converted_2D_scan
-    # /lidar/cone_detection
-    # # /debug_markers/lidar_markers
-    # /system/as_status # needed for lap = 0 in mapping
-    # # /imu/nav_sat_fix # for visual
+    /imu/odometry # needed for RL
+    /vehicle/wheel_twist # needed for RL
+    # /lidar/objects
+    /lidar/converted_2D_scan
+    /lidar/cone_detection
+    # /debug_markers/lidar_markers
+    /system/as_status # needed for lap = 0 in mapping
+    # /imu/nav_sat_fix # for visual
 )
 
 # remap these topics if you are running programs which output to the same topic names
