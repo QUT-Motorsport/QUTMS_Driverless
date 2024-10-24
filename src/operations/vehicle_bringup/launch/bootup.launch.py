a4294e107619e7675132f8d12ac17c3f875210bf
Alastair <albradford2468@gmail.com>
2024-10-24 23:48:25 +1000

    SBG boots on launch. Created rotation node to simplify transform
    Allows GPS lock from the pits before startup sequence. Rotater node just turns any INS heading 90 degrees

--- src/operations/vehicle_bringup/launch/bootup.launch.py
+++ src/operations/vehicle_bringup/launch/bootup.launch.py
@@ -1,6 +1,9 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+from ament_index_python.packages import get_package_share_path
 
 def generate_launch_description():
     vehicle_supervisor_node = Node(
@@ -8,8 +11,13 @@ def generate_launch_description():
         executable="vehicle_supervisor_node",
     )
 
+    sbg_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(get_package_share_path("nav_bringup"), "launch", "sbg.launch.py"))
+    )
+
     return LaunchDescription(
         [
+            # sbg_launch,
             vehicle_supervisor_node
         ]
     )
