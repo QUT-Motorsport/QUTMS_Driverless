f6392aaa2d6111290de3069127aa4331f6c6dbd5
Alastair <albradford2468@gmail.com>
2024-10-27 14:54:58 +1000

    Split into a base image so docker repo doesnt need to be hit each time. Build, log, install persist between containers with fixed user params

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #set -e
 
-test -f "/opt/ros/$ROS_DISTRO/setup.bash" && source "/opt/ros/$ROS_DISTRO/setup.bash"
-test -f "/opt/ros/$ROS_DISTRO/install/setup.bash" && source "/opt/ros/$ROS_DISTRO/install/setup.bash"
+test -f "/opt/ros/${ROS_DISTRO}/setup.bash" && source "/opt/ros/$ROS_DISTRO/setup.bash"
+echo "Sourced ROS 2 ${ROS_DISTRO}"
 
 # source workspace setup if available
-test -f "/home/developer/driverless_ws/install/setup.bash" && source "/home/developer/driverless_ws/install/setup.bash"
+test -f "/home/${USER}/QUTMS/install/setup.bash" && source "/home/$USER/QUTMS/install/setup.bash"
+echo "Sourced QUTMS workspace"
 
 exec "$@"
