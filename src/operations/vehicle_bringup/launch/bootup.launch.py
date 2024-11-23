cb8b29e8ce15a511aca1e2e3bc3522ac6d92c344
Walden Leung <7531347+leungw16@users.noreply.github.com>
2024-11-24 01:02:11 +1000

    Added rosbag creator node to bootup

--- src/operations/vehicle_bringup/launch/bootup.launch.py
+++ src/operations/vehicle_bringup/launch/bootup.launch.py
@@ -14,6 +14,11 @@ def generate_launch_description():
         executable="vehicle_supervisor_node",
     )
 
+    rosbag_creator_node = Node(
+        package="rosbag_creator",
+        executable="rosbag_creator_node",
+    )
+
     system_watcher = Node(
         package="vehicle_bringup",
         executable="system_watcher_node",
@@ -52,5 +57,6 @@ def generate_launch_description():
             launch_system_on_watcher_exit,
             mission_launch,
             vehicle_supervisor_node,
+            rosbag_creator_node,
         ]
     )
