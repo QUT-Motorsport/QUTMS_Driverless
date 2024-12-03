bcddae65c7b4922c27ec3e3a7ebb4034bed8387e
Alastair <albradford2468@gmail.com>
2024-12-04 03:27:31 +1000

    Create a node to combine segmented points back into velodyne (topic wasnt recorded)

--- tools/play_bag.sh
+++ tools/play_bag.sh
@@ -9,39 +9,42 @@ source install/setup.bash
 # BAG_CMD="ros2 bag play -s mcap /mnt/e/rosbag2_2024_07_25-05_04_46/ --clock -p" # half lap
 # BAG_CMD="ros2 bag play -s mcap /mnt/e/rosbag2_2024_07_25-05_18_42/ --clock -p" # other half of lap
 # BAG_CMD="ros2 bag play -s mcap /mnt/e/rosbag2_2024_09_04-05_14_43 --clock -p" # 2.5 laps
-# BAG_CMD="ros2 bag play -s mcap /mnt/e/rosbag2_2024_09_24-06_08_59/ --clock -p" # 1 lap, loop close crashing
+BAG_CMD="ros2 bag play -s mcap /mnt/e/rosbag2_2024_09_24-02_11_49/ --clock -p" 
 # BAG_CMD="ros2 bag play -s mcap /mnt/e/rosbag2_2024_09_24-02_16_55/ --clock -p" # ebs test run
 # BAG_CMD="ros2 bag play -s mcap /mnt/e/rosbag2_2024_10_18-01_10_29/ --clock -p" # 9 laps, recording from 1st corner
 # BAG_CMD="ros2 bag play -s mcap /mnt/e/rosbag2_2024_10_18-01_40_49 --clock -p" # 10 laps recording from 2nd lap, 1st corner
 # BAG_CMD="ros2 bag play -s mcap /mnt/e/rosbag2_2024_10_30-04_40_17 --clock -p" # 14 laps recording from 2nd lap, 1st corner
-BAG_CMD="ros2 bag play -s mcap /mnt/e/rosbag2_2024_11_01-19_02_10 --clock -p" # 14 laps recording from 2nd lap, 1st corner
+# BAG_CMD="ros2 bag play -s mcap /mnt/e/rosbag2_2024_11_01-19_02_10 --clock -p" # 14 laps recording from 2nd lap, 1st corner
 
 TOPICS=(
-    # for mapping debugging
-    /imu/odometry # needed for RL
-    /vehicle/wheel_twist # needed for RL
-    # /lidar/objects
-    /lidar/converted_2D_scan
-    /lidar/cone_detection
-    # /debug_markers/lidar_markers
-    /system/av_state # needed for lap = 0 in mapping
-    /system/ros_state # needed for lap = 0 in mapping
+    # # for mapping debugging
+    # /imu/odometry # needed for RL
+    # /vehicle/wheel_twist # needed for RL
+    # # /lidar/objects
+    # /lidar/converted_2D_scan
+    # /lidar/cone_detection
+    # # /debug_markers/lidar_markers
+    # /system/av_state # needed for lap = 0 in mapping
+    # /system/ros_state # needed for lap = 0 in mapping
 
     # raw data for visuals (nice to always have)
-    /imu/nav_sat_fix
-    /vehicle/velocity
-    /vehicle/steering_angle
-    /imu/data
-    /control/driving_command
+    # /imu/nav_sat_fix
+    # /vehicle/velocity
+    # /vehicle/steering_angle
+    # /imu/data
+    # /control/driving_command
 
-    # for fast lap debugging
-    /tf
-    /tf_static
-    /odometry/filtered
-    /slam/occupancy_grid
-    /slam/occupancy_grid_metadata
-    /slam/pose
-    /slam/graph_visualization
+    # # for fast lap debugging
+    # /tf
+    # /tf_static
+    # /odometry/filtered
+    # /slam/occupancy_grid
+    # /slam/occupancy_grid_metadata
+    # /slam/pose
+    # /slam/graph_visualization
+
+    /lidar/objects
+    /lidar/ground_plane
 )
 
 # remap these topics if you are running programs which output to the same topic names
@@ -49,7 +52,9 @@ REMAPS=(
     # /odometry/filtered:=/odometry/filtered_old
     # /sbg_translated/odometry:=/sbg_translated/odometry_old
     # /tf:=/tf_old
-    /control/driving_command:=/control/driving_command_old
+    # /control/driving_command:=/control/driving_command_old
+    /lidar/objects:=/lidar/objects_old
+    /lidar/ground_plane:=/lidar/ground_plane_old
 )
 
 # for each topic append to the command
