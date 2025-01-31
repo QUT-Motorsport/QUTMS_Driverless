18a3aeaff9a3ba083ef5f601b796858bb581e1ea
kelvin <nguyenleminh1002@gmail.com>
2025-01-31 13:58:56 +1000

    test

--- src/control/ros2_control/bringup/launch/qev-3d_controller_launch.py
+++ src/control/ros2_control/bringup/launch/qev-3d_controller_launch.py
@@ -11,3 +11,25 @@ from launch_ros.substitutions import FindPackageShare
 def generate_launch_description():
       # List of arguments to be declared into Node  
       declared_arguments = []
+
+      # Open Rviz2 GUI
+      declared_arguments.append(
+            DeclareLaunchArgument(
+                  "gui",
+                  default_value="true", # Default value is true
+                  description="Start RViz2 automatically with this launch file.",
+            )
+      )
+
+      # Remap odometry TF from the steering controller to the TF tree
+      declared_arguments.append( 
+            DeclareLaunchArgument(
+                  "remap_odometry_tf",
+                  default_value="false", # Default value is false
+                  description="Remap odometry TF from the steering controller to the TF tree.",
+            )
+      )
+
+      # Arguments Initialization
+      gui = LaunchConfiguration("gui")
+      remap_odometry_tf = LaunchConfiguration("remap_odometry_tf")
