455e00374b8eccf465a57587dff256f45760a1e7
Dominic Grimaldi <dominic.grimaldi@connect.qut.edu.au>
2026-04-08 11:46:25 +1000

    Implement health check loop in safety monitor

--- src/operations/safety_monitor/safety_monitor/safety_monitor_node.py
+++ src/operations/safety_monitor/safety_monitor/safety_monitor_node.py
@@ -15,6 +15,13 @@ class SafetyMonitor(Node):
         # Watchdog parameters
         self.last_command_time = time.time()
         self.TIMEOUT_LIMIT = 0.5 # Seconds. Trigger E-Stop if no message for 500ms
+
+        self.health_status = {
+            "lidar": False,
+            "planner": False,
+            "vcu": False,
+            "slam": False
+        }
         
         # Create a subscriber to the driving command topic
         self.subscription = self.create_subscription(
@@ -46,6 +53,19 @@ class SafetyMonitor(Node):
         else:
             self.get_logger().info(f'Monitoring -> Speed: {speed:.2f} m/s, Steering: {steering:.2f} rad')
 
+    def lidar_callback(self, msg):
+        # LiDaR is healthy if we recieved a message
+        self.health_status["lidar"] = True
+        self.last_lidar_received = self.get_clock().now()
+
+    def planner_callback(self, msg):
+        # Permorm math check
+        speed_ok = abs(msg.drive.speed) <= self.MAX_SPEED
+
+        # Update based on math
+        self.health_status["lidar"] = speed_ok
+        self.last_lidar_received = self.get_clock().now()
+
     def check_watchdogs(self):
         if self.system_locked:
             return
@@ -57,14 +77,31 @@ class SafetyMonitor(Node):
         if time_since_last_msg > self.TIMEOUT_LIMIT:
             self.trigger_estop(f"Lost communication with Planner! (Timeout: {time_since_last_msg:.2f}s)")
 
+    def health_check_loop(self):
+        if self.system_locked:
+            return
+        
+        now = self.get_clock().now()
+
+        # Check for timeouts and set health to False if a node is silent
+        if (now - self.last_lidar_received).nanoseconds > 0.5 * 1e9: # 500ms
+            self.health_status["lidar"] = False
+            
+        if (now - self.last_planner_received).nanoseconds > 0.2 * 1e9: # 200ms
+            self.health_status["planner"] = False
+        
+        # Returns True if all values are True
+        if not all(self.health_status.values()):
+            # Find failed for logs
+            failed_systems = [k for k, v in self.health_status.items() if not v]
+            self.trigger_estop(f"System Health Failure: {failed_systems}")
+
     def trigger_estop(self, reason):
         # Update system lock
         self.system_locked = True
 
         # Update logs
-        self.get_logger().error('CRITICAL SAFETY VIOLATION!')
-        self.get_logger().error(f'Attempted Speed: {speed:.2f}, Attempted Steering: {steering:.2f}')
-        self.get_logger().error('E-STOP TRIGGERED. Locking system.')
+        self.get_logger().error(f'E-STOP TRIGGERED! Reason: {reason}')
 
         # TODO VCU cut power
 
