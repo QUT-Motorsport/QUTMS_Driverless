3662a80f978ed7e18d8694b69e88c4f96d1547aa
Walden Leung <7531347+leungw16@users.noreply.github.com>
2026-04-27 15:48:57 +1000

    Modify startup launch to launch based on env variables

--- src/operations/vehicle_bringup/launch/bootup.launch.py
+++ src/operations/vehicle_bringup/launch/bootup.launch.py
@@ -2,21 +2,25 @@ import os
 
 from ament_index_python.packages import get_package_share_path
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, RegisterEventHandler, SetEnvironmentVariable
+from launch.actions import IncludeLaunchDescription, RegisterEventHandler, SetEnvironmentVariable, GroupAction
+from launch.conditions import IfCondition, UnlessCondition
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+from launch.subsitutions import EnvironmentVariable
 
 
 def generate_launch_description():
     vehicle_supervisor_node = Node(
         package="vehicle_bringup",
         executable="vehicle_supervisor_node",
+        condition=UnlessCondition(EnvironmentVariable('PUSHCART')),
     )
 
     system_watcher = Node(
         package="vehicle_bringup",
         executable="system_watcher_node",
+        condition=UnlessCondition(EnvironmentVariable('PUSHCART')),
     )
 
     system_launch = IncludeLaunchDescription(
@@ -25,21 +29,39 @@ def generate_launch_description():
         )
     )
 
-    launch_system_on_watcher_exit = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=system_watcher,
-            on_exit=[system_launch],
-        )
+    launch_system_on_watcher_exit = GroupAction(
+        actions=[
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=system_watcher,
+                    on_exit=[system_launch],
+                )
+            )
+        ],
+        condition=UnlessCondition(EnvironmentVariable('PUSHCART')),
     )
 
     mission_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_path("vehicle_bringup"), "launch", "mission.launch.py")
-        )
+        ),
+        condition=UnlessCondition(EnvironmentVariable('PUSHCART')),
     )
 
     sbg_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(get_package_share_path("nav_bringup"), "launch", "sbg.launch.py"))
+    )
+
+    rosbag = Node(
+        package='rosbag2_transport',
+        executable='recorder',
+        name='rosbag_recorder',
+        parameters = os.path.join(get_package_share_path("vehicle_bringup"), "config", "rosbag.yaml"),
+    )
+
+    pushcart_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(get_package_share_path("vehicle_bringup"), "launch", "push_log.launch.py")),
+        condition=IfCondition(EnvironmentVariable('PUSHCART')),
     )
 
     stdout_linebuf_envvar = SetEnvironmentVariable("RCUTILS_LOGGING_BUFFERED_STREAM", "0")
@@ -49,9 +71,11 @@ def generate_launch_description():
             system_launch,
             stdout_linebuf_envvar,
             sbg_launch,
-            # system_watcher,
-            # launch_system_on_watcher_exit,
-            # mission_launch,
-            # vehicle_supervisor_node,
+            rosbag,
+            pushcart_launch,
+            system_watcher,
+            launch_system_on_watcher_exit,
+            mission_launch,
+            vehicle_supervisor_node,
         ]
     )
